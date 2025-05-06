2cff6b3becc9f9465e70d97613252b6df7341858
Paul-Emmanuel Raoul <skyper@skyplabs.net>
2025-05-06 13:38:16 +0200

    Add new routes and operation placeholders

---
+++ routers/api/packages/opentofu/backend/http/http.go
@@ -0,0 +1,25 @@
+package http
+
+import (
+	"forgejo.org/services/context"
+)
+
+func GetState(ctx *context.Context) {
+	panic("Not yet implemented")
+}
+
+func UpdateState(ctx *context.Context) {
+	panic("Not yet implemented")
+}
+
+func LockState(ctx *context.Context) {
+	panic("Not yet implemented")
+}
+
+func UnlockState(ctx *context.Context) {
+	panic("Not yet implemented")
+}
+
+func DeleteState(ctx *context.Context) {
+	panic("Not yet implemented")
+}
