ec24685fddf725985beaf66c18b14b2450a3a6b9
Alex Smith <amsmith.pro@pm.me>
2025-06-11 19:19:39 +0200

    chore: add conditional lazy-load

--- web_src/js/render/gltf.js
+++ web_src/js/render/gltf.js
@@ -1,5 +1,8 @@
 // import { ModelViewerElement } from '@google/model-viewer';
 
 export async function initGltfViewer() {
+  const els = document.querySelectorAll('.gltf-viewer');
+  if (!els.length) return;
+
   const gltfviewer = await import(/* webpackChunkName: "@google/model-viewer" */'@google/model-viewer');
 }
