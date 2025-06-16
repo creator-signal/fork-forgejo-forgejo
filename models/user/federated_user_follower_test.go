9b0dfdfb671339ef88441162a46c6e4c50110fb9
Michael Jerger <michael.jerger@meissa-gmbh.de>
2025-06-16 11:19:16 +0200

    reflect PR comments

--- models/user/federated_user_follower_test.go
+++ models/user/federated_user_follower_test.go
@@ -7,6 +7,7 @@ import (
 	"testing"
 
 	"forgejo.org/modules/validation"
+	"github.com/stretchr/testify/assert"
 )
 
 func Test_FederatedUserFollowerValidation(t *testing.T) {
@@ -14,14 +15,12 @@ func Test_FederatedUserFollowerValidation(t *testing.T) {
 		FollowedUserID:  12,
 		FollowingUserID: 1,
 	}
-	if res, err := validation.IsValid(sut); !res {
-		t.Errorf("sut should be valid but was %q", err)
-	}
+	res, err := validation.IsValid(sut)
+	assert.Truef(t, res, "sut should be valid but was %q", err)
 
 	sut = FederatedUserFollower{
 		FollowedUserID: 1,
 	}
-	if res, _ := validation.IsValid(sut); res {
-		t.Errorf("sut should be invalid")
-	}
+	res, _ = validation.IsValid(sut)
+	assert.False(t, res, "sut should be invalid")
 }
