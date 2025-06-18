f8db9a554ad11b9d1801aadff812e8cf6fde21c6
Earl Warren <contact@earl-warren.org>
2025-06-18 12:36:12 +0200

    fix: only send Forgejo Actions notifications to one user
    - If the run was attributed to a system user (which is the case for scheduled runs
      for instance), ignore it and fallback to the mail of the
      owner. System users may have email addresses, but they are not to be used.
    - If the owner is a system user or an organization with no email
      associated with it, do nothing.
    - If a user with an email exists, check if they did not disable notifications
      and send the email.
    
    Refs: https://codeberg.org/forgejo/forgejo/issues/8187

--- services/mailer/mail_actions.go
+++ services/mailer/mail_actions.go
@@ -23,19 +23,20 @@ func MailActionRun(run *actions_model.ActionRun, priorStatus actions_model.Statu
 		return nil
 	}
 
-	if run.TriggerUser.Email != "" && run.TriggerUser.EmailNotificationsPreference != user_model.EmailNotificationsDisabled {
-		if err := sendMailActionRun(run.TriggerUser, run, priorStatus, lastRun); err != nil {
-			return err
-		}
+	user := run.TriggerUser
+	// this happens e.g. when this is a scheduled run
+	if user.IsSystem() {
+		user = run.Repo.Owner
+	}
+	if user.IsSystem() || user.Email == "" {
+		return nil
 	}
 
-	if run.Repo.Owner.Email != "" && run.Repo.Owner.Email != run.TriggerUser.Email && run.Repo.Owner.EmailNotificationsPreference != user_model.EmailNotificationsDisabled {
-		if err := sendMailActionRun(run.Repo.Owner, run, priorStatus, lastRun); err != nil {
-			return err
-		}
+	if user.EmailNotificationsPreference == user_model.EmailNotificationsDisabled {
+		return nil
 	}
 
-	return nil
+	return sendMailActionRun(user, run, priorStatus, lastRun)
 }
 
 func sendMailActionRun(to *user_model.User, run *actions_model.ActionRun, priorStatus actions_model.Status, lastRun *actions_model.ActionRun) error {
