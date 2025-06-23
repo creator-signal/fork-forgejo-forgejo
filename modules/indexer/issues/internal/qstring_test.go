c16c60bb5494eddaae649cf36d60052c0cdfc762
Danko Aleksejevs <danko@very.lv>
2025-06-23 19:21:56 +0300

    Fix Token.ParseIssueReference crashing on empty string

--- modules/indexer/issues/internal/qstring_test.go
+++ modules/indexer/issues/internal/qstring_test.go
@@ -169,3 +169,35 @@ func TestIssueQueryString(t *testing.T) {
 		})
 	}
 }
+
+func TestToken_ParseIssueReference(t *testing.T) {
+	var tk Token
+	{
+		tk.Term = "123"
+		id, err := tk.ParseIssueReference()
+		require.NoError(t, err)
+		assert.Equal(t, id, int64(123))
+	}
+	{
+		tk.Term = "#123"
+		id, err := tk.ParseIssueReference()
+		require.NoError(t, err)
+		assert.Equal(t, id, int64(123))
+	}
+	{
+		tk.Term = "!123"
+		id, err := tk.ParseIssueReference()
+		require.NoError(t, err)
+		assert.Equal(t, id, int64(123))
+	}
+	{
+		tk.Term = "text"
+		_, err := tk.ParseIssueReference()
+		require.Error(t, err)
+	}
+	{
+		tk.Term = ""
+		_, err := tk.ParseIssueReference()
+		require.Error(t, err)
+	}
+}
