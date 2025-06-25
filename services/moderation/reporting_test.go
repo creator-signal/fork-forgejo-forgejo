74bddee30bc1365a658bdf2396b710684ecc461b
Leni Kadali <lenikadali@noreply.codeberg.org>
2025-06-25 13:07:13 +0300

    Fix type, add attempt at test
    Fix type comparison and add an attempt at a test
    for the new method

---
+++ services/moderation/reporting_test.go
@@ -0,0 +1,35 @@
+package moderation
+
+import (
+	"testing"
+
+	"forgejo.org/models/db"
+	report_model "forgejo.org/models/moderation"
+	"forgejo.org/models/unittest"
+	"forgejo.org/modules/timeutil"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestMain(m *testing.M) {
+	unittest.MainTest(m)
+}
+
+func TestRemoveResolvedReportsTypeHandled(t *testing.T) {
+	require.NoError(t, unittest.PrepareTestDatabase())
+	// Add a resolved report
+	resolvedReport := &report_model.AbuseReport{
+		Status:     report_model.ReportStatusTypeHandled,
+		ReporterID: 1, ContentType: report_model.ReportedContentTypeRepository,
+		ContentID: 2, Category: report_model.AbuseCategoryTypeOther,
+		CreatedUnix: timeutil.TimeStampNow(),
+	}
+	_, err := db.GetEngine(db.DefaultContext).NoAutoTime().Insert(resolvedReport)
+	require.NoError(t, err)
+
+	err = RemoveResolvedReports(db.DefaultContext, 0)
+	require.NoError(t, err)
+
+	// Resolved reports older than a minute should be deleted.
+	unittest.AssertExistsIf(t, false, resolvedReport)
+}
