87393e0bde8a3c421109ec35286d84c3bd106e3d
Earl Warren <contact@earl-warren.org>
2025-07-07 19:52:44 +0200

    fix: corrupted wiki unit default permission (#8234 follow-up) (#8258) (lint followup)

--- models/forgejo_migrations/v36.go
+++ models/forgejo_migrations/v36.go
@@ -1,7 +1,7 @@
 // Copyright 2025 The Forgejo Authors. All rights reserved.
 // SPDX-License-Identifier: GPL-3.0-or-later
 
-package forgejo_migrations
+package forgejo_migrations //nolint:revive
 
 import (
 	"xorm.io/xorm"
