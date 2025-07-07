6ad8f790a4b8bcd3deb2e85fff40835b41900d81
kochklops <kochklops@noreply.codeberg.org>
2025-07-07 17:03:55 +0000

    Test custom merge message template

--- services/pull/pull_test.go
+++ services/pull/pull_test.go
@@ -102,6 +102,10 @@ func TestPullRequest_GetDefaultMergeMessage_GlobalTemplate(t *testing.T) {
 	require.NoError(t, err)
 	defer gitRepo.Close()
 
+	templateRepo, err := git.OpenRepository(git.DefaultContext, "./../../modules/git/tests/repos/templates_repo")
+	require.NoError(t, err)
+	defer templateRepo.Close()
+
 	mergeMessageTemplates[repo_model.MergeStyleMerge] = "${PullRequestTitle} (${PullRequestReference})\n${PullRequestDescription}"
 
 	// Check template is used for Merge...
@@ -116,4 +120,11 @@ func TestPullRequest_GetDefaultMergeMessage_GlobalTemplate(t *testing.T) {
 	require.NoError(t, err)
 
 	assert.Equal(t, "Merge pull request 'issue3' (#3) from branch2 into master", mergeMessage)
+
+	// ...and that custom Merge template takes priority
+	mergeMessage, body, err = GetDefaultMergeMessage(db.DefaultContext, templateRepo, pr, repo_model.MergeStyleMerge)
+	require.NoError(t, err)
+
+	assert.Equal(t, "Default merge message template", mergeMessage)
+	assert.Equal(t, "This line was read from .forgejo/default_merge_message/MERGE_TEMPLATE.md", body)
 }
