ceea2330deb6ca6b64165bbe1403024f5cb3989f
floss4good <floss4good@disroot.org>
2025-07-16 02:39:03 +0300

    extend integration tests in order make sure the reports linked to the same content are grouped together
    and to check that correct icon, title, content URL, category and reports count are shown

--- tests/integration/admin_moderation_test.go
+++ tests/integration/admin_moderation_test.go
@@ -5,8 +5,10 @@ package integration
 
 import (
 	"net/http"
+	"strings"
 	"testing"
 
+	"forgejo.org/models/unittest"
 	"forgejo.org/modules/setting"
 	"forgejo.org/modules/test"
 	"forgejo.org/routers"
@@ -15,7 +17,34 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+func testReportDetails(t *testing.T, htmlDoc *HTMLDoc, reportID, contentIcon, contentRef, contentURL, category, reportsNo string) {
+	// Check icon octicon
+	icon := htmlDoc.Find("#report-" + reportID + " svg." + contentIcon)
+	assert.Equal(t, 1, icon.Length())
+
+	// Check content reference and URL
+	title := htmlDoc.Find("#report-" + reportID + " .flex-item-main .flex-item-title a")
+	assert.Equal(t, 1, title.Length())
+
+	assert.Equal(t, contentRef, title.Text())
+
+	href, exists := title.Attr("href")
+	assert.True(t, exists)
+	assert.Equal(t, contentURL, href)
+
+	// Check category
+	cat := htmlDoc.Find("#report-" + reportID + " .flex-item-main .flex-items-inline .item:nth-child(3)")
+	assert.Equal(t, 1, cat.Length())
+	assert.Equal(t, category, strings.TrimSpace(cat.Text()))
+
+	// Check number of reports for the same content
+	count := htmlDoc.Find("#report-" + reportID + " a span")
+	assert.Equal(t, 1, count.Length())
+	assert.Equal(t, reportsNo, count.Text())
+}
+
 func TestAdminModerationViewReports(t *testing.T) {
+	defer unittest.OverrideFixtures("tests/integration/fixtures/TestAdminModerationViewReports")()
 	defer tests.PrepareTestEnv(t)()
 
 	t.Run("Moderation enabled", func(t *testing.T) {
@@ -40,11 +69,25 @@ func TestAdminModerationViewReports(t *testing.T) {
 		t.Run("Admin user", func(t *testing.T) {
 			defer tests.PrintCurrentTest(t)()
 
-			assert.Equal(t, true, setting.Moderation.Enabled)
-
 			session := loginUser(t, "user1")
 			req := NewRequest(t, "GET", "/admin/moderation/reports")
-			session.MakeRequest(t, req, http.StatusOK)
+			resp := session.MakeRequest(t, req, http.StatusOK)
+			htmlDoc := NewHTMLParser(t, resp.Body)
+
+			// Check how many reports are being displayed.
+			// Reports linked to the same content (type and id) should be grouped; therefore we should see only 6 instead of 9.
+			reports := htmlDoc.Find(".admin-setting-content .flex-list .flex-item.report")
+			assert.Equal(t, 6, reports.Length())
+
+			// Check details for shown reports.
+			testReportDetails(t, htmlDoc, "1", "octicon-person", "@SPAM-services", "/SPAM-services", "Illegal content", "1")
+			testReportDetails(t, htmlDoc, "2", "octicon-repo", "SPAM-services/spammer-Tools", "/SPAM-services/spammer-Tools", "Illegal content", "1")
+			testReportDetails(t, htmlDoc, "3", "octicon-issue-opened", "SPAM-services/spammer-Tools#1", "/SPAM-services/spammer-Tools/issues/1", "Spam", "1")
+			// #4 is combined with #7 and #9
+			testReportDetails(t, htmlDoc, "4", "octicon-person", "@spammer01", "/spammer01", "Spam", "3")
+			// #5 is combined with #6
+			testReportDetails(t, htmlDoc, "5", "octicon-comment", "contributor/first/issues/1#issuecomment-1001", "/contributor/first/issues/1#issuecomment-1001", "Malware", "2")
+			testReportDetails(t, htmlDoc, "8", "octicon-issue-opened", "contributor/first#1", "/contributor/first/issues/1", "Other violations of platform rules", "1")
 		})
 	})
 
