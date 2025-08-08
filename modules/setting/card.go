4fe542f0951333453414d53b721cbc774b78b3d0
Gnarwhal <git.aspect893@passmail.net>
2025-08-08 19:38:41 +0000

    feat: add setting for custom card background color

--- modules/setting/card.go
+++ modules/setting/card.go
@@ -2,6 +2,8 @@ package setting
 
 import (
 	"image/color"
+	"regexp"
+	"strconv"
 
 	"forgejo.org/modules/log"
 )
@@ -32,6 +34,7 @@ var Card = struct {
 func loadCardFrom(rootCfg ConfigProvider) {
 	sec := rootCfg.Section("card")
 	loadTheme(sec.Key("THEME").MustString("light"))
+	loadBackground(sec.Key("BACKGROUND").MustString(""))
 }
 
 func loadTheme(theme string) {
@@ -49,4 +52,35 @@ func loadTheme(theme string) {
 	Card.Background.Color = color.White
 	Card.Text.PrimaryColor = color.Black
 	Card.Text.SecondaryColor = color.Gray{128}
+}
+
+var rgbPattern = regexp.MustCompile(`(?:rgb\(\s*([0-9]{1,3})\s*,\s*([0-9]{1,3})\s*,\s*([0-9]{1,3}\s*)\))`)
+
+func loadBackground(src string) {
+	if src == "" {
+		return
+	}
+
+	if rgb := rgbPattern.FindStringSubmatch(src); rgb != nil {
+		r, errR := strconv.ParseUint(rgb[1], 10, 8)
+		if errR != nil {
+			log.Error("r parameter to rgb(...) must be in range [0, 255]")
+		}
+		g, errG := strconv.ParseUint(rgb[2], 10, 8)
+		if errG != nil {
+			log.Error("g parameter to rgb(...) must be in range [0, 255]")
+		}
+		b, errB := strconv.ParseUint(rgb[3], 10, 8)
+		if errB != nil {
+			log.Error("b parameter to rgb(...) must be in range [0, 255]")
+		}
+
+		if errR != nil || errG != nil || errB != nil {
+			return
+		}
+
+		Card.Background.Color = color.RGBA{uint8(r), uint8(g), uint8(b), 255}
+	}
+
+	log.Error("card.BACKGROUND must be 'rgb(...)'")
 }
