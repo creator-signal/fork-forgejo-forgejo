e4461a0cb40d4ac4d817d52ed1b338dd684c36f8
Gnarwhal <git.aspect893@passmail.net>
2025-08-08 22:00:55 +0000

    feat: add settings for custom summary card text colors

--- modules/setting/card.go
+++ modules/setting/card.go
@@ -35,6 +35,8 @@ func loadCardFrom(rootCfg ConfigProvider) {
 	sec := rootCfg.Section("card")
 	loadTheme(sec.Key("THEME").MustString("light"))
 	loadBackground(sec.Key("BACKGROUND").MustString(""))
+	loadTextPrimary(sec.Key("TEXT_PRIMARY").MustString(""))
+	loadTextSecondary(sec.Key("TEXT_SECONDARY").MustString(""))
 }
 
 func loadTheme(theme string) {
@@ -54,33 +56,82 @@ func loadTheme(theme string) {
 	Card.Text.SecondaryColor = color.Gray{128}
 }
 
-var rgbPattern = regexp.MustCompile(`(?:rgb\(\s*([0-9]{1,3})\s*,\s*([0-9]{1,3})\s*,\s*([0-9]{1,3}\s*)\))`)
-
 func loadBackground(src string) {
 	if src == "" {
 		return
 	}
 
-	if rgb := rgbPattern.FindStringSubmatch(src); rgb != nil {
-		r, errR := strconv.ParseUint(rgb[1], 10, 8)
-		if errR != nil {
-			log.Error("r parameter to rgb(...) must be in range [0, 255]")
-		}
-		g, errG := strconv.ParseUint(rgb[2], 10, 8)
-		if errG != nil {
-			log.Error("g parameter to rgb(...) must be in range [0, 255]")
-		}
-		b, errB := strconv.ParseUint(rgb[3], 10, 8)
-		if errB != nil {
-			log.Error("b parameter to rgb(...) must be in range [0, 255]")
-		}
+	color, err := tryColorFromRgb(src)
+	if err != nil {
+		log.Error("failed to process card.BACKGROUND rgb(...): %v", err)
+		return
+	}
 
-		if errR != nil || errG != nil || errB != nil {
-			return
-		}
-
-		Card.Background.Color = color.RGBA{uint8(r), uint8(g), uint8(b), 255}
+	if color != nil {
+		Card.Background.Color = color
+		return
 	}
 
 	log.Error("card.BACKGROUND must be 'rgb(...)'")
+}
+
+func loadTextPrimary(src string) {
+	if src == "" {
+		return
+	}
+
+	color, err := tryColorFromRgb(src)
+	if err != nil {
+		log.Error("failed to process card.TEXT_PRIMARY rgb(...): %v", err)
+		return
+	}
+
+	if color != nil {
+		Card.Text.PrimaryColor = color
+		return
+	}
+
+	log.Error("card.TEXT_PRIMARY must be 'rgb(...)'")
+}
+
+func loadTextSecondary(src string) {
+	if src == "" {
+		return
+	}
+
+	color, err := tryColorFromRgb(src)
+	if err != nil {
+		log.Error("failed to process card.TEXT_SECONDARY rgb(...): %v", err)
+		return
+	}
+
+	if color != nil {
+		Card.Text.SecondaryColor = color
+		return
+	}
+
+	log.Error("card.TEXT_SECONDARY must be 'rgb(...)'")
+}
+
+var rgbPattern = regexp.MustCompile(`(?:rgb\(\s*([0-9]{1,3})\s*,\s*([0-9]{1,3})\s*,\s*([0-9]{1,3}\s*)\))`)
+
+func tryColorFromRgb(src string) (color.Color, error) {
+	if rgb := rgbPattern.FindStringSubmatch(src); rgb != nil {
+		r, err := strconv.ParseUint(rgb[1], 10, 8)
+		if err != nil {
+			return nil, err
+		}
+		g, err := strconv.ParseUint(rgb[2], 10, 8)
+		if err != nil {
+			return nil, err
+		}
+		b, err := strconv.ParseUint(rgb[3], 10, 8)
+		if err != nil {
+			return nil, err
+		}
+
+		return color.RGBA{uint8(r), uint8(g), uint8(b), 255}, nil
+	}
+
+	return nil, nil
 }
