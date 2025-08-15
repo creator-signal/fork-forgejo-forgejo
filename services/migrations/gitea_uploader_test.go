68bf202a511550d909194f51fd7eea8928c3eaf3
erik <erik_se@posteo.de>
2025-08-15 10:29:56 +0200

    Update signature

--- services/migrations/gitea_uploader_test.go
+++ services/migrations/gitea_uploader_test.go
@@ -156,7 +156,7 @@ func TestGiteaUploadRepo(t *testing.T) {
 
 	var (
 		ctx        = t.Context()
-		downloader = NewGithubDownloaderV3(ctx, true, true, "https://github.com", "", "", "", "go-xorm", "builder")
+		downloader = NewGithubDownloaderV3(ctx, "https://github.com", true, true, "", "", "", "go-xorm", "builder")
 		repoName   = "builder-" + time.Now().Format("2006-01-02-15-04-05")
 		uploader   = NewGiteaLocalUploader(graceful.GetManager().HammerContext(), user, user.Name, repoName)
 	)
