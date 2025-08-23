a66177991105fac5876b53b63f8335a4ab4e0bc0
JakobDev <jakobdev@gmx.de>
2025-08-23 23:16:16 +0200

    Use other YAML package

--- tests/integration/api_funding_test.go
+++ tests/integration/api_funding_test.go
@@ -18,7 +18,7 @@ import (
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
-	"gopkg.in/yaml.v3"
+	"go.yaml.in/yaml/v3"
 )
 
 func createFundingConfig(t *testing.T, user *user_model.User, repo *repo_model.Repository, fundingConfig map[string]any) {
