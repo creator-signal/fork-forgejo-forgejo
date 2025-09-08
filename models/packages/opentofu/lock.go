465bc05d9dd30abb08698a8c0f9fc3999657a004
Paul-Emmanuel Raoul <skyper@skyplabs.net>
2025-09-09 00:47:55 +0200

    Add OpenTofu state lock logic to models

---
+++ models/packages/opentofu/lock.go
@@ -0,0 +1,216 @@
+package opentofu
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"forgejo.org/models/db"
+	"forgejo.org/modules/util"
+)
+
+// ErrStateLockNotExist represents a "StateLockNotExist" kind of error.
+//
+// It is returned when no state lock exists for a given package.
+type ErrStateLockNotExist struct {
+	PackageName string
+	OwnerID     int64
+}
+
+// IsErrStateLockNotExist checks if an error is of type ErrStateLockNotExist.
+func IsErrStateLockNotExist(err error) bool {
+	_, ok := err.(ErrStateLockNotExist)
+	return ok
+}
+
+func (err ErrStateLockNotExist) Error() string {
+	return fmt.Sprintf("package %s is not locked [OwnerID: %d]", err.PackageName, err.OwnerID)
+}
+
+func (err ErrStateLockNotExist) Unwrap() error {
+	return util.ErrNotExist
+}
+
+// ErrStateLockAlreadyExist represents a "StateLockAlreadyExist" kind of error.
+//
+// It is returned when a state lock already exists for a given package.
+type ErrStateLockAlreadyExist struct {
+	PackageName string
+	OwnerID     int64
+}
+
+// IsErrStateLockAlreadyExist checks if an error is of type
+// StateLockAlreadyExist.
+func IsErrStateLockAlreadyExist(err error) bool {
+	_, ok := err.(ErrStateLockAlreadyExist)
+	return ok
+}
+
+func (err ErrStateLockAlreadyExist) Error() string {
+	return fmt.Sprintf("package %s is already locked [OwnerID: %d]", err.PackageName, err.OwnerID)
+}
+
+func (err ErrStateLockAlreadyExist) Unwrap() error {
+	return util.ErrAlreadyExist
+}
+
+// ErrInvalidLockID represents a "InvalidLockID" kind of error.
+//
+// It is returned when a given lock ID does not match the lock ID stored in
+// the database.
+type ErrInvalidLockID struct {
+	PackageName string
+	OwnerID     int64
+	LockID      string
+}
+
+// IsErrInvalidLockID checks if an error is of type ErrInvalidLockID.
+func IsErrInvalidLockID(err error) bool {
+	_, ok := err.(ErrInvalidLockID)
+	return ok
+}
+
+func (err ErrInvalidLockID) Error() string {
+	return fmt.Sprintf("lock ID %s is invalid [PackageName: %s, OwnerID: %d]", err.LockID, err.PackageName, err.OwnerID)
+}
+
+func (err ErrInvalidLockID) Unwrap() error {
+	return util.ErrPermissionDenied
+}
+
+// StateLock represents a state lock preventing an OpenTofu/Terraform state file
+// from being updating if another user is already updating it.
+//
+// The state file to lock is identified by a package name and its owner ID.
+type StateLock struct {
+	ID int64 `xorm:"pk autoincr" json:"-"`
+
+	// The name of the locked package.
+	PackageName string `xorm:"INDEX NOT NULL" json:"-"`
+
+	// ID of the package owner (either a user or an organisation).
+	OwnerID int64 `xorm:"INDEX NOT NULL" json:"-"`
+
+	// The lock ID itself.
+	//
+	// The lock ID is a string that can take any form as long as it is random. At
+	// the time of writing these lines, OpenTofu sends a UUID v4:
+	//
+	// https://github.com/opentofu/opentofu/blob/22910f2b01f7ade5aa9ec7505f9e24dfe65e5e6c/internal/states/statemgr/locker.go#L164-L174
+	//
+	// Given that the maximum size of a lock ID is not standardised, a trade-off has
+	// been made to prevent a malicious client from sending a large lock ID value
+	// while being agnostic to the client implementation.
+	LockID string `xorm:"VARCHAR(80) NOT NULL" json:"ID"`
+
+	// The client software operation for which the lock request has been made (e.g. plan, apply).
+	Operation string `xorm:"VARCHAR(40)" json:"Operation"`
+
+	// Optional extra info provided by the client software.
+	Info string `xorm:"VARCHAR(300)" json:"Info,omitempty"`
+
+	// Name of the Forgejo user having performed the lock request.
+	UserName string `xorm:"NOT NULL" json:"Who"`
+
+	// Version of the client software having performed the lock request.
+	ClientVersion string `xorm:"VARCHAR(40)" json:"Version,omitempty"`
+
+	// Timestamp of the lock creation.
+	CreatedUnix time.Time `xorm:"created NOT NULL" json:"Created"`
+
+	// Package path.
+	Path string `xorm:"-" json:"Path"`
+}
+
+// TableName sets the table name for the Lock struct
+func (s *StateLock) TableName() string {
+	return "package_opentofu_state_locks"
+}
+
+// init registers the Lock struct as table in the database
+func init() {
+	db.RegisterModel(new(StateLock))
+}
+
+// GetLock returns the state lock, if any, based on the package name and its
+// owner ID.
+//
+// Returns ErrStateLockNotExist if the state lock is not found.
+func GetLock(ctx context.Context, packageName string, ownerID int64) (*StateLock, error) {
+	var lock StateLock
+
+	has, err := db.GetEngine(ctx).Where(
+		"package_name = ?",
+		packageName,
+	).And(
+		"owner_id = ?",
+		ownerID,
+	).Get(&lock)
+	if err != nil {
+		return nil, err
+	} else if !has {
+		return nil, ErrStateLockNotExist{packageName, ownerID}
+	}
+
+	return &lock, nil
+}
+
+// Lock locks an OpenTofu/Terraform state file package by inserting the lock
+// info given as parameter into the database.
+//
+// The package to lock is identified by its name and its owner ID.
+//
+// If the package is already locked, the lock info from the database are
+// returned in addition to ErrStateLockAlreadyExist.
+func Lock(ctx context.Context, lockInfo *StateLock) (*StateLock, error) {
+	lock, err := GetLock(ctx, lockInfo.PackageName, lockInfo.OwnerID)
+	if err != nil {
+		if !IsErrStateLockNotExist(err) {
+			return nil, err
+		}
+	} else {
+		return lock, ErrStateLockAlreadyExist{lockInfo.PackageName, lockInfo.OwnerID}
+	}
+
+	_, err = db.GetEngine(ctx).Insert(lockInfo)
+	if err != nil {
+		return nil, err
+	}
+
+	return lockInfo, nil
+}
+
+// Unlock unlocks an OpenTofu/Terraform state file package by removing the lock
+// ID given as parameter from the database.
+//
+// The package to unlock is identified by its name and its owner ID. The lock ID
+// needs to match the value stored in the database.
+//
+// Returns ErrStateLockNotExist if the state lock does not exist.
+func Unlock(ctx context.Context, packageName string, ownerID int64, lockID string) error {
+	lock, err := GetLock(ctx, packageName, ownerID)
+	if err != nil {
+		return err
+	}
+
+	if lockID != lock.LockID {
+		return ErrInvalidLockID{
+			LockID:      lockID,
+			PackageName: packageName,
+			OwnerID:     ownerID,
+		}
+	}
+
+	_, err = db.GetEngine(ctx).Where(
+		"package_name = ?",
+		lock.PackageName,
+	).And(
+		"owner_id = ?",
+		ownerID,
+	).Delete(lock)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
---
+++ models/packages/opentofu/lock_test.go
@@ -0,0 +1,261 @@
+package opentofu
+
+import (
+	"fmt"
+	"strings"
+	"testing"
+
+	"forgejo.org/models/db"
+	packages_model "forgejo.org/models/packages"
+	"forgejo.org/models/unittest"
+	user_model "forgejo.org/models/user"
+	"forgejo.org/modules/packages"
+	"forgejo.org/modules/setting"
+	"forgejo.org/modules/test"
+	packages_service "forgejo.org/services/packages"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestMain(m *testing.M) {
+	unittest.MainTest(m)
+}
+
+func preparePackage(t *testing.T, owner *user_model.User, name string) int64 {
+	t.Helper()
+
+	data, err := packages.CreateHashedBufferFromReader(strings.NewReader("data"))
+	require.NoError(t, err)
+
+	pv, _, err := packages_service.CreatePackageOrAddFileToExisting(
+		db.DefaultContext,
+		&packages_service.PackageCreationInfo{
+			PackageInfo: packages_service.PackageInfo{
+				Owner:       owner,
+				PackageType: packages_model.TypeOpenTofuState,
+				Name:        name,
+			},
+			Creator: owner,
+		},
+		&packages_service.PackageFileCreationInfo{
+			PackageFileInfo: packages_service.PackageFileInfo{
+				Filename: name,
+			},
+			Data:    data,
+			Creator: owner,
+			IsLead:  true,
+		},
+	)
+
+	require.NoError(t, err)
+	return pv.PackageID
+}
+
+func prepareStateLock(t *testing.T, lockInfo *StateLock) {
+	t.Helper()
+
+	e := db.GetEngine(db.DefaultContext)
+
+	_, err := e.Insert(lockInfo)
+	require.NoError(t, err)
+}
+
+func TestGetLock(t *testing.T) {
+	require.NoError(t, unittest.PrepareTestDatabase())
+	defer test.MockVariableValue(&setting.Database.IterateBufferSize, 1)()
+
+	user := unittest.AssertExistsAndLoadBean(t, &user_model.User{ID: 2})
+
+	clientVersion := "OpenTofu v1.10.6"
+	extraInfo := "Dummy extra info"
+
+	packageNameUnlocked := "opentofu-state-unlocked"
+	preparePackage(t, user, packageNameUnlocked)
+
+	packageNameLocked := "opentofu-state-locked"
+	preparePackage(t, user, packageNameLocked)
+	lockIDLocked := "110e9066-29ca-49b6-b507-87c58bd1177b"
+	prepareStateLock(t, &StateLock{
+		PackageName:   packageNameLocked,
+		OwnerID:       user.ID,
+		LockID:        lockIDLocked,
+		Info:          extraInfo,
+		UserName:      user.Name,
+		ClientVersion: clientVersion,
+	})
+
+	// Try to get the state lock of a non-existing package.
+	t.Run("NoPackage", func(t *testing.T) {
+		lock, err := GetLock(db.DefaultContext, "non-existing-package", user.ID)
+		require.Nil(t, lock)
+		require.Error(t, err)
+		require.IsType(t, ErrStateLockNotExist{}, err)
+		expectedError := fmt.Sprintf("package %s is not locked [OwnerID: %d]", "non-existing-package", user.ID)
+		require.Errorf(t, err, expectedError)
+	})
+
+	// Try to get the state lock of an existing package that is not locked.
+	t.Run("NoStateLock", func(t *testing.T) {
+		lock, err := GetLock(db.DefaultContext, packageNameUnlocked, user.ID)
+		require.Nil(t, lock)
+		require.Error(t, err)
+		require.IsType(t, ErrStateLockNotExist{}, err)
+		expectedError := fmt.Sprintf("package %s is not locked [OwnerID: %d]", packageNameUnlocked, user.ID)
+		require.Errorf(t, err, expectedError)
+	})
+
+	// Get the state lock from the database.
+	t.Run("GetStateLock", func(t *testing.T) {
+		lock, err := GetLock(db.DefaultContext, packageNameLocked, user.ID)
+		require.NoError(t, err)
+		assert.Equal(t, packageNameLocked, lock.PackageName)
+		assert.Equal(t, user.ID, lock.OwnerID)
+		assert.Equal(t, lockIDLocked, lock.LockID)
+		assert.Equal(t, extraInfo, lock.Info)
+		assert.Equal(t, user.Name, lock.UserName)
+		assert.Equal(t, clientVersion, lock.ClientVersion)
+	})
+}
+
+func TestLock(t *testing.T) {
+	require.NoError(t, unittest.PrepareTestDatabase())
+	defer test.MockVariableValue(&setting.Database.IterateBufferSize, 1)()
+
+	user := unittest.AssertExistsAndLoadBean(t, &user_model.User{ID: 2})
+
+	packageNameNonExisting := "non-existing-package"
+	clientVersion := "OpenTofu v1.10.6"
+	extraInfo := "Dummy extra info"
+
+	packageNameUnlocked := "opentofu-state-unlocked"
+	preparePackage(t, user, packageNameUnlocked)
+	lockIDUnlocked := "f0f34926-f710-44ec-9fe0-1605603ebf4b"
+
+	packageNameLocked := "opentofu-state-locked"
+	preparePackage(t, user, packageNameLocked)
+	lockIDLocked := "110e9066-29ca-49b6-b507-87c58bd1177b"
+	prepareStateLock(t, &StateLock{
+		PackageName:   packageNameLocked,
+		OwnerID:       user.ID,
+		LockID:        lockIDLocked,
+		Info:          extraInfo,
+		UserName:      user.Name,
+		ClientVersion: clientVersion,
+	})
+
+	// Try to lock a non-existing package.
+	//
+	// Locking a non-existing package should not produce any error as both OpenTofu
+	// and Terraform send a lock request when uploading a state file for the very
+	// first time.
+	t.Run("NoPackage", func(t *testing.T) {
+		lock, err := Lock(db.DefaultContext, &StateLock{
+			PackageName:   packageNameNonExisting,
+			OwnerID:       user.ID,
+			LockID:        lockIDUnlocked,
+			Info:          extraInfo,
+			UserName:      user.Name,
+			ClientVersion: clientVersion,
+		})
+		require.NoError(t, err)
+		assert.Equal(t, packageNameNonExisting, lock.PackageName)
+		assert.Equal(t, user.ID, lock.OwnerID)
+		assert.Equal(t, lockIDUnlocked, lock.LockID)
+		assert.Equal(t, extraInfo, lock.Info)
+		assert.Equal(t, user.Name, lock.UserName)
+		assert.Equal(t, clientVersion, lock.ClientVersion)
+	})
+
+	// Try to lock an already-locked package.
+	t.Run("AlreadyLocked", func(t *testing.T) {
+		lock, err := Lock(db.DefaultContext, &StateLock{
+			PackageName: packageNameLocked,
+			OwnerID:     user.ID,
+			LockID:      "dummy-lock-id",
+		})
+		require.Error(t, err)
+		require.IsType(t, ErrStateLockAlreadyExist{}, err)
+		expectedError := fmt.Sprintf("package %s is already locked [OwnerID: %d]", packageNameLocked, user.ID)
+		require.Errorf(t, err, expectedError)
+
+		assert.Equal(t, packageNameLocked, lock.PackageName)
+		assert.Equal(t, user.ID, lock.OwnerID)
+		assert.Equal(t, lockIDLocked, lock.LockID)
+		assert.Equal(t, extraInfo, lock.Info)
+		assert.Equal(t, user.Name, lock.UserName)
+		assert.Equal(t, clientVersion, lock.ClientVersion)
+	})
+
+	// Lock an OpenTofu/Terraform state file/package.
+	t.Run("LockState", func(t *testing.T) {
+		lock, err := Lock(db.DefaultContext, &StateLock{
+			PackageName:   packageNameUnlocked,
+			OwnerID:       user.ID,
+			LockID:        lockIDUnlocked,
+			Info:          extraInfo,
+			UserName:      user.Name,
+			ClientVersion: clientVersion,
+		})
+		require.NoError(t, err)
+		assert.Equal(t, packageNameUnlocked, lock.PackageName)
+		assert.Equal(t, user.ID, lock.OwnerID)
+		assert.Equal(t, lockIDUnlocked, lock.LockID)
+		assert.Equal(t, extraInfo, lock.Info)
+		assert.Equal(t, user.Name, lock.UserName)
+		assert.Equal(t, clientVersion, lock.ClientVersion)
+	})
+}
+
+func TestUnlock(t *testing.T) {
+	require.NoError(t, unittest.PrepareTestDatabase())
+	defer test.MockVariableValue(&setting.Database.IterateBufferSize, 1)()
+
+	user := unittest.AssertExistsAndLoadBean(t, &user_model.User{ID: 2})
+
+	packageNameUnlocked := "opentofu-state-unlocked"
+	preparePackage(t, user, packageNameUnlocked)
+	lockIDUnlocked := "f0f34926-f710-44ec-9fe0-1605603ebf4b"
+
+	packageNameLocked := "opentofu-state-locked"
+	preparePackage(t, user, packageNameLocked)
+	lockIDLocked := "110e9066-29ca-49b6-b507-87c58bd1177b"
+	prepareStateLock(t, &StateLock{
+		PackageName: packageNameLocked,
+		OwnerID:     user.ID,
+		LockID:      lockIDLocked,
+	})
+
+	// Try to unlock of a non-existing package.
+	t.Run("NoPackage", func(t *testing.T) {
+		err := Unlock(db.DefaultContext, "non-existing-package", user.ID, lockIDLocked)
+		require.Error(t, err)
+		require.IsType(t, ErrStateLockNotExist{}, err)
+		expectedError := fmt.Sprintf("package %s is not locked [OwnerID: %d]", "non-existing-package", user.ID)
+		require.Errorf(t, err, expectedError)
+	})
+
+	// Try to unlock an already unlocked package.
+	t.Run("NotLocked", func(t *testing.T) {
+		err := Unlock(db.DefaultContext, packageNameUnlocked, user.ID, lockIDUnlocked)
+		require.Error(t, err)
+		require.IsType(t, ErrStateLockNotExist{}, err)
+		expectedError := fmt.Sprintf("package %s is not locked [OwnerID: %d]", packageNameUnlocked, user.ID)
+		require.Errorf(t, err, expectedError)
+	})
+
+	// Try to unlock a package with an invalid lock ID.
+	t.Run("InvalidLockID", func(t *testing.T) {
+		err := Unlock(db.DefaultContext, packageNameLocked, user.ID, "invalid-lock-id")
+		require.Error(t, err)
+		require.IsType(t, ErrInvalidLockID{}, err)
+		expectedError := fmt.Sprintf("lock ID %s is invalid [PackageName: %s, OwnerID: %d]", "invalid-lock-id", packageNameLocked, user.ID)
+		require.Errorf(t, err, expectedError)
+	})
+
+	// Unlock an OpenTofu/Terraform state file/package.
+	t.Run("UnlockState", func(t *testing.T) {
+		err := Unlock(db.DefaultContext, packageNameLocked, user.ID, lockIDLocked)
+		require.NoError(t, err)
+	})
+}
