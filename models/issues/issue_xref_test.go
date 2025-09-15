20e9df256529f666f3f45978d3c1efad72b8a52b
Stephen Michel <git@smichel.me>
2025-09-15 11:59:48 -0400

    Add pseudocode tests

--- models/issues/issue_xref_test.go
+++ models/issues/issue_xref_test.go
@@ -91,6 +91,58 @@ func TestXRef_NeuterCrossReferences(t *testing.T) {
 	assert.Equal(t, references.XRefActionNeutered, ref.RefAction)
 }
 
+/* TODO: Turn this into real testing code
+func TestXRef_GetIssueByCrossReference(t *testing.T) {
+	require.NoError(t, unittest.PrepareTestDatabase())
+
+	// Issues to test against
+	itarget1 := testCreateIssue(t, 1, 2, "title1", "content1", false)
+	itarget2 := testCreateIssue(t, 1, 2, "title1", "content1", false)
+
+	// Issue to test from
+	sameRepoIssue := testCreateIssue(t, 1, 2, "title3", "content3", false)
+	xRepoIssue := testCreateIssue(t, 3, 3, "title4", "content4", false)
+
+	// Possible lookup strings
+	shortRef1 := fmt.Sprintf("#%d", itarget1.Index)
+	fullRef1 := "org1/repo2" + shortRef1
+	shortRef2 := fmt.Sprintf("#%d", itarget2.Index)
+	fullRef2 := "org1/repo2" + shortRef2
+
+	// Lookup using short reference in same repo
+  result, err := sameRepoIssue.GetIssueByCrossReference(??, ??, shortRef1);
+	assert(err == nil)
+	assert(result == itarget1)
+
+	// Lookup using full reference in same repo
+  result, err := sameRepoIssue.GetIssueByCrossReference(??, ??, fullRef1);
+	assert(err == nil)
+	assert(result == itarget1)
+
+	// Fails if text contains multiple references
+	multiRef := shortRef1 + " " + fullRef2
+  result, err := sameRepoIssue.GetIssueByCrossReference(??, ??, multiRef);
+	assert(err != nil)
+	assert(result == nil)
+
+	// Fails if text contains action keywords
+	closesRef = "closes " + shortRef1
+  result, err := sameRepoIssue.GetIssueByCrossReference(??, ??, closesRef);
+	assert(err != nil)
+	assert(result == nil)
+
+	// Cross-reference lookup by admin
+  result, err := xRepoIssue.GetIssueByCrossReference(??, ??, fullRef1);
+	assert(err == nil)
+	assert(result == itarget1)
+
+	// Cross-reference lookup with no permission
+  result, err := xRepoIssue.GetIssueByCrossReference(??, ??, fullRef1);
+	assert(err != nil)
+	assert(result == nil)
+}
+*/
+
 func TestXRef_ResolveCrossReferences(t *testing.T) {
 	require.NoError(t, unittest.PrepareTestDatabase())
 
