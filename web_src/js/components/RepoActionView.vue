40001069dcfad1ebc724aeff29799c2b5efb509f
Tom Hubrecht <tom@hubrecht.ovh>
2025-10-02 16:07:39 +0200

    fix(ui): Don't use the subtle color for log text
    This improves the contrast from the background

--- web_src/js/components/RepoActionView.vue
+++ web_src/js/components/RepoActionView.vue
@@ -1036,6 +1036,7 @@ export default {
   white-space: break-spaces;
   margin-left: 10px;
   overflow-wrap: anywhere;
+  color: var(--color-console-fg);
 }
 
 /* selectors here are intentionally exact to only match fullscreen */
