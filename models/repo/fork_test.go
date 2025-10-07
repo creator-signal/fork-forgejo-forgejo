66bf295ce5f8cf6bfdb8b583ddd929798db96212
Leni Kadali <lenikadali@noreply.codeberg.org>
2025-10-07 23:59:27 +0300

    Add test reproducing the issue root cause
    Added a test that reproduces the issue's root cause
    as mentioned here
    https://codeberg.org/forgejo/forgejo/issues/6751#issuecomment-2680995

--- models/repo/fork_test.go
+++ models/repo/fork_test.go
@@ -109,3 +109,19 @@ func TestGetUserForkLaxWithTwoChoices(t *testing.T) {
 	assert.Equal(t, int64(65), repo65.ID)
 	assert.Equal(t, int64(11), repo65.ForkID)
 }
+
+func TestGetOrgUserHasForkedRepo(t *testing.T) {
+	require.NoError(t, unittest.PrepareTestDatabase())
+
+	// orgUser3 has repo 65 forked from repo63
+	repo63, err := repo_model.GetRepositoryByID(db.DefaultContext, 63)
+	require.NoError(t, err)
+	assert.NotNil(t, repo63)
+
+	require.True(t, repo_model.HasForkedRepo(db.DefaultContext, 3, repo63.ID))
+	repo65, err := repo_model.GetUserFork(db.DefaultContext, repo63.ID, 3)
+	require.NoError(t, err)
+	assert.NotNil(t, repo65)
+	assert.Equal(t, int64(64), repo65.ID)
+	assert.Equal(t, int64(63), repo65.ForkID)
+}
