25d7a79afae6d93260b8aebd5e976e85003bb26a
Leni Kadali <lenikadali@noreply.codeberg.org>
2025-10-08 22:15:14 +0300

    Remove IsDeleted and update tests, code
    Removes the IsDeleted column and updates the tests
    and code not to use it anymore.

---
+++ models/forgejo_migrations/v44.go
@@ -0,0 +1,41 @@
+// Copyright 2025 The Forgejo Authors. All rights reserved.
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+package forgejo_migrations
+
+import (
+	"forgejo.org/models/migrations/base"
+	"forgejo.org/modules/timeutil"
+
+	"xorm.io/xorm"
+)
+
+type Action struct {
+	UserID      int64 // Receiver user id.
+	ActUserID   int64 // Action user id.
+	RepoID      int64
+	IsDeleted   bool               `xorm:"NOT NULL DEFAULT false"`
+	IsPrivate   bool               `xorm:"NOT NULL DEFAULT false"`
+	CreatedUnix timeutil.TimeStamp `xorm:"created"`
+}
+
+// TableName sets the name of this table
+func (a *Action) TableName() string {
+	return "action"
+}
+
+func RemoveIsDeletedColumnFromActivityActionTable(x *xorm.Engine) error {
+	if err := x.Sync(&Action{}); err != nil {
+		return err
+	}
+
+	sess := x.NewSession()
+	defer sess.Close()
+	if err := sess.Begin(); err != nil {
+		return err
+	}
+	if err := base.DropTableColumns(sess, "action", "is_deleted"); err != nil {
+		return err
+	}
+	return sess.Commit()
+}
