2fd4dfc9b3b20f4c17a62ec83b46c1ec50f035d9
Jordan Atwood <nightfirecat@nightfirec.at>
2025-10-18 15:33:08 -0700

    chore(ui): remove unused variable in notification.ts

--- web_src/js/features/notification.ts
+++ web_src/js/features/notification.ts
@@ -80,7 +80,7 @@ export function initNotificationCount() {
         return;
       }
       if (event.data.type === 'notification-count') {
-        const _promise = receiveUpdateCount(event.data);
+        receiveUpdateCount(event.data);
       } else if (event.data.type === 'no-event-source') {
         // browser doesn't support EventSource, falling back to periodic poller
         if (!usingPeriodicPoller) startPeriodicPoller(notificationSettings.MinTimeout);
