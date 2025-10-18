31bd7c343fab54a744bf27eba748deff72e616e6
0ko <0ko@noreply.codeberg.org>
2025-10-18 08:52:37 +0500

    make danger button font weight medium
    maybe it should be applied to all buttons but currently dangerous buttons are the example of this being really needed

--- tests/e2e/buttons.test.e2e.ts
+++ tests/e2e/buttons.test.e2e.ts
@@ -33,23 +33,40 @@ test('Buttons and other controls have consistent height', async ({page}) => {
   expect(buttonHeight).toBe(purgeButtonHeight);
 });
 
-test('Button colors', async ({page}) => {
+test('Button visuals', async ({page}) => {
+  async function getButtonProperties(page, selector) {
+    return await page.locator(selector).evaluate((el) => {
+      // In Firefox getComputedStyle is undefined if returned from evaluate
+      const s = getComputedStyle(el);
+      return {
+        backgroundColor: s.backgroundColor,
+        fontWeight: s.fontWeight,
+      };
+    });
+  }
+
   // const context = await browser.newContext({javaScriptEnabled: false});
   // const page = await context.newPage();
   const response = await page.goto('/devtest/buttons');
   expect(response?.status()).toBe(200);
 
   const transparent = 'rgba(0, 0, 0, 0)';
-  const primaryBg = await page.locator('button.primary').evaluate((el) => getComputedStyle(el).backgroundColor);
-  const secondaryBg = await page.locator('button.secondary').evaluate((el) => getComputedStyle(el).backgroundColor);
-  const dangerBg = await page.locator('button.danger').evaluate((el) => getComputedStyle(el).backgroundColor);
+
+  const primary = await getButtonProperties(page, 'button.primary')
+  const secondary = await getButtonProperties(page, 'button.secondary')
+  const danger = await getButtonProperties(page, 'button.danger')
 
   // Evaluate that all buttons have background-color specified
-  expect(primaryBg).not.toBe(transparent);
-  expect(secondaryBg).not.toBe(transparent);
-  expect(dangerBg).not.toBe(transparent);
+  expect(primary.backgroundColor).not.toBe(transparent);
+  expect(secondary.backgroundColor).not.toBe(transparent);
+  expect(danger.backgroundColor).not.toBe(transparent);
 
   // Evaluate that their background-colors are different
-  expect(primaryBg).not.toBe(secondaryBg);
-  expect(primaryBg).not.toBe(dangerBg);
+  expect(primary.backgroundColor).not.toBe(secondary.backgroundColor);
+  expect(primary.backgroundColor).not.toBe(danger.backgroundColor);
+
+  // Evaluate font weights
+  expect(primary.fontWeight).toBe("400");
+  expect(secondary.fontWeight).toBe("400");
+  expect(danger.fontWeight).toBe("500");
 });
