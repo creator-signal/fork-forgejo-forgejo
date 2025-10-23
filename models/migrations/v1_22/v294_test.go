c847c01a7df23972d880db5c87fdee5b1eb8168e
Mathieu Fenniak <mathieu@fenniak.net>
2025-10-23 08:58:16 -0600

    fix lint errors

--- models/migrations/v1_22/v294_test.go
+++ models/migrations/v1_22/v294_test.go
@@ -45,7 +45,8 @@ func Test_AddUniqueIndexForProjectIssue(t *testing.T) {
 	for _, index := range tables[0].Indexes {
 		if index.Type == schemas.UniqueType {
 			found = true
-			slices.Equal(index.Cols, []string{"project_id", "issue_id"})
+			slices.Sort(index.Cols)
+			assert.Equal(t, []string{"issue_id", "project_id"}, index.Cols)
 			break
 		}
 	}
