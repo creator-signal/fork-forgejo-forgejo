edaca15ae6d847fc4d9ecc2d8115aff5f796abd8
0ko <0ko@noreply.codeberg.org>
2025-10-24 23:27:59 +0500

    add db migration (seems too simple)

---
+++ models/forgejo_migrations/v14a_add-avatar_svg-user.go
@@ -0,0 +1,23 @@
+// Copyright 2025 The Forgejo Authors. All rights reserved.
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+package forgejo_migrations
+
+import (
+	"xorm.io/xorm"
+)
+
+func init() {
+	registerMigration(&Migration{
+		Description: "add avatar_svg field to user table",
+		Upgrade:     addAvatarSvgToUser,
+	})
+}
+
+func addAvatarSvgToUser(x *xorm.Engine) error {
+	type User struct {
+		AvatarSVG int64 `xorm:"index"`
+	}
+	_, err := x.SyncWithOptions(xorm.SyncOptions{IgnoreDropIndices: true}, new(User))
+	return err
+}
