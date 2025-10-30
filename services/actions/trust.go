a7b395232ccdcb1825d05aab64953135fd7b0fad
Earl Warren <contact@earl-warren.org>
2025-10-30 15:58:51 +0100

    feat: trust management functions runs created from a pull request
    - UpdateTrustedWithPullRequest - cancels or approves runs and keep
      track of posters that are to always be trusted
    - LoadPullRequestPosterIsTrustedWithActions - logic to determine
      if the poster is to be implicitly trusted (e.g. the admin of the
      instance), explicitly trusted (i.e. it is in the ActionUser table)
      or not at all.
    - PullRequestCancel & PullRequestApprove will either cancel or approve
      all runs of a given pull request.
    - RevokeTrustByRepoIDAndPosterID is almost the same as
      PullRequestCancel except it operates as if revoking all pull
      requests of a given poster, cancelling ongoing jobs. This is
      expected to be used when blocking a user.
    - SetRunTrustForPullRequest helper to set the fields of ActionRun
    - CleanupActionUser - get rid of unused trust records

---
+++ services/actions/trust.go
@@ -0,0 +1,263 @@
+// Copyright 2025 The Forgejo Authors. All rights reserved.
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+package actions
+
+import (
+	"context"
+	"fmt"
+
+	actions_model "forgejo.org/models/actions"
+	"forgejo.org/models/db"
+	issues_model "forgejo.org/models/issues"
+	access_model "forgejo.org/models/perm/access"
+	unit_model "forgejo.org/models/unit"
+	actions_module "forgejo.org/modules/actions"
+	"forgejo.org/modules/log"
+)
+
+const (
+	UserTrustDenied   = "deny"
+	UserAlwaysTrusted = "always"
+	UserTrustedOnce   = "once"
+	UserTrustRevoked  = "revoke"
+)
+
+func CleanupActionUser(ctx context.Context) error {
+	return actions_model.RevokeInactiveActionUser(ctx)
+}
+
+func loadPullRequestAttributes(ctx context.Context, pr *issues_model.PullRequest) error {
+	if err := pr.LoadIssue(ctx); err != nil {
+		return err
+	}
+
+	if err := pr.Issue.LoadRepo(ctx); err != nil {
+		return err
+	}
+
+	return pr.Issue.LoadPoster(ctx)
+}
+
+func UpdateTrustedWithPullRequest(ctx context.Context, doerID int64, pr *issues_model.PullRequest, trusted string) error {
+	if err := updateTrusted(ctx, pr, trusted); err != nil {
+		return err
+	}
+
+	switch trusted {
+	case UserAlwaysTrusted, UserTrustedOnce:
+		return PullRequestApprove(ctx, doerID, pr)
+	case UserTrustRevoked, UserTrustDenied:
+		return PullRequestCancel(ctx, pr)
+	default:
+		return fmt.Errorf("UpdateTrustedWithPullRequest: unknown trust %v", trusted)
+	}
+}
+
+func updateTrusted(ctx context.Context, pr *issues_model.PullRequest, trusted string) error {
+	switch trusted {
+	case UserTrustedOnce:
+		return nil
+	case UserAlwaysTrusted:
+		if err := loadPullRequestAttributes(ctx, pr); err != nil {
+			return err
+		}
+		return actions_model.InsertActionUser(ctx, &actions_model.ActionUser{
+			UserID:                  pr.Issue.Poster.ID,
+			RepoID:                  pr.Issue.Repo.ID,
+			TrustedWithPullRequests: true,
+		})
+	case UserTrustDenied:
+		return nil
+	case UserTrustRevoked:
+		if err := loadPullRequestAttributes(ctx, pr); err != nil {
+			return err
+		}
+		return actions_model.DeleteActionUserByUserIDAndRepoID(ctx, pr.Issue.Poster.ID, pr.Issue.Repo.ID)
+	}
+	return nil
+}
+
+func SetRunTrustForPullRequest(ctx context.Context, run *actions_model.ActionRun, pr *issues_model.PullRequest) error {
+	if pr == nil {
+		return nil
+	}
+
+	if err := pr.LoadIssue(ctx); err != nil {
+		return err
+	}
+
+	run.IsForkPullRequest = pr.IsForkPullRequest()
+	run.PullRequestPosterID = pr.Issue.PosterID
+	run.PullRequestID = pr.ID
+
+	needApproval, err := ifNeedApproval(ctx, run, pr)
+	if err != nil {
+		return err
+	}
+
+	if needApproval {
+		run.NeedApproval = needApproval
+	}
+
+	return nil
+}
+
+func ifNeedApproval(ctx context.Context, run *actions_model.ActionRun, pr *issues_model.PullRequest) (bool, error) {
+	// 1. don't need approval if it's not a fork PR
+	// 2. don't need approval if the event is `pull_request_target` since the workflow will run in the context of base branch
+	// 		see https://docs.github.com/en/actions/managing-workflow-runs/approving-workflow-runs-from-public-forks#about-workflow-runs-from-public-forks
+	if !run.IsForkPullRequest || run.TriggerEvent == actions_module.GithubEventPullRequestTarget {
+		return false, nil
+	}
+
+	trusted, err := LoadPullRequestPosterIsTrustedWithActions(ctx, pr)
+	if err != nil {
+		return false, err
+	}
+
+	return trusted == PosterIsNotTrustedWithActions, nil
+}
+
+const (
+	PosterIsNotTrustedWithActions        = "no"
+	PosterIsExplicitlyTrustedWithActions = "explicitly"
+	PosterIsImplicitlyTrustedWithActions = "implicitly"
+)
+
+func LoadPullRequestPosterIsTrustedWithActions(ctx context.Context, pr *issues_model.PullRequest) (string, error) {
+	if err := loadPullRequestAttributes(ctx, pr); err != nil {
+		return "", err
+	}
+
+	return posterIsTrustedWithPullRequest(ctx, pr)
+}
+
+func posterIsTrustedWithPullRequest(ctx context.Context, pr *issues_model.PullRequest) (string, error) {
+	implicitlyTrusted, err := posterIsImplicitlyTrustedWithPullRequest(ctx, pr)
+	if err != nil {
+		return "", err
+	}
+	if implicitlyTrusted {
+		log.Trace("%s is implicitly trusted to run actions in repository %s", pr.Issue.Poster, pr.Issue.Repo)
+		return PosterIsImplicitlyTrustedWithActions, nil
+	}
+
+	explicitlyTrusted, err := posterIsExplicitlyTrustedWithPullRequest(ctx, pr)
+	if err != nil {
+		return "", err
+	}
+	if explicitlyTrusted {
+		log.Trace("%s is explicitly trusted to run actions in repository %s", pr.Issue.Poster, pr.Issue.Repo)
+		return PosterIsExplicitlyTrustedWithActions, nil
+	}
+
+	log.Trace("%s is not trusted to run actions in repository %s", pr.Issue.Poster, pr.Issue.Repo)
+	return PosterIsNotTrustedWithActions, nil
+}
+
+func posterIsImplicitlyTrustedWithPullRequest(ctx context.Context, pr *issues_model.PullRequest) (bool, error) {
+	// users that are trusted to create a pull request that is not from a fork
+	// are also implicitly trusted to run workflows
+	if !pr.IsForkPullRequest() {
+		log.Trace("a pull request that is not from a fork nor AGit is implicitly trusted to run actions")
+		return true, nil
+	}
+
+	// users with write permission to the actions unit are trusted to
+	// run actions
+	permission, err := access_model.GetUserRepoPermission(ctx, pr.Issue.Repo, pr.Issue.Poster)
+	if err != nil {
+		return false, err
+	}
+	if permission.CanWrite(unit_model.TypeActions) {
+		log.Trace("%s is a member of a team with write permissions to the Action unit on %s", pr.Issue.Poster, pr.Issue.Repo)
+		return true, nil
+	}
+
+	return false, nil
+}
+
+func posterIsExplicitlyTrustedWithPullRequest(ctx context.Context, pr *issues_model.PullRequest) (bool, error) {
+	// there is no need to check if the user is blocked because it is not
+	// allowed to create a pull request
+	if pr.Issue.Poster.IsRestricted {
+		log.Trace("%v is restricted and cannot be trusted with pull requests", pr.Issue.Poster)
+		return false, nil
+	}
+
+	user, err := actions_model.GetActionUserByUserIDAndRepoID(ctx, pr.Issue.Poster.ID, pr.Issue.Repo.ID)
+	if err != nil {
+		log.Trace("%v is not expliclty trusted with pull requests on repository %v", pr.Issue.Poster, pr.Issue.Repo)
+		if actions_model.IsErrUserNotExist(err) {
+			return false, nil
+		}
+		return false, err
+	}
+
+	log.Trace("%v is expliclty trusted with pull requests on repository %v", pr.Issue.Poster, pr.Issue.Repo)
+	return user.TrustedWithPullRequests, nil
+}
+
+func RevokeTrustByRepoIDAndPosterID(ctx context.Context, repoID, posterID int64) error {
+	if err := actions_model.DeleteActionUserByUserIDAndRepoID(ctx, posterID, repoID); err != nil {
+		return err
+	}
+
+	runs, err := actions_model.GetRunsNotDoneByRepoIDAndPullRequestPosterID(ctx, repoID, posterID)
+	if err != nil {
+		return err
+	}
+
+	for _, run := range runs {
+		if err := CancelRun(ctx, run); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func PullRequestCancel(ctx context.Context, pr *issues_model.PullRequest) error {
+	runs, err := actions_model.GetRunsNotDoneByRepoIDAndPullRequestID(ctx, pr.Issue.RepoID, pr.ID)
+	if err != nil {
+		return err
+	}
+
+	for _, run := range runs {
+		if err := CancelRun(ctx, run); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func PullRequestApprove(ctx context.Context, doerID int64, pr *issues_model.PullRequest) error {
+	runs, err := actions_model.GetRunsThatNeedApproval(ctx, pr.Issue.RepoID, pr.ID)
+	if err != nil {
+		return err
+	}
+
+	for _, run := range runs {
+		if err := db.WithTx(ctx, func(ctx context.Context) error {
+			jobs, err := actions_model.GetRunJobsByRunID(ctx, run.ID)
+			if err != nil {
+				return err
+			}
+			for _, job := range jobs {
+				if len(job.Needs) == 0 && job.Status.IsBlocked() {
+					job.Status = actions_model.StatusWaiting
+					_, err := UpdateRunJob(ctx, job, nil, "status")
+					if err != nil {
+						return err
+					}
+				}
+			}
+			CreateCommitStatus(ctx, jobs...)
+
+			return actions_model.UpdateRunApprovalByID(ctx, run.ID, false, doerID)
+		}); err != nil {
+			return err
+		}
+	}
+	return nil
+}
---
+++ services/actions/trust_test.go
@@ -0,0 +1,328 @@
+// Copyright 2025 The Forgejo Authors. All rights reserved.
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+package actions
+
+import (
+	"testing"
+
+	actions_model "forgejo.org/models/actions"
+	issues_model "forgejo.org/models/issues"
+	"forgejo.org/models/unittest"
+	actions_module "forgejo.org/modules/actions"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestActionsTrust_ChangeStatus(t *testing.T) {
+	require.NoError(t, unittest.PrepareTestDatabase())
+
+	repoID := int64(10)
+	pullRequestPosterID := int64(30)
+
+	runDone := &actions_model.ActionRun{
+		RepoID:              repoID,
+		PullRequestPosterID: pullRequestPosterID,
+		Status:              actions_model.StatusSuccess,
+	}
+	require.NoError(t, actions_model.InsertRun(t.Context(), runDone, nil))
+
+	runNotByPoster := &actions_model.ActionRun{
+		RepoID:              repoID,
+		PullRequestPosterID: 43243,
+		Status:              actions_model.StatusRunning,
+	}
+	require.NoError(t, actions_model.InsertRun(t.Context(), runNotByPoster, nil))
+
+	runNotInTheSameRepository := &actions_model.ActionRun{
+		RepoID:              5,
+		PullRequestPosterID: pullRequestPosterID,
+		Status:              actions_model.StatusSuccess,
+	}
+	require.NoError(t, actions_model.InsertRun(t.Context(), runNotInTheSameRepository, nil))
+
+	t.Run("RevokeTrustByRepoIDAndPosterID", func(t *testing.T) {
+		singleWorkflows, err := actions_module.JobParser([]byte(`
+jobs:
+  job:
+    runs-on: docker
+    steps:
+      - run: echo OK
+`))
+		require.NoError(t, err)
+		require.Len(t, singleWorkflows, 1)
+		runNotDone := &actions_model.ActionRun{
+			TriggerUserID:       2,
+			RepoID:              repoID,
+			Status:              actions_model.StatusWaiting,
+			PullRequestPosterID: pullRequestPosterID,
+		}
+		require.NoError(t, actions_model.InsertRun(t.Context(), runNotDone, singleWorkflows))
+		require.NoError(t, actions_model.InsertActionUser(t.Context(), &actions_model.ActionUser{
+			UserID:                  pullRequestPosterID,
+			RepoID:                  repoID,
+			TrustedWithPullRequests: true,
+		}))
+
+		previousCancelledCount := unittest.GetCount(t, &actions_model.ActionRun{Status: actions_model.StatusCancelled})
+		_, err = actions_model.GetActionUserByUserIDAndRepoID(t.Context(), pullRequestPosterID, repoID)
+		require.NoError(t, err)
+
+		require.NoError(t, RevokeTrustByRepoIDAndPosterID(t.Context(), repoID, pullRequestPosterID))
+
+		_, err = actions_model.GetActionUserByUserIDAndRepoID(t.Context(), pullRequestPosterID, repoID)
+		assert.True(t, actions_model.IsErrUserNotExist(err))
+		currentCancelledCount := unittest.GetCount(t, &actions_model.ActionRun{Status: actions_model.StatusCancelled})
+		assert.Equal(t, previousCancelledCount+1, currentCancelledCount)
+		run := unittest.AssertExistsAndLoadBean(t, &actions_model.ActionRun{ID: runNotDone.ID})
+		assert.Equal(t, actions_model.StatusCancelled.String(), run.Status.String())
+	})
+
+	createPullRequestRun := func(t *testing.T, pullRequestID, repoID int64) *actions_model.ActionRun {
+		t.Helper()
+		singleWorkflows, err := actions_module.JobParser([]byte(`
+jobs:
+  job:
+    runs-on: docker
+    steps:
+      - run: echo OK
+`))
+		require.NoError(t, err)
+		require.Len(t, singleWorkflows, 1)
+		runNotApproved := &actions_model.ActionRun{
+			TriggerUserID:       2,
+			RepoID:              repoID,
+			Status:              actions_model.StatusWaiting,
+			NeedApproval:        true,
+			PullRequestID:       pullRequestID,
+			PullRequestPosterID: pullRequestPosterID,
+		}
+		require.NoError(t, actions_model.InsertRun(t.Context(), runNotApproved, singleWorkflows))
+		return runNotApproved
+	}
+
+	t.Run("PullRequestCancel", func(t *testing.T) {
+		pullRequestID := int64(485)
+		runNotApproved := createPullRequestRun(t, pullRequestID, repoID)
+
+		previousCancelledCount := unittest.GetCount(t, &actions_model.ActionRun{Status: actions_model.StatusCancelled})
+
+		require.NoError(t, PullRequestCancel(t.Context(), &issues_model.PullRequest{
+			ID: pullRequestID,
+			Issue: &issues_model.Issue{
+				RepoID: repoID,
+			},
+		}))
+
+		currentCancelledCount := unittest.GetCount(t, &actions_model.ActionRun{Status: actions_model.StatusCancelled})
+		assert.Equal(t, previousCancelledCount+1, currentCancelledCount)
+		run := unittest.AssertExistsAndLoadBean(t, &actions_model.ActionRun{ID: runNotApproved.ID})
+		assert.Equal(t, actions_model.StatusCancelled.String(), run.Status.String())
+	})
+
+	t.Run("UpdateTrustedWithPullRequest not trusted", func(t *testing.T) {
+		pullRequestID := int64(485)
+		runNotApproved := createPullRequestRun(t, pullRequestID, repoID)
+
+		previousCancelledCount := unittest.GetCount(t, &actions_model.ActionRun{Status: actions_model.StatusCancelled})
+
+		require.NoError(t, UpdateTrustedWithPullRequest(t.Context(), 0, &issues_model.PullRequest{
+			ID: pullRequestID,
+			Issue: &issues_model.Issue{
+				RepoID: repoID,
+			},
+		}, UserTrustDenied))
+
+		currentCancelledCount := unittest.GetCount(t, &actions_model.ActionRun{Status: actions_model.StatusCancelled})
+		assert.Equal(t, previousCancelledCount+1, currentCancelledCount)
+		run := unittest.AssertExistsAndLoadBean(t, &actions_model.ActionRun{ID: runNotApproved.ID})
+		assert.Equal(t, actions_model.StatusCancelled.String(), run.Status.String())
+	})
+
+	t.Run("PullRequestApprove", func(t *testing.T) {
+		pullRequestID := int64(534)
+		runNotApproved := createPullRequestRun(t, pullRequestID, repoID)
+
+		previousWaitingCount := unittest.GetCount(t, &actions_model.ActionRunJob{Status: actions_model.StatusWaiting})
+
+		doerID := int64(84322)
+		require.NoError(t, PullRequestApprove(t.Context(), doerID, &issues_model.PullRequest{
+			ID: pullRequestID,
+			Issue: &issues_model.Issue{
+				RepoID: repoID,
+			},
+		}))
+
+		currentWaitingCount := unittest.GetCount(t, &actions_model.ActionRunJob{Status: actions_model.StatusWaiting})
+		assert.Equal(t, previousWaitingCount+1, currentWaitingCount)
+		run := unittest.AssertExistsAndLoadBean(t, &actions_model.ActionRun{ID: runNotApproved.ID})
+		assert.Equal(t, actions_model.StatusWaiting.String(), run.Status.String())
+		assert.Equal(t, doerID, run.ApprovedBy)
+		assert.False(t, run.NeedApproval)
+	})
+
+	t.Run("UpdateTrustedWithPullRequest trusted", func(t *testing.T) {
+		pullRequestID := int64(534)
+		runNotApproved := createPullRequestRun(t, pullRequestID, repoID)
+
+		previousWaitingCount := unittest.GetCount(t, &actions_model.ActionRunJob{Status: actions_model.StatusWaiting})
+
+		doerID := int64(84322)
+		require.NoError(t, UpdateTrustedWithPullRequest(t.Context(), doerID, &issues_model.PullRequest{
+			ID: pullRequestID,
+			Issue: &issues_model.Issue{
+				RepoID: repoID,
+			},
+		}, UserTrustedOnce))
+
+		currentWaitingCount := unittest.GetCount(t, &actions_model.ActionRunJob{Status: actions_model.StatusWaiting})
+		assert.Equal(t, previousWaitingCount+1, currentWaitingCount)
+		run := unittest.AssertExistsAndLoadBean(t, &actions_model.ActionRun{ID: runNotApproved.ID})
+		assert.Equal(t, actions_model.StatusWaiting.String(), run.Status.String())
+		assert.Equal(t, doerID, run.ApprovedBy)
+		assert.False(t, run.NeedApproval)
+	})
+}
+
+func TestActionsTrust_LoadPullRequest(t *testing.T) {
+	defer unittest.OverrideFixtures("services/actions/TestActionsTrust_LoadPullRequest")()
+	require.NoError(t, unittest.PrepareTestDatabase())
+
+	t.Run("implicitly trusted because the pull request is not from a fork", func(t *testing.T) {
+		pr := unittest.AssertExistsAndLoadBean(t, &issues_model.PullRequest{ID: 2000})
+		trust, err := LoadPullRequestPosterIsTrustedWithActions(t.Context(), pr)
+		require.NoError(t, err)
+		pr.LoadAttributes(t.Context())
+		require.False(t, pr.IsForkPullRequest())
+		assert.Equal(t, PosterIsImplicitlyTrustedWithActions, trust)
+	})
+
+	t.Run("implicitly trusted because the poster of a forked pull request is admin", func(t *testing.T) {
+		pr := unittest.AssertExistsAndLoadBean(t, &issues_model.PullRequest{ID: 3000})
+		trust, err := LoadPullRequestPosterIsTrustedWithActions(t.Context(), pr)
+		require.NoError(t, err)
+		pr.LoadAttributes(t.Context())
+		require.True(t, pr.IsForkPullRequest())
+		require.True(t, pr.Issue.Poster.IsAdmin)
+		assert.Equal(t, PosterIsImplicitlyTrustedWithActions, trust)
+	})
+
+	t.Run("explicitly trusted because the poster of a forked pull request is trusted", func(t *testing.T) {
+		pr := unittest.AssertExistsAndLoadBean(t, &issues_model.PullRequest{ID: 1000})
+		trust, err := LoadPullRequestPosterIsTrustedWithActions(t.Context(), pr)
+		require.NoError(t, err)
+		pr.LoadAttributes(t.Context())
+		require.True(t, pr.IsForkPullRequest())
+		_, err = actions_model.GetActionUserByUserIDAndRepoID(t.Context(), pr.Issue.PosterID, pr.Issue.RepoID)
+		require.NoError(t, err)
+		assert.Equal(t, PosterIsExplicitlyTrustedWithActions, trust)
+	})
+
+	t.Run("explicitly trusted because the poster of a forked pull request was permanently approved", func(t *testing.T) {
+		pr := unittest.AssertExistsAndLoadBean(t, &issues_model.PullRequest{ID: 1000})
+		trust, err := LoadPullRequestPosterIsTrustedWithActions(t.Context(), pr)
+		require.NoError(t, err)
+		pr.LoadAttributes(t.Context())
+		require.True(t, pr.IsForkPullRequest())
+		_, err = actions_model.GetActionUserByUserIDAndRepoID(t.Context(), pr.Issue.PosterID, pr.Issue.RepoID)
+		require.NoError(t, err)
+		assert.Equal(t, PosterIsExplicitlyTrustedWithActions, trust)
+	})
+
+	t.Run("not trusted because the poster of a forked pull request has no privileges", func(t *testing.T) {
+		pr := unittest.AssertExistsAndLoadBean(t, &issues_model.PullRequest{ID: 4000})
+		trust, err := LoadPullRequestPosterIsTrustedWithActions(t.Context(), pr)
+		require.NoError(t, err)
+		pr.LoadAttributes(t.Context())
+		require.True(t, pr.IsForkPullRequest())
+		assert.Equal(t, PosterIsNotTrustedWithActions, trust)
+	})
+
+	t.Run("not trusted because the poster of a forked pull request is restricted", func(t *testing.T) {
+		pr := unittest.AssertExistsAndLoadBean(t, &issues_model.PullRequest{ID: 5000})
+		trust, err := LoadPullRequestPosterIsTrustedWithActions(t.Context(), pr)
+		require.NoError(t, err)
+		pr.LoadAttributes(t.Context())
+		require.True(t, pr.IsForkPullRequest())
+		_, err = actions_model.GetActionUserByUserIDAndRepoID(t.Context(), pr.Issue.PosterID, pr.Issue.RepoID)
+		require.NoError(t, err)
+		require.True(t, pr.Issue.Poster.IsRestricted)
+		assert.Equal(t, PosterIsNotTrustedWithActions, trust)
+	})
+
+	t.Run("approval not needed because the run is not from a fork", func(t *testing.T) {
+		run := &actions_model.ActionRun{
+			IsForkPullRequest: false,
+		}
+		needApproval, err := ifNeedApproval(t.Context(), run, nil)
+		require.NoError(t, err)
+		assert.False(t, needApproval)
+	})
+
+	t.Run("approval not needed because the run is a pull_request_target event", func(t *testing.T) {
+		run := &actions_model.ActionRun{
+			IsForkPullRequest: true,
+			TriggerEvent:      actions_module.GithubEventPullRequestTarget,
+		}
+		needApproval, err := ifNeedApproval(t.Context(), run, nil)
+		require.NoError(t, err)
+		assert.False(t, needApproval)
+	})
+
+	t.Run("approval needed because the run is from a forked pull request and the user is not trusted", func(t *testing.T) {
+		run := &actions_model.ActionRun{
+			IsForkPullRequest: true,
+		}
+		pr := unittest.AssertExistsAndLoadBean(t, &issues_model.PullRequest{ID: 5000})
+		needApproval, err := ifNeedApproval(t.Context(), run, pr)
+		require.NoError(t, err)
+		require.True(t, pr.Issue.Poster.IsRestricted)
+		assert.True(t, needApproval)
+	})
+
+	t.Run("run for a pull request is set with trust data", func(t *testing.T) {
+		run := &actions_model.ActionRun{
+			IsForkPullRequest: true,
+		}
+		pr := unittest.AssertExistsAndLoadBean(t, &issues_model.PullRequest{ID: 5000})
+		require.NoError(t, SetRunTrustForPullRequest(t.Context(), run, nil))
+		require.NoError(t, SetRunTrustForPullRequest(t.Context(), run, pr))
+		require.True(t, pr.Issue.Poster.IsRestricted)
+		assert.True(t, run.NeedApproval)
+		assert.True(t, run.IsForkPullRequest)
+		assert.Equal(t, pr.Issue.PosterID, run.PullRequestPosterID)
+		assert.Equal(t, pr.ID, run.PullRequestID)
+	})
+}
+
+func TestActionsTrust_updateTrusted(t *testing.T) {
+	require.NoError(t, unittest.PrepareTestDatabase())
+
+	pr := unittest.AssertExistsAndLoadBean(t, &issues_model.PullRequest{ID: 1})
+	require.NoError(t, loadPullRequestAttributes(t.Context(), pr))
+
+	t.Run("UserTrustedOnce", func(t *testing.T) {
+		require.Zero(t, unittest.GetCount(t, &actions_model.ActionUser{UserID: pr.Issue.Poster.ID}))
+		require.NoError(t, updateTrusted(t.Context(), pr, UserTrustedOnce))
+		require.Zero(t, unittest.GetCount(t, &actions_model.ActionUser{UserID: pr.Issue.Poster.ID}))
+	})
+
+	t.Run("UserAlwaysTrusted", func(t *testing.T) {
+		require.Zero(t, unittest.GetCount(t, &actions_model.ActionUser{UserID: pr.Issue.Poster.ID}))
+		require.NoError(t, updateTrusted(t.Context(), pr, UserAlwaysTrusted))
+		require.Equal(t, 1, unittest.GetCount(t, &actions_model.ActionUser{UserID: pr.Issue.Poster.ID}))
+	})
+
+	t.Run("UserTrustRevoked", func(t *testing.T) {
+		require.Equal(t, 1, unittest.GetCount(t, &actions_model.ActionUser{UserID: pr.Issue.Poster.ID}))
+		require.NoError(t, updateTrusted(t.Context(), pr, UserTrustRevoked))
+		require.Zero(t, unittest.GetCount(t, &actions_model.ActionUser{UserID: pr.Issue.Poster.ID}))
+	})
+
+	t.Run("UserTrustDenied", func(t *testing.T) {
+		require.Zero(t, unittest.GetCount(t, &actions_model.ActionUser{UserID: pr.Issue.Poster.ID}))
+		require.NoError(t, updateTrusted(t.Context(), pr, UserTrustDenied))
+		require.Zero(t, unittest.GetCount(t, &actions_model.ActionUser{UserID: pr.Issue.Poster.ID}))
+	})
+}
