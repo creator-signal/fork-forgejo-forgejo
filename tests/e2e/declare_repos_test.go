46d3925eb835760e12570391257f95baae76444b
BtbN <btbn@btbn.de>
2025-10-31 16:17:23 +0100

    fix: pull request review comment position (#9914)
    ## Checklist
    
    This PR contains both #9889 and #9912, since it depends on the one, and the other provides a test for it.
    The exact reasoning behind its logic is described here: https://codeberg.org/forgejo/forgejo/issues/9473#issuecomment-7976186
    
    This PR should return the behaviour back to how it was before a PR to Gitea changed it.
    Only the resulting Database-Entry will reference the line blamed commit, now also with the correct adjusted line.
    While the context diff view is pulled from the commit the commenter actually commented on.
    
    ### Tests
    
    - I added test coverage for Go changes...
      - [ ] in their respective `*_test.go` for unit tests.
      - [ ] in the `tests/integration` directory if it involves interactions with a live Forgejo server.
    - I added test coverage for JavaScript changes...
      - [ ] in `web_src/js/*.test.js` if it can be unit tested.
      - [x] in `tests/e2e/*.test.e2e.js` if it requires interactions with a live Forgejo server (see also the [developer guide for JavaScript testing](https://codeberg.org/forgejo/forgejo/src/branch/forgejo/tests/e2e/README.md#end-to-end-tests)).
    
    ### Documentation
    
    - [ ] I created a pull request [to the documentation](https://codeberg.org/forgejo/docs) to explain to Forgejo users how to use this change.
    - [x] I did not document these changes and I do not expect someone else to do it.
    
    ### Release notes
    
    - [ ] I do not want this change to show in the release notes.
    - [x] I want the title to show in the release notes with a link to this pull request.
    - [ ] I want the content of the `release-notes/<pull request number>.md` to be be used for the release notes instead of the title.
    
    Co-authored-by: Gusted <postmaster@gusted.xyz>
    Co-authored-by: Mathieu Fenniak <mathieu@fenniak.net>
    Reviewed-on: https://codeberg.org/forgejo/forgejo/pulls/9914
    Reviewed-by: Mathieu Fenniak <mfenniak@noreply.codeberg.org>
    Reviewed-by: Earl Warren <earl-warren@noreply.codeberg.org>
    Co-authored-by: BtbN <btbn@btbn.de>
    Co-committed-by: BtbN <btbn@btbn.de>
    (cherry picked from commit 6298ee4d3a7d50aeb3bdeaf653359978ee9fc2e6)

--- tests/e2e/declare_repos_test.go
+++ tests/e2e/declare_repos_test.go
@@ -5,6 +5,7 @@ package e2e
 
 import (
 	"fmt"
+	"os"
 	"strings"
 	"testing"
 	"time"
@@ -126,6 +127,19 @@ body:
 			postIssue(repo, user, 450, "right issue", "an issue containing word right")
 			postIssue(repo, user, 150, "left issue", "an issue containing word left")
 		}),
+		newRepo(t, 2, "long-diff-test", nil, []FileChanges{{
+			Filename: "test-README.md",
+			Versions: []string{
+				readStringFile(t, "tests/e2e/declarative-repo/long-diff-test/0-README.md"),
+			},
+		}}, func(user *user_model.User, repo *repo_model.Repository) {
+			commit1Sha := addCommitToBranch(t, user, repo, "main", "test-branch", "test-README.md", "",
+				readStringFile(t, "tests/e2e/declarative-repo/long-diff-test/1-README.md"))
+			commit2Sha := addCommitToBranch(t, user, repo, "test-branch", "test-branch", "test-README.md", commit1Sha,
+				readStringFile(t, "tests/e2e/declarative-repo/long-diff-test/2-README.md"))
+			addCommitToBranch(t, user, repo, "test-branch", "test-branch", "test-README.md", commit2Sha,
+				readStringFile(t, "tests/e2e/declarative-repo/long-diff-test/3-README.md"))
+		}),
 		// add your repo declarations here
 	}
 
@@ -134,6 +148,12 @@ body:
 			cleanup()
 		}
 	}
+}
+
+func readStringFile(t *testing.T, fn string) string {
+	c, err := os.ReadFile(fn)
+	require.NoError(t, err)
+	return string(c)
 }
 
 func newRepo(t *testing.T, userID int64, repoName string, initOpts *tests.DeclarativeRepoOptions, fileChanges []FileChanges, setup SetupRepo) func() {
@@ -201,4 +221,33 @@ func newRepo(t *testing.T, userID int64, repoName string, initOpts *tests.Declar
 	require.NoError(t, err)
 
 	return cleanupFunc
+}
+
+func addCommitToBranch(t *testing.T, user *user_model.User, repo *repo_model.Repository, oldBranch, newBranch, filename, lastSha, content string) string {
+	resp, err := files_service.ChangeRepoFiles(git.DefaultContext, repo, user, &files_service.ChangeRepoFilesOptions{
+		Files: []*files_service.ChangeRepoFile{{
+			Operation:     "update",
+			TreePath:      filename,
+			ContentReader: strings.NewReader(content),
+		}},
+		Message:   "add commit to branch",
+		OldBranch: oldBranch,
+		NewBranch: newBranch,
+		Author: &files_service.IdentityOptions{
+			Name:  user.Name,
+			Email: user.Email,
+		},
+		Committer: &files_service.IdentityOptions{
+			Name:  user.Name,
+			Email: user.Email,
+		},
+		Dates: &files_service.CommitDateOptions{
+			Author:    time.Now(),
+			Committer: time.Now(),
+		},
+		LastCommitID: lastSha,
+	})
+	require.NoError(t, err)
+	assert.NotEmpty(t, resp)
+	return resp.Commit.SHA
 }
--- tests/e2e/pr-review.test.e2e.ts
+++ tests/e2e/pr-review.test.e2e.ts
@@ -132,3 +132,55 @@ test('PR: Test mentions values', async ({page}) => {
   await page.locator("ul.suggestions li[data-value='@user1']").click();
   await expect(page.locator('.review-box-panel textarea#_combo_markdown_editor_0')).toHaveValue('@user1 ');
 });
+
+test('multi-commit commenting', async ({page, request}) => {
+  const response = await page.goto('/user2/long-diff-test');
+  expect(response?.status()).toBe(200);
+
+  try {
+    await page.getByText('2 branches').click(); // navigate to branch list
+    await page.getByText('New pull request').click(); // load compare view for the branch
+    await page.locator('.show-form-container').getByText('New pull request').click(); // actually open the PR form
+    await page.locator('.primary.button').getByText('Create pull request').click(); // submit PR creation
+
+    // Test situation: adding a comment on a line that was created in the *second* commit, doing it from the "Files changed" view.
+    await page.getByText('Files changed').click();
+    await page.getByText('More  This line was changed in commit 2')
+      .locator('..')
+      .locator('button.add-code-comment')
+      .click();
+    await page.getByPlaceholder('Leave a comment').fill('Comment on line changed in commit 2');
+    await page.getByText('Add single comment').click();
+
+    // Test assertion: when viewing the comment from the 'Conversation' page, it's diff should look correct:
+    await page.getByText('Conversation').click();
+    await expect(page.locator('.pull.menu .item.active')).toContainText('Conversation'); // ensure we navigated back to Conversation page
+    await expect(page.locator('.text.comment-content .render-content.markup')).toHaveText('Comment on line changed in commit 2');
+    await expect(page.locator('.diff-file-box .code-diff')).toContainText('More  This line was changed in commit 2');
+
+    // Test assertion: when viewing the comment from the second commit, it should be placed correctly in the UI:
+    await page.getByRole('link', {name: 'Commits'}).click();
+    await page.getByText('add commit to branch').nth(1).click();
+    // FIXME: The intent of this test is to make sure that the comment box appears in the "right spot", which is *below*
+    // the line of code that it was commented on.  This check uses the elements bounding boxes which... is pretty ugly
+    // and could be done better.  Probably would be better to find the line of code that the box is rendered on,
+    // instead.
+    const codeLine = await page.getByText('More  This line was changed in commit 2').boundingBox();
+    const commentBox = await page.locator('.render-content.markup').getByText('Comment on line changed in commit 2').boundingBox();
+    expect(commentBox.y).toBeGreaterThan(codeLine.y);
+  } finally {
+    // Delete any PRs on the test repo so that this test can be rerun.
+    const issuesResp = await request.get(`/api/v1/repos/user2/long-diff-test/issues`);
+    expect(issuesResp.ok()).toBeTruthy();
+    const issues = await issuesResp.json();
+    for (const issue of issues) {
+      const delResp = await request.delete(`/api/v1/repos/user2/long-diff-test/issues/${issue.number}`, {
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Basic ${btoa(`user1:password`)}`,
+        },
+      });
+      expect(delResp.ok()).toBeTruthy();
+    }
+  }
+});
