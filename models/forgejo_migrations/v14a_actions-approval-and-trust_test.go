efb27c31bd00c46c20116a054f66d9640239804e
Earl Warren <contact@earl-warren.org>
2025-11-01 14:35:37 +0100

    copy the models instead of referencing them so they are durable

--- models/forgejo_migrations/v14a_actions-approval-and-trust_test.go
+++ models/forgejo_migrations/v14a_actions-approval-and-trust_test.go
@@ -5,19 +5,77 @@ package forgejo_migrations
 
 import (
 	"testing"
+	"time"
 
 	actions_model "forgejo.org/models/actions"
 	"forgejo.org/models/db"
 	migration_tests "forgejo.org/models/gitea_migrations/test"
 	repo_model "forgejo.org/models/repo"
 	user_model "forgejo.org/models/user"
+	"forgejo.org/modules/timeutil"
+	webhook_module "forgejo.org/modules/webhook"
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
 func Test_v14ActionsApprovalAndTrustPopulateTableActionUser(t *testing.T) {
-	x, deferable := migration_tests.PrepareTestEnv(t, 0, new(user_model.User), new(repo_model.Repository), new(actions_model.ActionUser), new(actions_model.ActionRun))
+	type ActionUser struct {
+		ID     int64 `xorm:"pk autoincr"`
+		UserID int64 `xorm:"INDEX UNIQUE(action_user_index) REFERENCES(user, id)"`
+		RepoID int64 `xorm:"INDEX UNIQUE(action_user_index) REFERENCES(repository, id)"`
+
+		TrustedWithPullRequests bool
+
+		LastAccess timeutil.TimeStamp `xorm:"INDEX"`
+	}
+	type ActionRun struct {
+		ID            int64
+		Title         string
+		RepoID        int64                  `xorm:"index unique(repo_index) index(concurrency)"`
+		Repo          *repo_model.Repository `xorm:"-"`
+		OwnerID       int64                  `xorm:"index"`
+		WorkflowID    string                 `xorm:"index"`                    // the name of workflow file
+		Index         int64                  `xorm:"index unique(repo_index)"` // a unique number for each run of a repository
+		TriggerUserID int64                  `xorm:"index"`
+		TriggerUser   *user_model.User       `xorm:"-"`
+		ScheduleID    int64
+		Ref           string `xorm:"index"` // the commit/tag/… that caused the run
+		IsRefDeleted  bool   `xorm:"-"`
+		CommitSHA     string
+		Event         webhook_module.HookEventType // the webhook event that causes the workflow to run
+		EventPayload  string                       `xorm:"LONGTEXT"`
+		TriggerEvent  string                       // the trigger event defined in the `on` configuration of the triggered workflow
+		Status        actions_model.Status         `xorm:"index"`
+		Version       int                          `xorm:"version default 0"` // Status could be updated concomitantly, so an optimistic lock is needed
+		// Started and Stopped is used for recording last run time, if rerun happened, they will be reset to 0
+		Started timeutil.TimeStamp
+		Stopped timeutil.TimeStamp
+		// PreviousDuration is used for recording previous duration
+		PreviousDuration time.Duration
+		Created          timeutil.TimeStamp `xorm:"created"`
+		Updated          timeutil.TimeStamp `xorm:"updated"`
+		NotifyEmail      bool
+
+		// pull request trust
+		IsForkPullRequest   bool
+		PullRequestPosterID int64
+		PullRequestID       int64 `xorm:"index"`
+		NeedApproval        bool
+		ApprovedBy          int64 `xorm:"index"`
+
+		ConcurrencyGroup string `xorm:"'concurrency_group' index(concurrency)"`
+		ConcurrencyType  actions_model.ConcurrencyMode
+
+		PreExecutionError string `xorm:"LONGTEXT"` // used to report errors that blocked execution of a workflow
+	}
+	type Repository struct {
+		ID int64 `xorm:"pk autoincr"`
+	}
+	type User struct {
+		ID int64 `xorm:"pk autoincr"`
+	}
+	x, deferable := migration_tests.PrepareTestEnv(t, 0, new(User), new(Repository), new(ActionUser), new(ActionRun))
 	defer deferable()
 	if x == nil || t.Failed() {
 		return
