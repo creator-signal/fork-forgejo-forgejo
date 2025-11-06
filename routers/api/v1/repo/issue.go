4f453b90e1fa63e6c6cda0d054489267fee78d8f
t-h-i-s <t-h-i-s@noreply.codeberg.org>
2025-11-06 17:04:30 +0100

    routers/api/v1/repo/issue.go: prohibit set of due date for restricted users in swagger API

--- routers/api/v1/repo/issue.go
+++ routers/api/v1/repo/issue.go
@@ -863,6 +863,11 @@ func EditIssue(ctx *context.APIContext) {
 
 	// Update or remove the deadline, only if set and allowed
 	if (form.Deadline != nil || form.RemoveDeadline != nil) && canWrite {
+		if ctx.Doer.IsRestricted && !setting.RestrictedUser.AllowEditDueDate {
+			ctx.Error(http.StatusForbidden, "", "restricted users cannot modify due dates")
+			return
+		}
+
 		var deadlineUnix timeutil.TimeStamp
 
 		if form.RemoveDeadline == nil || !*form.RemoveDeadline {
