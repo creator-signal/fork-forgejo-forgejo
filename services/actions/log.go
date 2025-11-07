4f9eff6abd03b4d1bfc18c5d08aa718b6ec78847
Earl Warren <contact@earl-warren.org>
2025-11-07 17:26:25 +0100

    fix: garbage collect lingering actions logs
    If, for any reason (e.g. server crash), a task is recorded as done
    in the database but the logs are still in the database instead of
    being in storage, they need to be collected.
    
    The log_in_storage field is only set to true after the logs have been
    transfered to storage and can be relied upon to reflect which tasks
    have lingering logs.
    
    A task collects lingering logs every day, 3000 at a time, sleeping
    one second between them. In normal circumstances there will be only a
    few of them, even on a large instance, and there is no need to collect
    them as quickly as possible.
    
    When there are a lot of them for some reason, garbage collection must
    happen at a rate that is not too hard on storage I/O.
    
    Refs https://codeberg.org/forgejo/forgejo/issues/9999
    
    (cherry picked from commit 9982b2eea46069fa7a29b0f186dd0fac6494f173)

--- services/actions/log.go
+++ services/actions/log.go
@@ -5,10 +5,75 @@ package actions
 
 import (
 	"context"
+	"fmt"
+	"time"
 
 	actions_model "forgejo.org/models/actions"
+	"forgejo.org/models/db"
 	"forgejo.org/modules/actions"
+	"forgejo.org/modules/log"
+	"forgejo.org/modules/optional"
+	"forgejo.org/modules/timeutil"
 )
+
+var (
+	transferLingeringLogsMax   = 3000
+	transferLingeringLogsSleep = 1 * time.Second
+	transferLingeringLogsOld   = 24 * time.Hour
+)
+
+func TransferLingeringLogs(ctx context.Context) error {
+	return transferLingeringLogs(ctx, transferLingeringLogsOpts(time.Now()))
+}
+
+func transferLingeringLogsOpts(now time.Time) actions_model.FindTaskOptions {
+	// performance considerations: the search is linear because
+	// LogInStorage has no index. But it is bounded by
+	// LogExpired which is always true for older records and has an index.
+	return actions_model.FindTaskOptions{
+		Status:       actions_model.DoneStatuses(),
+		LogInStorage: optional.Some(false),
+		LogExpired:   optional.Some(false),
+		// do it after a long delay to avoid any possibility of race with an ongoing operation
+		// as it is not protected by a transaction
+		UpdatedBefore: timeutil.TimeStamp(now.Add(-transferLingeringLogsOld).Unix()),
+	}
+}
+
+func transferLingeringLogs(ctx context.Context, opts actions_model.FindTaskOptions) error {
+	total, err := db.Count[actions_model.ActionTask](ctx, opts)
+	if err != nil {
+		return err
+	}
+	if total <= 0 {
+		log.Debug("nothing to do")
+		return nil
+	}
+	count := 0
+	err = db.Iterate(ctx, opts.ToConds(), func(ctx context.Context, task *actions_model.ActionTask) error {
+		if err := TransferLogs(ctx, task); err != nil {
+			return err
+		}
+		log.Debug("processed task %d", task.ID)
+		count++
+		if count < transferLingeringLogsMax && count != int(total) {
+			log.Debug("sleeping %v to not stress the storage", transferLingeringLogsSleep)
+			time.Sleep(transferLingeringLogsSleep)
+		}
+		if count >= transferLingeringLogsMax {
+			return fmt.Errorf("stopped after processing %v tasks out of %v and will resume later", transferLingeringLogsMax, total)
+		}
+		return nil
+	})
+	if count >= transferLingeringLogsMax {
+		log.Info("%v", err)
+		return nil
+	}
+	if count > 0 {
+		log.Info("processed %d tasks", count)
+	}
+	return err
+}
 
 func TransferLogs(ctx context.Context, task *actions_model.ActionTask) error {
 	if task.LogInStorage {
