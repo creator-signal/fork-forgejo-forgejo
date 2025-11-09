5edb9d5f64cc2773056b9a858d0d71684ddda1cf
0ko <0ko@noreply.codeberg.org>
2025-11-09 08:36:39 +0500

    feat(ui/dropdown): js arrow navigation prototype

--- web_src/js/modules/dropdown.ts
+++ web_src/js/modules/dropdown.ts
@@ -9,12 +9,14 @@
 export function initDropdowns() {
   document.addEventListener('click', (event) => {
     const dropdown = document.querySelector<HTMLDetailsElement>('details.dropdown[open]');
-    // No open dropdowns on page, nothing to do.
-    if (dropdown === null) return;
+    if (dropdown === null)
+      // No open dropdowns on page, nothing to do.
+      return;
 
     const target = event.target as HTMLElement;
-    // User clicked something in the open dropdown, don't interfere.
-    if (dropdown.contains(target)) return;
+    if (dropdown.contains(target))
+      // User clicked something in the open dropdown, don't interfere.
+      return;
 
     // User clicked something that isn't the open dropdown, so close it.
     dropdown.removeAttribute('open');
@@ -22,14 +24,54 @@ export function initDropdowns() {
 
   // Close open dropdowns on Escape press
   document.addEventListener('keydown', (event) => {
-    // This press wasn't escape, nothing to do.
-    if (event.key !== 'Escape') return;
+    if (!['Escape', 'ArrowUp', 'ArrowDown'].includes(event.key))
+      // This eventListener is only concerned about a few keys
+      return;
 
     const dropdown = document.querySelector<HTMLDetailsElement>('details.dropdown[open]');
-    // No open dropdowns on page, nothing to do.
-    if (dropdown === null) return;
+    if (dropdown === null)
+      // No open dropdowns on page, nothing to do.
+      return;
 
-    // User pressed Escape while having an open dropdown, probably wants it be closed.
-    dropdown.removeAttribute('open');
+    if (event.key === 'Escape') {
+      // User pressed Escape while having an open dropdown, probably wants it be closed.
+      dropdown.removeAttribute('open');
+      return;
+    }
+
+    // todo: double check if this is needed
+    event.preventDefault();
+    event.stopPropagation();
+
+    const dropdownItems = dropdown.querySelectorAll<HTMLLIElement>('details.dropdown[open] > .content > ul > li');
+
+    // Knowing document.activeElement, find the <li> that contains it
+    let activeLi: HTMLLIElement, activeLiIndex: number;
+    for (let i = 0; i < dropdownItems.length; i++) {
+      const li = dropdownItems[i] as HTMLLIElement;
+      if (!li.contains(document.activeElement)) continue;
+      activeLi = li;
+      activeLiIndex = i;
+      break;
+    }
+    if (activeLi === undefined)
+      // The focused element is not a list item or it's contents, but something else in the dropdown
+      return;
+
+    if (event.key === 'ArrowUp') {
+      if (activeLiIndex === 0)
+        // Last child is already selected
+        return;
+      console.log("Refucos up")
+      dropdownItems[activeLiIndex - 1].querySelector<HTMLElement>(':is(a, button)').focus();
+    }
+
+    if (event.key === 'ArrowDown') {
+      if (activeLiIndex === dropdownItems.length - 1)
+        // First child is already selected
+        return;
+      console.log("Refucos down")
+      dropdownItems[activeLiIndex + 1].querySelector<HTMLElement>(':is(a, button)').focus();
+    }
   });
 }
