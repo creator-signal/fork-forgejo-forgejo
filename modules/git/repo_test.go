45cc8384c2e97cf8c5bcd42d87db531bc3d213c4
Gusted <postmaster@gusted.xyz>
2025-11-16 16:34:03 +0100

    chore: add unit test

--- modules/git/repo_test.go
+++ modules/git/repo_test.go
@@ -17,6 +17,7 @@ import (
 
 	"forgejo.org/modules/setting"
 	"forgejo.org/modules/test"
+
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
--- modules/setting/git_test.go
+++ modules/setting/git_test.go
@@ -1,9 +1,12 @@
 // Copyright 2019 The Gitea Authors. All rights reserved.
+// Copyright 2025 The Forgejo Authors. All rights reserved.
 // SPDX-License-Identifier: MIT
 
 package setting
 
 import (
+	"os"
+	"path/filepath"
 	"testing"
 
 	"forgejo.org/modules/test"
@@ -57,4 +60,38 @@ EXPIRATION = 123
 
 	assert.Equal(t, "false", GitConfig.GetOption("core.logAllRefUpdates"))
 	assert.Equal(t, "123", GitConfig.GetOption("gc.reflogExpire"))
+}
+
+func TestGitSelfTestGitCredentialHelperPath(t *testing.T) {
+	defer test.MockProtect(&Git)()
+	defer test.MockProtect(&GitConfig)()
+
+	t.Run("Path is file as directory", func(t *testing.T) {
+		require.ErrorContains(t, selfTestGitCredentialHelperPath("/dev/null/aaa"), `could not determine if "/dev/null/aaa" is a directory: stat /dev/null/aaa: not a directory`)
+	})
+
+	t.Run("Path is under a read-only directory", func(t *testing.T) {
+		readOnlyDir := filepath.Join(t.TempDir(), "read-only")
+		require.NoError(t, os.Mkdir(readOnlyDir, 0o500))
+
+		require.ErrorContains(t, selfTestGitCredentialHelperPath(filepath.Join(readOnlyDir, "aaa")), `could not create a directory at "`)
+	})
+
+	t.Run("Path is a file", func(t *testing.T) {
+		require.ErrorContains(t, selfTestGitCredentialHelperPath("/dev/null"), `the path "/dev/null" is not a directory`)
+	})
+
+	t.Run("Path is a read-only directory", func(t *testing.T) {
+		readOnlyDir := filepath.Join(t.TempDir(), "read-only")
+		require.NoError(t, os.Mkdir(readOnlyDir, 0o400))
+
+		require.ErrorContains(t, selfTestGitCredentialHelperPath(readOnlyDir), `could not write self-test program to "`)
+	})
+
+	t.Run("Normal", func(t *testing.T) {
+		readOnlyDir := filepath.Join(t.TempDir(), "normal")
+		require.NoError(t, os.Mkdir(readOnlyDir, 0o700))
+
+		require.NoError(t, selfTestGitCredentialHelperPath(readOnlyDir))
+	})
 }
