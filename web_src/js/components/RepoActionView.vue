0a43ba134e7023159a5f7262f3d12e503c99e599
Mathieu Fenniak <mathieu@fenniak.net>
2025-12-07 19:36:54 -0700

    refactor: extract ActionJobStep from RepoActionView

--- web_src/js/components/RepoActionView.vue
+++ web_src/js/components/RepoActionView.vue
@@ -1,9 +1,8 @@
 <script>
 import {SvgIcon} from '../svg.js';
 import ActionRunStatus from './ActionRunStatus.vue';
+import ActionJobStep from './ActionJobStep.vue';
 import {toggleElem} from '../utils/dom.js';
-import {formatDatetime} from '../utils/time.js';
-import {renderAnsi} from '../render/ansi.js';
 import {GET, POST, DELETE} from '../modules/fetch.js';
 
 export default {
@@ -11,6 +10,7 @@ export default {
   components: {
     SvgIcon,
     ActionRunStatus,
+    ActionJobStep,
   },
   props: {
     initialJobData: {
@@ -230,109 +230,20 @@ export default {
         this.loadJob();
       }
     },
+
     // cancel a run
     cancelRun() {
       POST(`${this.run.link}/cancel`);
     },
+
     // approve a run
     approveRun() {
       const url = `${this.run.commit.branch.link}#pull-request-trust-panel`;
       window.location.href = url;
     },
-    // show/hide the step logs for a group
-    toggleGroupLogs(event) {
-      const line = event.target.parentElement;
-      const list = line.nextSibling;
-      list.classList.toggle('hidden', event.newState !== 'open');
-    },
-
-    createLogLine(line, startTime, stepIndex, group) {
-      const lineNo = line.index - this.lineNumberOffset[stepIndex];
-      const div = document.createElement('div');
-      div.classList.add('job-log-line');
-      div.setAttribute('id', `jobstep-${stepIndex}-${lineNo}`);
-      div._jobLogTime = line.timestamp;
-
-      const lineNumber = document.createElement('a');
-      lineNumber.classList.add('line-num', 'muted');
-      lineNumber.textContent = lineNo;
-      lineNumber.setAttribute('href', `#jobstep-${stepIndex}-${lineNo}`);
-      div.append(lineNumber);
-
-      // for "Show timestamps"
-      const logTimeStamp = document.createElement('span');
-      logTimeStamp.className = 'log-time-stamp';
-      const date = new Date(parseFloat(line.timestamp * 1000));
-      const timeStamp = formatDatetime(date);
-      logTimeStamp.textContent = timeStamp;
-      toggleElem(logTimeStamp, this.timeVisible['log-time-stamp']);
-      // for "Show seconds"
-      const logTimeSeconds = document.createElement('span');
-      logTimeSeconds.className = 'log-time-seconds';
-      const seconds = Math.floor(parseFloat(line.timestamp) - parseFloat(startTime));
-      logTimeSeconds.textContent = `${seconds}s`;
-      toggleElem(logTimeSeconds, this.timeVisible['log-time-seconds']);
-
-      let logMessage = document.createElement('span');
-      logMessage.innerHTML = renderAnsi(line.message);
-      // If the input to renderAnsi is not empty and the output is empty we can
-      // assume the input was only ANSI escape codes that have been removed. In
-      // that case we should not display this message
-      if (line.message !== '' && logMessage.innerHTML === '') {
-        this.lineNumberOffset[stepIndex]++;
-        return [];
-      }
-      if (group.isHeader) {
-        const details = document.createElement('details');
-        details.addEventListener('toggle', this.toggleGroupLogs);
-        const summary = document.createElement('summary');
-        summary.append(logMessage);
-        details.append(summary);
-        logMessage = details;
-      }
-      logMessage.className = 'log-msg';
-      logMessage.style.paddingLeft = `${group.depth}em`;
-
-      div.append(logTimeStamp);
-      div.append(logMessage);
-      div.append(logTimeSeconds);
-
-      return div;
-    },
 
     appendLogs(stepIndex, logLines, startTime) {
-      const groupStack = [];
-      const container = this.$refs.logs[stepIndex];
-      for (const line of logLines) {
-        const el = groupStack.length > 0 ? groupStack[groupStack.length - 1] : container;
-        const group = {
-          depth: groupStack.length,
-          isHeader: false,
-        };
-        if (line.message.startsWith('##[group]')) {
-          group.isHeader = true;
-
-          const logLine = this.createLogLine(
-            {
-              ...line,
-              message: line.message.substring(9),
-            },
-            startTime, stepIndex, group,
-          );
-          logLine.setAttribute('data-group', group.index);
-          el.append(logLine);
-
-          const list = document.createElement('div');
-          list.classList.add('job-log-list', 'hidden');
-          list.setAttribute('data-group', group.index);
-          groupStack.push(list);
-          el.append(list);
-        } else if (line.message.startsWith('##[endgroup]')) {
-          groupStack.pop();
-        } else {
-          el.append(this.createLogLine(line, startTime, stepIndex, group));
-        }
-      }
+      this.$refs.jobSteps[stepIndex].appendLogs(logLines, startTime);
     },
 
     async fetchArtifacts() {
@@ -680,21 +591,21 @@ export default {
         </div>
         <div class="job-step-container" ref="steps" v-if="currentJob.steps.length">
           <div class="job-step-section" v-for="(jobStep, i) in currentJob.steps" :key="i">
-            <div class="job-step-summary" tabindex="0" @click.stop="isExpandable(jobStep.status) && toggleStepLogs(i)" @keyup.enter.stop="isExpandable(jobStep.status) && toggleStepLogs(i)" @keyup.space.stop="isExpandable(jobStep.status) && toggleStepLogs(i)" :class="[currentJobStepsStates[i].expanded ? 'selected' : '', isExpandable(jobStep.status) && 'step-expandable']">
-              <!-- If the job is done and the job step log is loaded for the first time, show the loading icon
-                currentJobStepsStates[i].cursor === null means the log is loaded for the first time
-              -->
-              <SvgIcon v-if="isDone(run.status) && currentJobStepsStates[i].expanded && currentJobStepsStates[i].cursor === null" name="octicon-sync" class="tw-mr-2 job-status-rotate"/>
-              <SvgIcon v-else :name="currentJobStepsStates[i].expanded ? 'octicon-chevron-down': 'octicon-chevron-right'" :class="['tw-mr-2', !isExpandable(jobStep.status) && 'tw-invisible']"/>
-              <ActionRunStatus :status="jobStep.status" class="tw-mr-2"/>
-
-              <span class="step-summary-msg gt-ellipsis">{{ jobStep.summary }}</span>
-              <span class="step-summary-duration">{{ jobStep.duration }}</span>
-            </div>
-
-            <!-- the log elements could be a lot, do not use v-if to destroy/reconstruct the DOM,
-            use native DOM elements for "log line" to improve performance, Vue is not suitable for managing so many reactive elements. -->
-            <div class="job-step-logs" ref="logs" v-show="currentJobStepsStates[i].expanded"/>
+            <ActionJobStep
+              ref="jobSteps"
+              :run-status="run.status"
+              :is-expandable="isExpandable"
+              :is-done="isDone"
+              :step-id="i"
+              :status="jobStep.status"
+              :summary="jobStep.summary"
+              :duration="jobStep.duration"
+              :expanded="currentJobStepsStates[i].expanded"
+              :cursor="currentJobStepsStates[i].cursor"
+              :time-visible-timestamp="timeVisible['log-time-stamp']"
+              :time-visible-seconds="timeVisible['log-time-seconds']"
+              @toggle="() => toggleStepLogs(i)"
+            />
           </div>
         </div>
       </div>
@@ -961,37 +872,6 @@ export default {
   border-radius: 0 0 var(--border-radius) var(--border-radius);
   border-top: 1px solid var(--color-console-border);
   z-index: 0;
-}
-
-.job-step-container .job-step-summary {
-  padding: 5px 10px;
-  display: flex;
-  align-items: center;
-  border-radius: var(--border-radius);
-}
-
-.job-step-container .job-step-summary.step-expandable {
-  cursor: pointer;
-}
-
-.job-step-container .job-step-summary.step-expandable:hover {
-  color: var(--color-console-fg);
-  background: var(--color-console-hover-bg);
-}
-
-.job-step-container .job-step-summary .step-summary-msg {
-  flex: 1;
-}
-
-.job-step-container .job-step-summary .step-summary-duration {
-  margin-left: 16px;
-}
-
-.job-step-container .job-step-summary.selected {
-  color: var(--color-console-fg);
-  background-color: var(--color-console-active-bg);
-  position: sticky;
-  top: 60px;
 }
 
 @media (max-width: 767.98px) {
