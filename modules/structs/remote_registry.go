718560cd60204a51d3bd535ddb62998c7c429868
erik <erik.seiert@meissa-gmbh.de>
2025-12-16 12:45:18 +0100

    Add rr options struct

---
+++ modules/structs/remote_registry.go
@@ -0,0 +1,14 @@
+package structs
+
+// CreateOrgOption options for creating an organization
+type CreateRemoteRegistryOption struct {
+	// required: true
+	Name string `json:"name" binding:"Required;MaxSize(100)"`
+	// required: true
+	RemoteType string `json:"remote_type" binding:"Required"`
+	// required: true
+	RemoteURL      string `json:"remote_url" binding:"Required;ValidUrl;MaxSize(255)"`
+	RemoteUser     string `json:"remote_user" binding:"MaxSize(255)"`
+	RemotePassword string `json:"remote_pass" binding:"MaxSize(255)"`
+	RemoteToken    string `json:"remote_token" binding:"MaxSize(255)"`
+}
