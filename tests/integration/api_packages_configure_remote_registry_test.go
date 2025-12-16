76fcd3c33982db57a410e2b9620bfe27fbabc483
erik <erik.seiert@meissa-gmbh.de>
2025-12-16 16:47:10 +0100

    Refactor test

--- tests/integration/api_packages_configure_remote_registry_test.go
+++ tests/integration/api_packages_configure_remote_registry_test.go
@@ -9,37 +9,32 @@ import (
 	"testing"
 
 	auth_model "forgejo.org/models/auth"
+	"forgejo.org/models/packages"
 	rr_model "forgejo.org/models/remote_registry"
 	"forgejo.org/models/unittest"
 	user_model "forgejo.org/models/user"
 	api "forgejo.org/modules/structs"
 	"forgejo.org/tests"
+	"github.com/stretchr/testify/assert"
 )
 
-func TestConfigureRemoteRegistry(t *testing.T) {
+func TestCreateRemoteRegistry(t *testing.T) {
 	defer tests.PrepareTestEnv(t)()
-
 	user := unittest.AssertExistsAndLoadBean(t, &user_model.User{ID: 4})
 	session := loginUser(t, user.Name)
 	tokenWritePackage := getTokenForLoggedInUser(t, session, auth_model.AccessTokenScopeWritePackage)
 
-	t.Run("ConfigureRemoteRegistry", func(t *testing.T) {
-		defer tests.PrintCurrentTest(t)()
+	rr := api.CreateRemoteRegistryOption{
+		Name:        "testreg",
+		RemoteType:  "container",
+		RemoteURL:   "https://example.registry.com",
+		RemoteUser:  "someUser",
+		RemoteToken: "asdfwoe324lkjsdf0242523",
+	}
+	req := NewRequestWithJSON(t, "POST", fmt.Sprintf("/api/v1/packages/%s/remote-registry", user.Name), &rr).AddTokenAuth(tokenWritePackage)
+	MakeRequest(t, req, http.StatusCreated)
 
-		rr := api.CreateRemoteRegistryOption{
-			Name:        "testreg",
-			RemoteType:  "container",
-			RemoteURL:   "https://example.registry.com",
-			RemoteUser:  "someUser",
-			RemoteToken: "asdfwoe324lkjsdf0242523",
-		}
+	retrieved := unittest.AssertExistsAndLoadBean(t, &rr_model.RemoteRegistry{Name: "testreg"})
+	assert.Equal(t, packages.TypeContainer, retrieved.RemoteType)
 
-		req := NewRequestWithJSON(t, "POST", fmt.Sprintf("/api/v1/packages/%s/remote-registry", user.Name), &rr).AddTokenAuth(tokenWritePackage)
-		MakeRequest(t, req, http.StatusCreated)
-
-		unittest.AssertExistsAndLoadBean(t, &rr_model.RemoteRegistry{
-			ID:   int64(1),
-			Name: rr.Name,
-		})
-	})
 }
