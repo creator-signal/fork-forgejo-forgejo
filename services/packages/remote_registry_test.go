200bbe50828648a81ef45a3ee8b994cb21f24b38
erik <erik.seiert@meissa-gmbh.de>
2025-12-19 12:15:08 +0100

    Add test to service

---
+++ services/packages/remote_registry_test.go
@@ -0,0 +1,32 @@
+// Copyright 2022 The Gitea Authors. All rights reserved.
+// SPDX-License-Identifier: MIT
+
+package packages
+
+import (
+	"testing"
+
+	"forgejo.org/models/packages"
+	rr_model "forgejo.org/models/remote_registry"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func Test_NewRemoteRegistry(t *testing.T) {
+	name := "testreg"
+	remoteURL := "https://example.com"
+	remoteType := packages.TypeContainer
+	opts := RROpts{
+		rr_model.RemoteRegistryOwnerType("org"),
+		int64(1),
+		RRCredentials{},
+	}
+
+	rr, err := NewRemoteRegistry(name, remoteURL, remoteType, opts)
+
+	require.NoError(t, err)
+	assert.Equal(t, name, rr.Name)
+	assert.Equal(t, remoteURL, rr.RemoteURL)
+	assert.Equal(t, remoteType, rr.RemoteType)
+	assert.Equal(t, opts.OwnerType, rr.OwnerType)
+}
