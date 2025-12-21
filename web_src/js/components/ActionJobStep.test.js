768cfd65133dd1090bc9d3dbf1541d4760a434aa
Mathieu Fenniak <mathieu@fenniak.net>
2025-12-21 10:58:06 -0700

    refactor: move relevant styles down to ActionJobStep

--- web_src/js/components/ActionJobStep.test.js
+++ web_src/js/components/ActionJobStep.test.js
@@ -1,3 +1,6 @@
+// Copyright 2025 The Forgejo Authors. All rights reserved.
+// SPDX-License-Identifier: GPL-3.0-or-later
+
 import {describe, expect, test, vi} from 'vitest';
 import {mount} from '@vue/test-utils';
 import ActionJobStep from './ActionJobStep.vue';
