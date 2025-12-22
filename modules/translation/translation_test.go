f66ad2e59dc4b7315fec958bba05522c58777151
0ko <0ko@noreply.codeberg.org>
2025-12-22 09:50:04 +0500

    chore: fix translation(_test).go licenses

--- modules/translation/translation_test.go
+++ modules/translation/translation_test.go
@@ -1,9 +1,8 @@
 // Copyright 2023 The Gitea Authors. All rights reserved.
+// Copyright 2024 The Forgejo Authors. All rights reserved.
 // SPDX-License-Identifier: MIT
 
 package translation
-
-// TODO: make this package friendly to testing
 
 import (
 	"testing"
