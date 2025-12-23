e1b4d15b04253d328b5f265228422ac4315c184e
Beowulf <beowulf@beocode.eu>
2025-12-23 01:07:23 +0100

    fix(ui): don't stretch activity top author image

--- web_src/js/components/RepoActivityTopAuthors.vue
+++ web_src/js/components/RepoActivityTopAuthors.vue
@@ -42,56 +42,7 @@ export default {
     i18nCommitActivity: this,
   }),
   mounted() {
-    const refStyle = window.getComputedStyle(this.$refs.style);
-    this.colors.barColor = refStyle.backgroundColor;
-
-    for (const item of this.activityTopAuthors) {
-      const img = new Image();
-      img.src = item.avatar_link;
-      item.avatar_img = img;
-    }
-
-    Chart.register({
-      id: 'image_label',
-      afterDraw: (chart) => {
-        const xAxis = chart.boxes[0];
-        const yAxis = chart.boxes[1];
-        for (const [index] of xAxis.ticks.entries()) {
-          const x = xAxis.getPixelForTick(index);
-          const img = this.activityTopAuthors[index].avatar_img;
-
-          chart.ctx.save();
-          chart.ctx.drawImage(img, 0, 0, img.naturalWidth, img.naturalHeight, x - 10, yAxis.bottom + 10, 20, 20);
-          chart.ctx.restore();
-        }
-      },
-      beforeEvent: (chart, args) => {
-        const event = args.event;
-        if (event.type !== 'mousemove' && event.type !== 'click') return;
-
-        const yAxis = chart.boxes[1];
-        if (event.y < yAxis.bottom + 10 || event.y > yAxis.bottom + 30) {
-          chart.canvas.style.cursor = '';
-          return;
-        }
-
-        const xAxis = chart.boxes[0];
-        const pointIdx = xAxis.ticks.findIndex((_, index) => {
-          const x = xAxis.getPixelForTick(index);
-          return event.x >= x - 10 && event.x <= x + 10;
-        });
-
-        if (pointIdx === -1) {
-          chart.canvas.style.cursor = '';
-          return;
-        }
-
-        chart.canvas.style.cursor = 'pointer';
-        if (event.type === 'click' && this.activityTopAuthors[pointIdx].home_link) {
-          window.location.href = this.activityTopAuthors[pointIdx].home_link;
-        }
-      },
-    });
+    this.init();
   },
   methods: {
     graphPoints() {
@@ -137,7 +88,81 @@ export default {
             },
           },
         },
+        plugins: {
+          tooltip: {
+            intersect: false,
+          },
+        },
       };
+    },
+    init() {
+      const refStyle = window.getComputedStyle(this.$refs.style);
+      this.colors.barColor = refStyle.backgroundColor;
+
+      for (const item of this.activityTopAuthors) {
+        const img = new Image();
+        img.src = item.avatar_link;
+        item.avatar_img = img;
+      }
+
+      Chart.register({
+        id: 'image_label',
+        afterDraw: (chart) => {
+          const xAxis = chart.boxes[0];
+          const yAxis = chart.boxes[1];
+          for (const [index] of xAxis.ticks.entries()) {
+            const x = xAxis.getPixelForTick(index);
+            const img = this.activityTopAuthors[index].avatar_img;
+
+            chart.ctx.save();
+            const [width, height, dx, dy] = this.calcImageSizeAndShift(img);
+            chart.ctx.drawImage(img, 0, 0, img.naturalWidth, img.naturalHeight, x - 10 + dx, yAxis.bottom + 10 + dy, width, height);
+            chart.ctx.restore();
+          }
+        },
+        beforeEvent: (chart, args) => {
+          const event = args.event;
+          if (event.type !== 'mousemove' && event.type !== 'click') return;
+
+          const yAxis = chart.boxes[1];
+          if (event.y < yAxis.bottom + 10 || event.y > yAxis.bottom + 30) {
+            chart.canvas.style.cursor = '';
+            return;
+          }
+
+          const xAxis = chart.boxes[0];
+          const pointIdx = xAxis.ticks.findIndex((_, index) => {
+            const x = xAxis.getPixelForTick(index);
+            return event.x >= x - 10 && event.x <= x + 10;
+          });
+
+          if (pointIdx === -1) {
+            chart.canvas.style.cursor = '';
+            return;
+          }
+
+          chart.canvas.style.cursor = 'pointer';
+          if (event.type === 'click' && this.activityTopAuthors[pointIdx].home_link) {
+            window.location.href = this.activityTopAuthors[pointIdx].home_link;
+          }
+        },
+      });
+    },
+    calcImageSizeAndShift(img) {
+      const targetSize = 20;
+      const [imgWidth, imgHeight] = [img.naturalWidth, img.naturalHeight];
+
+      // The image should be contained in a square,
+      // so the scale depends on the longer dimension.
+      const scale = targetSize / (Math.max(imgWidth, imgHeight));
+      const calcScale = (size) => size * scale;
+      const [width, height] = [calcScale(imgWidth), calcScale(imgHeight)];
+
+      // The image should be centered in the 20x20 square.
+      const calcShift = (size) => (targetSize - size) / 2;
+      const [dx, dy] = [calcShift(width), calcShift(height)];
+
+      return [width, height, dx, dy];
     },
   },
 };
