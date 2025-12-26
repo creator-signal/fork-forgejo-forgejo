1722f8ac1019467a85f50a525419cfcef6bd866c
pat-s <patrick.schratz@gmail.com>
2025-12-26 22:26:38 +0100

    models: add user setting keys for watch preferences

--- models/user/setting_keys.go
+++ models/user/setting_keys.go
@@ -10,6 +10,14 @@ const (
 	SettingsKeyDiffWhitespaceBehavior = "diff.whitespace_behaviour"
 	// SettingsKeyShowOutdatedComments is the setting key whether or not to show outdated comments in PRs
 	SettingsKeyShowOutdatedComments = "comment_code.show_outdated"
+	// SettingsKeyDefaultWatchEvents is the setting key for default watch events when watching a repository
+	SettingsKeyDefaultWatchEvents = "repo.default_watch_events"
+	// SettingsKeyAutoWatchOnCreate is the setting key for auto-watch when creating a repo
+	SettingsKeyAutoWatchOnCreate = "repo.auto_watch_on_create"
+	// SettingsKeyAutoWatchOnAccess is the setting key for auto-watch when getting access to a repo
+	SettingsKeyAutoWatchOnAccess = "repo.auto_watch_on_access"
+	// SettingsKeyAutoWatchOnContribute is the setting key for auto-watch on first contribution
+	SettingsKeyAutoWatchOnContribute = "repo.auto_watch_on_contribute"
 	// UserActivityPubPrivPem is user's private key
 	UserActivityPubPrivPem = "activitypub.priv_pem"
 	// UserActivityPubPubPem is user's public key
