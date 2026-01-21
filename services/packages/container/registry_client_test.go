c3e6bb78af2ab8633069a994f952a8e74e9911aa
erik <erik.seiert@meissa-gmbh.de>
2026-01-21 12:53:15 +0100

    Add test for authentication

--- services/packages/container/registry_client_test.go
+++ services/packages/container/registry_client_test.go
@@ -1,6 +1,7 @@
 package container
 
 import (
+	"net/http"
 	"testing"
 
 	rr_model "forgejo.org/models/remote_registry"
@@ -27,4 +28,27 @@ func Test_PingRegistry(t *testing.T) {
 
 	assert.NoError(t, err)
 	assert.NotEmpty(t, resp)
+}
+
+func Test_AuthenticateRegistry(t *testing.T) {
+
+	server := mock_server.MockRegistryServer()
+	defer server.Close()
+
+	rrOpts := api.CreateRemoteRegistryOption{
+		Name:      "testreg",
+		RemoteURL: server.URL,
+	}
+
+	rr := rr_model.RemoteRegistry{
+		Name:        rrOpts.Name,
+		RemoteURL:   rrOpts.RemoteURL,
+		RemoteUser:  "someUser",
+		RemoteToken: "someToken",
+	}
+	resp, err := PingRemoteRegistry(t.Context(), &rr)
+	authResp, err := AuthenticateRemoteRegistry(t.Context(), resp, &rr)
+
+	assert.NoError(t, err)
+	assert.Equal(t, http.StatusOK, authResp.StatusCode)
 }
