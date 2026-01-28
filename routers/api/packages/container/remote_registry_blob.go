6a161e38e4dd0bee6fce7a193bd2fcc0c0a7859b
erik <erik.seiert@meissa-gmbh.de>
2026-01-28 10:46:30 +0100

    Add dummy remote functions

---
+++ routers/api/packages/container/remote_registry_blob.go
@@ -0,0 +1,32 @@
+// Copyright 2026 The Forgejo Authors. All rights reserved.
+// SPDX-License-Identifier: MIT
+
+package container
+
+import (
+	"forgejo.org/services/context"
+)
+
+func VerifyRemoteImageName(ctx *context.Context) {
+	return
+}
+
+func GetRemoteTagList(ctx *context.Context) {
+	return
+}
+
+func remoteHeadBlob(ctx *context.Context) {
+	return
+}
+
+func remoteGetBlob(ctx *context.Context) {
+	return
+}
+
+func remoteHeadManifest(ctx *context.Context) {
+	return
+}
+
+func remoteGetManifest(ctx *context.Context) {
+	return
+}
