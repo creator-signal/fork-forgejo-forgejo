2eebe8c12475a08b80ff889168525e717019aff3
erik <erik.seiert@meissa-gmbh.de>
2026-01-28 17:12:47 +0100

    Initial middleware implementation

--- routers/api/packages/container/remote_registry.go
+++ routers/api/packages/container/remote_registry.go
@@ -4,22 +4,26 @@
 package container
 
 import (
+	"net/http"
+
 	"forgejo.org/services/context"
 
 	rr_model "forgejo.org/models/remote_registry"
 	"forgejo.org/modules/log"
 	"forgejo.org/modules/setting"
+	rr_service "forgejo.org/services/packages"
 )
 
 // RemoteRegistryContext represents remote registry information in the request context
 type RemoteRegistryContext struct {
-	IsRemoteRequest   bool
 	RemoteRegistry    *rr_model.RemoteRegistry
 	OwnerName         string
 	RemoteName        string
 	ImageName         string
 	OriginalImageName string // the parsed image part before remote resolution
 }
+
+const remoteRegistryContextKey = "RemoteRegistryContext"
 
 // RemoteRegistryMiddleware detects and resolves remote registry requests
 func RemoteRegistryMiddleware(ctx *context.Context) {
@@ -35,4 +39,21 @@ func RemoteRegistryMiddleware(ctx *context.Context) {
 	log.Trace("Detected remote registry request: owner=%s, user=%s, remote=%s, image=%s",
 		ownerName, username, remoteName, imageName)
 
+	// Resolve remote registry with precedence
+	remoteRegistry, err := rr_service.GetRemoteRegistryFromContext(*ctx)
+	if err != nil {
+		log.Error("Failed to resolve remote registry %q: %v", remoteName, err)
+		apiError(ctx, http.StatusInternalServerError, err)
+		return
+	}
+	remoteCtx := &RemoteRegistryContext{
+		OwnerName:         ownerName,
+		RemoteName:        remoteName,
+		ImageName:         imageName,
+		OriginalImageName: imageName,
+		RemoteRegistry:    remoteRegistry,
+	}
+
+	// Store in context
+	ctx.Data[remoteRegistryContextKey] = remoteCtx
 }
