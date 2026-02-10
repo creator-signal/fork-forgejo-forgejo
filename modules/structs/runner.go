aa41a8b2b71899d2e3d5fa0676411fd65b153cd4
Manuel Ganter <manuel.ganter@think-ahead.tech>
2026-02-10 17:55:07 +0100

    added ephemeral property to http endpoints

--- modules/structs/runner.go
+++ modules/structs/runner.go
@@ -15,6 +15,11 @@ type RegisterRunnerOptions struct {
 	//
 	// required: false
 	Description string `json:"description"`
+
+	// Register as ephemeral runner https://forgejo.org/docs/latest/admin/actions/security/#ephemeral-runner
+	//
+	// required: false
+	Ephemeral bool `json:"ephemeral"`
 }
 
 // RegisterRunnerResponse contains the details of the just registered runner.
@@ -23,4 +28,5 @@ type RegisterRunnerResponse struct {
 	ID    int64  `json:"id" binding:"Required"`
 	UUID  string `json:"uuid" binding:"Required"`
 	Token string `json:"token" binding:"Required"`
+	Ephemeral bool `json:"ephemeral" binding:"Required"`
 }
