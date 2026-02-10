d70dc991166ff67d94e65914f85a1682dde91a67
erik <erik.seiert@meissa-gmbh.de>
2026-02-10 15:47:40 +0100

    Add rr host and cache time to creation info, add method for creating remote manifest

--- services/packages/container/manifest.go
+++ services/packages/container/manifest.go
@@ -6,6 +6,7 @@ package container
 import (
 	"errors"
 	"regexp"
+	"time"
 
 	"forgejo.org/models/user"
 
@@ -16,13 +17,15 @@ var ReferencePattern = regexp.MustCompile(`\A[a-zA-Z0-9_][a-zA-Z0-9._-]{0,127}\z
 
 // ManifestCreationInfo describes a manifest to create
 type ManifestCreationInfo struct {
-	MediaType  string
-	Owner      *user.User
-	Creator    *user.User
-	Image      string
-	Reference  string
-	IsTagged   bool
-	Properties map[string]string
+	MediaType          string
+	Owner              *user.User
+	Creator            *user.User
+	Image              string
+	Reference          string
+	IsTagged           bool
+	RemoteRegistryHost string
+	CacheTimeUnix      int64
+	Properties         map[string]string
 }
 
 func NewManifestCreationInfo(owner, creator *user.User, mediaType, image, reference string) (*ManifestCreationInfo, error) {
@@ -35,6 +38,28 @@ func NewManifestCreationInfo(owner, creator *user.User, mediaType, image, refere
 		Image:     image,
 		Reference: reference,
 		IsTagged:  isTagged,
+	}
+
+	if mci.IsTagged && !ReferencePattern.MatchString(reference) {
+		return &ManifestCreationInfo{}, errors.New("Tag is invalid")
+	}
+
+	return mci, nil
+}
+
+func NewRemoteManifestCreationInfo(owner, creator *user.User, mediaType, image, reference, host string) (*ManifestCreationInfo, error) {
+	isTagged := digest.Digest(reference).Validate() != nil
+	time := time.Now().Unix()
+
+	mci := &ManifestCreationInfo{
+		MediaType:          mediaType,
+		Owner:              owner,
+		Creator:            creator,
+		Image:              image,
+		Reference:          reference,
+		IsTagged:           isTagged,
+		RemoteRegistryHost: host,
+		CacheTimeUnix:      time,
 	}
 
 	if mci.IsTagged && !ReferencePattern.MatchString(reference) {
