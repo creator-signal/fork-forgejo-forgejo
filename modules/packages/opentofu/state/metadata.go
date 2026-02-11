offsoc/forgejo

Add package metadata

modules/packages/opentofu/state/metadata.go
@@ -1,11 +1,95 @@
 package state
 
+import (
+	"crypto/md5"
+	"encoding/json"
+	"errors"
+	"fmt"
+)
+
 const (
 	// See https://opentofu.org/docs/language/state/
 	OpenTofuStateFilename = "terraform.tfstate"
 )
 
-// Metadata represents the metadata of an OpenTofu/Terraform package
+var (
+	ErrParseStateFile          = errors.New("Failed to parse the state file")
+	ErrParseEncryptionMetadata = errors.New("Failed to parse the encryption metadata")
+)
+
+// Metadata represents the metadata of an OpenTofu/Terraform package.
 type Metadata struct {
-	Version int `json:"version"`
+	// The MD5 checksum of the state file.
+	ChecksumMD5 [16]byte
+
+	// Whether or not the state file is encrypted.
+	Encrypted bool
+
+	// The "lineage" is a unique ID assigned to a state when it is created. If a
+	// lineage is different, then it means the states were created at different
+	// times and its very likely you're modifying a different state.
+	//
+	// See https://opentofu.org/docs/language/state/backends/#manual-state-pullpush.
+	Lineage string `json:"lineage"`
+
+	// OpenTofu/Terraform version used to generate the state file.
+	//
+	// Absent from encrypted state files.
+	OpenTofuVersion string `json:"terraform_version,omitempty"`
+
+	// Every state has a monotonically increasing "serial" number. If the
+	// destination state has a higher serial, OpenTofu will not allow you to write
+	// it since it means that changes have occurred since the state you're
+	// attempting to write.
+	//
+	// See https://opentofu.org/docs/language/state/backends/#manual-state-pullpush.
+	Serial uint64 `json:"serial"`
+
+	// Version of the state file format.
+	//
+	// Absent from encrypted state files.
+	StateFileVersion uint64 `json:"version,omitempty"`
+}
+
+// ParseMetadataFromStateFile extracts the package metadata from a state file.
+//
+// If md5Checksum is present, the MD5 hash will not be computed again but store
+// directly in the metadata.
+func ParseMetadataFromStateFile(stateFile *[]byte, md5Checksum *[16]byte) (*Metadata, error) {
+	var metadata Metadata
+	if err := json.Unmarshal(*stateFile, &metadata); err != nil {
+		return nil, fmt.Errorf("%w: %v", ErrParseStateFile, err)
+	}
+
+	isEncrypted, err := isEncryptedPayload(stateFile)
+	if err != nil {
+		return nil, err
+	}
+	metadata.Encrypted = isEncrypted
+
+	if md5Checksum != nil {
+		metadata.ChecksumMD5 = *md5Checksum
+
+	} else {
+		metadata.ChecksumMD5 = md5.Sum(*stateFile)
+	}
+
+	return &metadata, nil
+}
+
+// isEncryptedPayload detects whether or not the OpenTofu/Terraform state file's
+// payload is encrypted.
+func isEncryptedPayload(data *[]byte) (bool, error) {
+	type EncryptionMetadata struct {
+		Data    []byte            `json:"encrypted_data"`
+		Meta    map[string][]byte `json:"meta"`
+		Version string            `json:"encryption_version"`
+	}
+
+	var encryptionMetadata EncryptionMetadata
+	if err := json.Unmarshal(*data, &encryptionMetadata); err != nil {
+		return false, fmt.Errorf("%w: %v", ErrParseEncryptionMetadata, err)
+	}
+
+	return encryptionMetadata.Version != "", nil
 }

modules/packages/opentofu/state/metadata_test.go
@@ -1,0 +1,118 @@
+package state
+
+import (
+	"crypto/md5"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+const (
+	unencryptedVersion4StateFile string = `{
+		"version": 4,
+		"terraform_version": "1.9.1",
+		"serial": 1,
+		"lineage": "fcf2c3a0-3a0f-703f-c611-0ca7776c06b8",
+		"outputs": {},
+		"resources": [],
+		"check_results": null
+	}`
+
+	encryptedVersion4StateFile string = `{
+		"serial": 1,
+		"lineage": "2ce64317-825a-f8cd-b35e-84fa110b561f",
+		"meta": {
+			"key_provider.pbkdf2.local": "eyJzYWx0IjoiNW9WSGpZM3BGaHRaNncwUE1hSDkrcnc2UzR1cjE4Q05adUR4RXNWM1dmcz0iLCJpdGVyYXRpb25zIjo2MDAwMDAsImhhc2hfZnVuY3Rpb24iOiJzaGE1MTIiLCJrZXlfbGVuZ3RoIjozMn0="
+		},
+		"encrypted_data": "e9oQmRWzPOcELCgmi1uJd5vfKMSnO7oQe/Mpyo8pKEXyNwChoPs0HpDeFhf2h32diyUORX28rWQ+D+wotHseJ+9c6EeE/G3TNr+ilmvdy0nKqZ4ABw/YoD6Zwn+DKF4qviUK2pnXN3HOQqgHUMmiuzL/AqIz1gDBncDyKJLdzuAXjXI1NyFRtCNJmVJpJkyq0ZhQG/i1KxbxAQtCAySH8T3KPE/njy4X7E08vy29rHtGxF0=",
+		"encryption_version": "v0"
+	}`
+)
+
+func TestParseMetadata(t *testing.T) {
+	t.Run("InvalidJSON", func(t *testing.T) {
+		stateFile := []byte("This is not a JSON file")
+
+		metadata, err := ParseMetadataFromStateFile(&stateFile, nil)
+		assert.Nil(t, metadata)
+		require.ErrorIs(t, err, ErrParseStateFile)
+	})
+
+	t.Run("ValidUnencryptedVersion4StateFile", func(t *testing.T) {
+		stateFile := []byte(unencryptedVersion4StateFile)
+
+		metadata, err := ParseMetadataFromStateFile(&stateFile, nil)
+		assert.Equal(t, Metadata{
+			ChecksumMD5:      md5.Sum(stateFile),
+			Encrypted:        false,
+			Lineage:          "fcf2c3a0-3a0f-703f-c611-0ca7776c06b8",
+			OpenTofuVersion:  "1.9.1",
+			Serial:           1,
+			StateFileVersion: 4,
+		}, *metadata)
+		assert.Nil(t, err)
+	})
+
+	t.Run("ValidUnencryptedVersion4StateFileWithChecksum", func(t *testing.T) {
+		stateFile := []byte(unencryptedVersion4StateFile)
+		md5Checksum := md5.Sum(stateFile)
+
+		metadata, err := ParseMetadataFromStateFile(&stateFile, &md5Checksum)
+		assert.Equal(t, Metadata{
+			ChecksumMD5:      md5.Sum(stateFile),
+			Encrypted:        false,
+			Lineage:          "fcf2c3a0-3a0f-703f-c611-0ca7776c06b8",
+			OpenTofuVersion:  "1.9.1",
+			Serial:           1,
+			StateFileVersion: 4,
+		}, *metadata)
+		assert.Nil(t, err)
+	})
+
+	t.Run("ValidEncryptedV4StateFile", func(t *testing.T) {
+		stateFile := []byte(encryptedVersion4StateFile)
+
+		metadata, err := ParseMetadataFromStateFile(&stateFile, nil)
+		assert.Equal(t, Metadata{
+			ChecksumMD5: md5.Sum(stateFile),
+			Encrypted:   true,
+			Lineage:     "2ce64317-825a-f8cd-b35e-84fa110b561f",
+			Serial:      1,
+		}, *metadata)
+		assert.Nil(t, err)
+	})
+}
+
+func TestIsEncryptedPayload(t *testing.T) {
+	t.Run("InvalidJSON", func(t *testing.T) {
+		stateFile := []byte("This is not a JSON file")
+
+		isEncrypted, err := isEncryptedPayload(&stateFile)
+		assert.False(t, isEncrypted)
+		require.ErrorIs(t, err, ErrParseEncryptionMetadata)
+	})
+
+	t.Run("MissingEncryptionVersion", func(t *testing.T) {
+		stateFile := []byte(`{
+			"serial": 1,
+			"lineage": "2ce64317-825a-f8cd-b35e-84fa110b561f",
+			"meta": {
+				"key_provider.pbkdf2.local": "eyJzYWx0IjoiNW9WSGpZM3BGaHRaNncwUE1hSDkrcnc2UzR1cjE4Q05adUR4RXNWM1dmcz0iLCJpdGVyYXRpb25zIjo2MDAwMDAsImhhc2hfZnVuY3Rpb24iOiJzaGE1MTIiLCJrZXlfbGVuZ3RoIjozMn0="
+			},
+			"encrypted_data": "e9oQmRWzPOcELCgmi1uJd5vfKMSnO7oQe/Mpyo8pKEXyNwChoPs0HpDeFhf2h32diyUORX28rWQ+D+wotHseJ+9c6EeE/G3TNr+ilmvdy0nKqZ4ABw/YoD6Zwn+DKF4qviUK2pnXN3HOQqgHUMmiuzL/AqIz1gDBncDyKJLdzuAXjXI1NyFRtCNJmVJpJkyq0ZhQG/i1KxbxAQtCAySH8T3KPE/njy4X7E08vy29rHtGxF0="
+		}`)
+
+		isEncrypted, err := isEncryptedPayload(&stateFile)
+		assert.False(t, isEncrypted)
+		assert.Nil(t, err)
+	})
+
+	t.Run("ValidEncryptedVersion4StateFile", func(t *testing.T) {
+		stateFile := []byte(encryptedVersion4StateFile)
+
+		isEncrypted, err := isEncryptedPayload(&stateFile)
+		assert.True(t, isEncrypted)
+		assert.Nil(t, err)
+	})
+}

creator-signal/fork-forgejo-forgejo

modules/packages/opentofu/state/metadata.go
@@ -1,11 +1,95 @@
 package state
 
+import (
+	"crypto/md5"
+	"errors"
+	"fmt"
+
+	"forgejo.org/modules/json"
+)
+
 const (
 	// See https://opentofu.org/docs/language/state/
 	OpenTofuStateFilename = "terraform.tfstate"
 )
 
-// Metadata represents the metadata of an OpenTofu/Terraform package
+var (
+	ErrParseStateFile          = errors.New("failed to parse the state file")
+	ErrParseEncryptionMetadata = errors.New("failed to parse the encryption metadata")
+)
+
+// Metadata represents the metadata of an OpenTofu/Terraform package.
 type Metadata struct {
-	Version int `json:"version"`
+	// The MD5 checksum of the state file.
+	ChecksumMD5 [16]byte
+
+	// Whether or not the state file is encrypted.
+	Encrypted bool
+
+	// The "lineage" is a unique ID assigned to a state when it is created. If a
+	// lineage is different, then it means the states were created at different
+	// times and its very likely you're modifying a different state.
+	//
+	// See https://opentofu.org/docs/language/state/backends/#manual-state-pullpush.
+	Lineage string `json:"lineage"`
+
+	// OpenTofu/Terraform version used to generate the state file.
+	//
+	// Absent from encrypted state files.
+	OpenTofuVersion string `json:"terraform_version,omitempty"`
+
+	// Every state has a monotonically increasing "serial" number. If the
+	// destination state has a higher serial, OpenTofu will not allow you to write
+	// it since it means that changes have occurred since the state you're
+	// attempting to write.
+	//
+	// See https://opentofu.org/docs/language/state/backends/#manual-state-pullpush.
+	Serial uint64 `json:"serial"`
+
+	// Version of the state file format.
+	//
+	// Absent from encrypted state files.
+	StateFileVersion uint64 `json:"version,omitempty"`
+}
+
+// ParseMetadataFromStateFile extracts the package metadata from a state file.
+//
+// If md5Checksum is present, the MD5 hash will not be computed again but store
+// directly in the metadata.
+func ParseMetadataFromStateFile(stateFile *[]byte, md5Checksum *[16]byte) (*Metadata, error) {
+	var metadata Metadata
+	if err := json.Unmarshal(*stateFile, &metadata); err != nil {
+		return nil, fmt.Errorf("%w: %v", ErrParseStateFile, err)
+	}
+
+	isEncrypted, err := isEncryptedPayload(stateFile)
+	if err != nil {
+		return nil, err
+	}
+	metadata.Encrypted = isEncrypted
+
+	if md5Checksum != nil {
+		metadata.ChecksumMD5 = *md5Checksum
+	} else {
+		metadata.ChecksumMD5 = md5.Sum(*stateFile)
+	}
+
+	return &metadata, nil
+}
+
+// isEncryptedPayload detects whether or not the OpenTofu/Terraform state file's
+// payload is encrypted.
+func isEncryptedPayload(data *[]byte) (bool, error) {
+	type EncryptionMetadata struct {
+		Data    []byte            `json:"encrypted_data"`
+		Meta    map[string][]byte `json:"meta"`
+		Version string            `json:"encryption_version"`
+	}
+
+	var encryptionMetadata EncryptionMetadata
+	if err := json.Unmarshal(*data, &encryptionMetadata); err != nil {
+		return false, fmt.Errorf("%w: %v", ErrParseEncryptionMetadata, err)
+	}
+
+	return encryptionMetadata.Version != "", nil
 }

modules/packages/opentofu/state/metadata_test.go
@@ -1,0 +1,118 @@
+package state
+
+import (
+	"crypto/md5"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+const (
+	unencryptedVersion4StateFile string = `{
+		"version": 4,
+		"terraform_version": "1.9.1",
+		"serial": 1,
+		"lineage": "fcf2c3a0-3a0f-703f-c611-0ca7776c06b8",
+		"outputs": {},
+		"resources": [],
+		"check_results": null
+	}`
+
+	encryptedVersion4StateFile string = `{
+		"serial": 1,
+		"lineage": "2ce64317-825a-f8cd-b35e-84fa110b561f",
+		"meta": {
+			"key_provider.pbkdf2.local": "eyJzYWx0IjoiNW9WSGpZM3BGaHRaNncwUE1hSDkrcnc2UzR1cjE4Q05adUR4RXNWM1dmcz0iLCJpdGVyYXRpb25zIjo2MDAwMDAsImhhc2hfZnVuY3Rpb24iOiJzaGE1MTIiLCJrZXlfbGVuZ3RoIjozMn0="
+		},
+		"encrypted_data": "e9oQmRWzPOcELCgmi1uJd5vfKMSnO7oQe/Mpyo8pKEXyNwChoPs0HpDeFhf2h32diyUORX28rWQ+D+wotHseJ+9c6EeE/G3TNr+ilmvdy0nKqZ4ABw/YoD6Zwn+DKF4qviUK2pnXN3HOQqgHUMmiuzL/AqIz1gDBncDyKJLdzuAXjXI1NyFRtCNJmVJpJkyq0ZhQG/i1KxbxAQtCAySH8T3KPE/njy4X7E08vy29rHtGxF0=",
+		"encryption_version": "v0"
+	}`
+)
+
+func TestParseMetadata(t *testing.T) {
+	t.Run("InvalidJSON", func(t *testing.T) {
+		stateFile := []byte("This is not a JSON file")
+
+		metadata, err := ParseMetadataFromStateFile(&stateFile, nil)
+		assert.Nil(t, metadata)
+		require.ErrorIs(t, err, ErrParseStateFile)
+	})
+
+	t.Run("ValidUnencryptedVersion4StateFile", func(t *testing.T) {
+		stateFile := []byte(unencryptedVersion4StateFile)
+
+		metadata, err := ParseMetadataFromStateFile(&stateFile, nil)
+		assert.Equal(t, Metadata{
+			ChecksumMD5:      md5.Sum(stateFile),
+			Encrypted:        false,
+			Lineage:          "fcf2c3a0-3a0f-703f-c611-0ca7776c06b8",
+			OpenTofuVersion:  "1.9.1",
+			Serial:           1,
+			StateFileVersion: 4,
+		}, *metadata)
+		assert.NoError(t, err)
+	})
+
+	t.Run("ValidUnencryptedVersion4StateFileWithChecksum", func(t *testing.T) {
+		stateFile := []byte(unencryptedVersion4StateFile)
+		md5Checksum := md5.Sum(stateFile)
+
+		metadata, err := ParseMetadataFromStateFile(&stateFile, &md5Checksum)
+		assert.Equal(t, Metadata{
+			ChecksumMD5:      md5.Sum(stateFile),
+			Encrypted:        false,
+			Lineage:          "fcf2c3a0-3a0f-703f-c611-0ca7776c06b8",
+			OpenTofuVersion:  "1.9.1",
+			Serial:           1,
+			StateFileVersion: 4,
+		}, *metadata)
+		assert.NoError(t, err)
+	})
+
+	t.Run("ValidEncryptedV4StateFile", func(t *testing.T) {
+		stateFile := []byte(encryptedVersion4StateFile)
+
+		metadata, err := ParseMetadataFromStateFile(&stateFile, nil)
+		assert.Equal(t, Metadata{
+			ChecksumMD5: md5.Sum(stateFile),
+			Encrypted:   true,
+			Lineage:     "2ce64317-825a-f8cd-b35e-84fa110b561f",
+			Serial:      1,
+		}, *metadata)
+		assert.NoError(t, err)
+	})
+}
+
+func TestIsEncryptedPayload(t *testing.T) {
+	t.Run("InvalidJSON", func(t *testing.T) {
+		stateFile := []byte("This is not a JSON file")
+
+		isEncrypted, err := isEncryptedPayload(&stateFile)
+		assert.False(t, isEncrypted)
+		require.ErrorIs(t, err, ErrParseEncryptionMetadata)
+	})
+
+	t.Run("MissingEncryptionVersion", func(t *testing.T) {
+		stateFile := []byte(`{
+			"serial": 1,
+			"lineage": "2ce64317-825a-f8cd-b35e-84fa110b561f",
+			"meta": {
+				"key_provider.pbkdf2.local": "eyJzYWx0IjoiNW9WSGpZM3BGaHRaNncwUE1hSDkrcnc2UzR1cjE4Q05adUR4RXNWM1dmcz0iLCJpdGVyYXRpb25zIjo2MDAwMDAsImhhc2hfZnVuY3Rpb24iOiJzaGE1MTIiLCJrZXlfbGVuZ3RoIjozMn0="
+			},
+			"encrypted_data": "e9oQmRWzPOcELCgmi1uJd5vfKMSnO7oQe/Mpyo8pKEXyNwChoPs0HpDeFhf2h32diyUORX28rWQ+D+wotHseJ+9c6EeE/G3TNr+ilmvdy0nKqZ4ABw/YoD6Zwn+DKF4qviUK2pnXN3HOQqgHUMmiuzL/AqIz1gDBncDyKJLdzuAXjXI1NyFRtCNJmVJpJkyq0ZhQG/i1KxbxAQtCAySH8T3KPE/njy4X7E08vy29rHtGxF0="
+		}`)
+
+		isEncrypted, err := isEncryptedPayload(&stateFile)
+		assert.False(t, isEncrypted)
+		assert.NoError(t, err)
+	})
+
+	t.Run("ValidEncryptedVersion4StateFile", func(t *testing.T) {
+		stateFile := []byte(encryptedVersion4StateFile)
+
+		isEncrypted, err := isEncryptedPayload(&stateFile)
+		assert.True(t, isEncrypted)
+		assert.NoError(t, err)
+	})
+}