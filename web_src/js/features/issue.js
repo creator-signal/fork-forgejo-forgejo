0562feaaaf4f89b6cefac88baab3a393a77e80e8
Maxim Slipenko <maxim@slipenko.com>
2026-02-17 12:11:37 +0300

    chore: add copyright

--- web_src/js/features/issue.js
+++ web_src/js/features/issue.js
@@ -1,3 +1,6 @@
+// Copyright 2026 The Forgejo Authors. All rights reserved.
+// SPDX-License-Identifier: GPL-3.0-or-later
+
 export function getIssueIcon(issue) {
   if (issue.is_pr) {
     if (issue.state === 'open') {
