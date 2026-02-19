f694a684963e5492698728cdbf0c96acfc83072c
Schmerling <schmerling@noreply.codeberg.org>
2026-02-19 15:17:51 +0100

    modules/templates/util_avatar.go aktualisiert

--- modules/templates/util_avatar.go
+++ modules/templates/util_avatar.go
@@ -3,6 +3,20 @@
 
 package templates
 
+import (
+	"context"
+	"fmt"
+	"html"
+	"html/template"
+
+	"forgejo.org/models/avatars"
+	"forgejo.org/models/organization"
+	repo_model "forgejo.org/models/repo"
+	user_model "forgejo.org/models/user"
+	gitea_html "forgejo.org/modules/html"
+	"forgejo.org/modules/setting"
+)
+
 type AvatarUtils struct {
 	ctx context.Context
 }
