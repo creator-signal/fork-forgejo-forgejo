live-clones/forgejo

feat: add APIContext.Reducer computed from access token

services/auth/basic.go
@@ -17,6 +17,7 @@ import (
 	"forgejo.org/modules/setting"
 	"forgejo.org/modules/util"
 	"forgejo.org/modules/web/middleware"
+	"forgejo.org/services/authz"
 )
 
 // Ensure the struct implements the interface.
@@ -102,6 +103,14 @@ func (b *Basic) Verify(req *http.Request, w http.ResponseWriter, store DataStore
 
 		store.GetData()["IsApiToken"] = true
 		store.GetData()["ApiTokenScope"] = token.Scope
+
+		reducer, err := authz.ComputeAuthorizationReducerForAccessToken(req.Context(), token)
+		if err != nil {
+			log.Error("authz.ComputeAuthorizationReducerForAccessToken: %v", err)
+			return nil, err
+		}
+		store.GetData()["ApiTokenReducer"] = reducer
+
 		return u, nil
 	} else if !auth_model.IsErrAccessTokenNotExist(err) && !auth_model.IsErrAccessTokenEmpty(err) {
 		log.Error("GetAccessTokenBySha: %v", err)

services/auth/oauth2.go
@@ -20,6 +20,7 @@ import (
 	"forgejo.org/modules/web/middleware"
 	"forgejo.org/services/actions"
 	"forgejo.org/services/auth/source/oauth2"
+	"forgejo.org/services/authz"
 )
 
 // Ensure the struct implements the interface.
@@ -200,6 +201,14 @@ func (o *OAuth2) userIDFromToken(ctx context.Context, tokenSHA string, store Dat
 	}
 	store.GetData()["IsApiToken"] = true
 	store.GetData()["ApiTokenScope"] = t.Scope
+
+	reducer, err := authz.ComputeAuthorizationReducerForAccessToken(ctx, t)
+	if err != nil {
+		log.Error("authz.ComputeAuthorizationReducerForAccessToken: %v", err)
+		return 0, err
+	}
+	store.GetData()["ApiTokenReducer"] = reducer
+
 	return t.UID, nil
 }
 

creator-signal/fork-forgejo-forgejo

services/auth/basic.go
@@ -17,6 +17,7 @@ import (
 	"forgejo.org/modules/setting"
 	"forgejo.org/modules/util"
 	"forgejo.org/modules/web/middleware"
+	"forgejo.org/services/authz"
 )
 
 // Ensure the struct implements the interface.
@@ -102,6 +103,14 @@ func (b *Basic) Verify(req *http.Request, w http.ResponseWriter, store DataStore
 
 		store.GetData()["IsApiToken"] = true
 		store.GetData()["ApiTokenScope"] = token.Scope
+
+		reducer, err := authz.GetAuthorizationReducerForAccessToken(req.Context(), token)
+		if err != nil {
+			log.Error("authz.GetAuthorizationReducerForAccessToken: %v", err)
+			return nil, err
+		}
+		store.GetData()["ApiTokenReducer"] = reducer
+
 		return u, nil
 	} else if !auth_model.IsErrAccessTokenNotExist(err) && !auth_model.IsErrAccessTokenEmpty(err) {
 		log.Error("GetAccessTokenBySha: %v", err)

services/auth/oauth2.go
@@ -20,6 +20,7 @@ import (
 	"forgejo.org/modules/web/middleware"
 	"forgejo.org/services/actions"
 	"forgejo.org/services/auth/source/oauth2"
+	"forgejo.org/services/authz"
 )
 
 // Ensure the struct implements the interface.
@@ -200,6 +201,14 @@ func (o *OAuth2) userIDFromToken(ctx context.Context, tokenSHA string, store Dat
 	}
 	store.GetData()["IsApiToken"] = true
 	store.GetData()["ApiTokenScope"] = t.Scope
+
+	reducer, err := authz.GetAuthorizationReducerForAccessToken(ctx, t)
+	if err != nil {
+		log.Error("authz.GetAuthorizationReducerForAccessToken: %v", err)
+		return 0, err
+	}
+	store.GetData()["ApiTokenReducer"] = reducer
+
 	return t.UID, nil
 }
 