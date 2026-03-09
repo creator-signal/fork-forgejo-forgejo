f7b52319a0ff3fedcdd23a29ebff404a4bbfdd76
Michael Kriese <michael.kriese@visualon.de>
2026-03-09 22:17:18 +0100

    test: split tests

--- routers/api/v1/repo/action_test.go
+++ routers/api/v1/repo/action_test.go
@@ -18,56 +18,98 @@ func TestActions(t *testing.T) {
 	unittest.PrepareTestEnv(t)
 
 	t.Run("ListActionRuns", func(t *testing.T) {
-		var runResp api.ListActionRunResponse
-		ctx, resp := contexttest.MockAPIContext(t, "user5/repo4/actions/runs")
-		contexttest.LoadRepo(t, ctx, 4)
 
-		ctx.SetFormString("status", "some-invalid-value")
-		ListActionRuns(ctx)
-		assert.Equal(t, http.StatusBadRequest, resp.Code)
-		resp.Body.Reset()
-		resp.Code = 200
-		ctx.Req.Form.Del("status")
+		t.Run("paging", func(t *testing.T) {
+			var runResp api.ListActionRunResponse
+			ctx, resp := contexttest.MockAPIContext(t, "user5/repo4/actions/runs")
+			contexttest.LoadRepo(t, ctx, 4)
 
-		ctx.SetFormString("status", "failure")
-		ctx.SetFormString("limit", "1")
-		ctx.SetFormString("page", "1")
+			ctx.SetFormString("limit", "1")
+			ctx.SetFormString("page", "1")
 
-		ListActionRuns(ctx)
-		assert.Equal(t, http.StatusOK, resp.Code)
-		contexttest.DecodeJSON(t, resp, &runResp)
-		assert.Len(t, runResp.Entries, 1)
-		assert.Equal(t, int64(2), runResp.TotalCount)
+			ListActionRuns(ctx)
+			assert.Equal(t, http.StatusOK, resp.Code)
+			contexttest.DecodeJSON(t, resp, &runResp)
+			assert.Len(t, runResp.Entries, 1)
+			assert.Equal(t, int64(6), runResp.TotalCount)
+		})
 
-		ctx.SetFormString("workflow_id", "some.yml")
-		resp.Body.Reset()
-		resp.Code = 200
-		ListActionRuns(ctx)
-		assert.Equal(t, http.StatusOK, resp.Code)
-		contexttest.DecodeJSON(t, resp, &runResp)
-		assert.Empty(t, runResp.Entries)
-		assert.Equal(t, int64(0), runResp.TotalCount)
+		t.Run("invalid status filter", func(t *testing.T) {
+			ctx, resp := contexttest.MockAPIContext(t, "user5/repo4/actions/runs")
+			contexttest.LoadRepo(t, ctx, 4)
+
+			ctx.SetFormString("status", "some-invalid-value")
+			ListActionRuns(ctx)
+			assert.Equal(t, http.StatusBadRequest, resp.Code)
+		})
+
+		t.Run("filtered by status", func(t *testing.T) {
+			ctx, resp := contexttest.MockAPIContext(t, "user5/repo4/actions/runs")
+			contexttest.LoadRepo(t, ctx, 4)
+
+			ctx.SetFormString("status", "failure")
+
+			ListActionRuns(ctx)
+			assert.Equal(t, http.StatusOK, resp.Code)
+			var runResp api.ListActionRunResponse
+			contexttest.DecodeJSON(t, resp, &runResp)
+			assert.Len(t, runResp.Entries, 2)
+			assert.Equal(t, int64(2), runResp.TotalCount)
+		})
+
+		t.Run("filtered by workflow_id", func(t *testing.T) {
+			ctx, resp := contexttest.MockAPIContext(t, "user5/repo4/actions/runs")
+			contexttest.LoadRepo(t, ctx, 4)
+
+			ctx.SetFormString("workflow_id", "some.yml")
+
+			ListActionRuns(ctx)
+			assert.Equal(t, http.StatusOK, resp.Code)
+			var runResp api.ListActionRunResponse
+			contexttest.DecodeJSON(t, resp, &runResp)
+			assert.Empty(t, runResp.Entries)
+			assert.Equal(t, int64(0), runResp.TotalCount)
+		})
 	})
 
 	t.Run("ListActionTasks", func(t *testing.T) {
-		var taskResp api.ActionTaskResponse
-		ctx, resp := contexttest.MockAPIContext(t, "user5/repo4/actions/tasks")
-		contexttest.LoadRepo(t, ctx, 4)
 
-		ctx.SetFormString("status", "some-invalid-value")
-		ListActionTasks(ctx)
-		assert.Equal(t, http.StatusBadRequest, resp.Code)
-		resp.Body.Reset()
-		resp.Code = 200
+		t.Run("paging", func(t *testing.T) {
+			var taskResp api.ActionTaskResponse
+			ctx, resp := contexttest.MockAPIContext(t, "user5/repo4/actions/tasks")
+			contexttest.LoadRepo(t, ctx, 4)
 
-		ctx.SetFormString("status", "failure")
-		ctx.SetFormString("limit", "1")
-		ctx.SetFormString("page", "1")
+			ctx.SetFormString("limit", "1")
+			ctx.SetFormString("page", "1")
 
-		ListActionTasks(ctx)
-		assert.Equal(t, http.StatusOK, resp.Code)
-		contexttest.DecodeJSON(t, resp, &taskResp)
-		assert.Len(t, taskResp.Entries, 1)
-		assert.Equal(t, int64(2), taskResp.TotalCount)
+			ListActionTasks(ctx)
+			assert.Equal(t, http.StatusOK, resp.Code)
+			contexttest.DecodeJSON(t, resp, &taskResp)
+			assert.Len(t, taskResp.Entries, 1)
+			assert.Equal(t, int64(12), taskResp.TotalCount)
+		})
+
+		t.Run("invalid status filter", func(t *testing.T) {
+			ctx, resp := contexttest.MockAPIContext(t, "user5/repo4/actions/tasks")
+			contexttest.LoadRepo(t, ctx, 4)
+
+			ctx.SetFormString("status", "some-invalid-value")
+			ListActionTasks(ctx)
+			assert.Equal(t, http.StatusBadRequest, resp.Code)
+		})
+
+		t.Run("filtered by status", func(t *testing.T) {
+			ctx, resp := contexttest.MockAPIContext(t, "user5/repo4/actions/tasks")
+			contexttest.LoadRepo(t, ctx, 4)
+
+			ctx.SetFormString("status", "failure")
+
+			ListActionTasks(ctx)
+			assert.Equal(t, http.StatusOK, resp.Code)
+			var taskResp api.ActionTaskResponse
+			contexttest.DecodeJSON(t, resp, &taskResp)
+			assert.Len(t, taskResp.Entries, 2)
+			assert.Equal(t, int64(2), taskResp.TotalCount)
+		})
 	})
 }
