f5430ffa3ca75c7b11698250ceb9698019e75332
Melissa Geels <melissa@nimble.tools>
2026-03-28 23:48:56 +0100

    Fixed migration error due to use of user_models.User instead of an anonymous simplified User type

--- models/forgejo_migrations/v14a_set_remote_user_prohibit_login.go
+++ models/forgejo_migrations/v14a_set_remote_user_prohibit_login.go
@@ -23,8 +23,11 @@ func init() {
 
 func setProhibitLoginActivityPubUser(x *xorm.Engine) error {
 	return db.WithTx(db.DefaultContext, func(ctx context.Context) error {
-		return db.Iterate(ctx, builder.Eq{"type": 5}, func(ctx context.Context, user *user_model.User) error {
-			log.Info("Checking if user %s is created from ActivityPub", user.LogString())
+		type User struct {
+			ID int64 `xorm:"pk autoincr"`
+		}
+		return db.Iterate(ctx, builder.Eq{"type": 5}, func(ctx context.Context, user *User) error {
+			log.Info("Checking if user %d is created from ActivityPub", user.ID)
 
 			// Users created from f3 also have the RemoteUser user type. All
 			// FederatedUser should reference exactly one User.
@@ -37,7 +40,7 @@ func setProhibitLoginActivityPubUser(x *xorm.Engine) error {
 				return nil
 			}
 
-			log.Info("Updating user %s", user.LogString())
+			log.Info("Updating user %d", user.ID)
 			_, err = db.GetEngine(ctx).Table("user").ID(user.ID).Cols("type", "prohibit_login", "passwd", "salt", "passwd_hash_algo").Update(&user_model.User{
 				Type:           user_model.UserTypeActivityPubUser,
 				ProhibitLogin:  true,
