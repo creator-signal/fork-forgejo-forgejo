232f7a4c0b32c22de89bf3ecf3f92ac9cc9954a4
Zachary Spector <public@zacharyspector.com>
2026-04-12 12:44:03 +1200

    Switch to `slices.SortFunc` in pypi

--- routers/api/packages/pypi/pypi.go
+++ routers/api/packages/pypi/pypi.go
@@ -8,6 +8,7 @@ import (
 	"io"
 	"net/http"
 	"regexp"
+	"slices"
 	"sort"
 	"strings"
 	"unicode"
@@ -96,7 +97,7 @@ func JSONPackageMetadata(ctx *contex.Context) {
 	}
 
 	// sort package descriptors by version to mimic PyPI format
-	sort.Slice(pds, func(i, j int) bool {
+	slices.SortFunc(pds, func(i, j int) bool {
 		return strings.Compare(pds[i].Version.Version, pds[j].Version.Version) < 0
 	})
 	ctx.Data["RegistryURL"] = setting.AppURL + "api/packages/" + ctx.Package.Owner.Name + "/pypi"
