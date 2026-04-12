5cf7a7743c164e7efcf2c9bee73acc8b74103d88
Florian Lackner <git@lackner.work>
2026-04-12 17:47:24 +0200

    add PublicKeySigning model

---
+++ models/asymkey/ssh_signing_key.go
@@ -0,0 +1,35 @@
+// Copyright 2014 The Gogs Authors. All rights reserved.
+// Copyright 2019 The Gitea Authors. All rights reserved.
+// SPDX-License-Identifier: MIT
+
+package asymkey
+
+import (
+	"strings"
+
+	"forgejo.org/models/db"
+	"forgejo.org/modules/timeutil"
+)
+
+// PublicKeySigning represents an SSH public key used for git object signatures.
+type PublicKeySigning struct {
+	ID          int64  `xorm:"pk autoincr"`
+	OwnerID     int64  `xorm:"INDEX NOT NULL"`
+	Name        string `xorm:"NOT NULL"`
+	Fingerprint string `xorm:"INDEX NOT NULL"`
+	Content     string `xorm:"MEDIUMTEXT NOT NULL"`
+
+	CreatedUnix timeutil.TimeStamp `xorm:"created"`
+	UpdatedUnix timeutil.TimeStamp `xorm:"updated"`
+	// true if the user proved via WebUI that they possess the private key
+	Verified bool `xorm:"NOT NULL DEFAULT false"`
+}
+
+// OmitEmail returns content of public key without email address.
+func (key *PublicKeySigning) OmitEmail() string {
+	return strings.Join(strings.Split(key.Content, " ")[:2], " ")
+}
+
+func init() {
+	db.RegisterModel(new(PublicKeySigning))
+}
