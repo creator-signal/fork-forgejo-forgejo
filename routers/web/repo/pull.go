ad9a464a9d4b2b0701b76abf0c58af212c4fcd35
Mathieu Fenniak <mathieu@fenniak.net>
2026-04-14 17:18:14 +0200

    fix: when reviewing in PRs, make comments relative to viewed base & head, not just viewed head (#12107)
    While developing tests for #12092, I came across a case where making a comment on a single-commit doesn't include the correct diff for the comment.  This is because code comment placement occurs between the PR's base and the commit being viewed, but, that diff could be different from the commit's parent to the commit, which is what is being viewed on a single-commit diff.
    
    Similar to #12055, this PR changes code comments to be more precise in their diff generation by providing the backend with both the base commit (`before_commit_id`) and head commit (`after_commit_id`) currently being viewed.  As a result, the diffs attached to comments should exactly match the diffs being viewed by the user when the comment was placed.
    
    ## Checklist
    
    The [contributor guide](https://forgejo.org/docs/next/contributor/) contains information that will be helpful to first time contributors. All work and communication must conform to Forgejo's [AI Agreement](https://codeberg.org/forgejo/governance/src/branch/main/AIAgreement.md). There also are a few [conditions for merging Pull Requests in Forgejo repositories](https://codeberg.org/forgejo/governance/src/branch/main/PullRequestsAgreement.md). You are also welcome to join the [Forgejo development chatroom](https://matrix.to/#/#forgejo-development:matrix.org).
    
    ### Tests for Go changes
    
    - I added test coverage for Go changes...
      - [ ] in their respective `*_test.go` for unit tests.
      - [x] in the `tests/integration` directory if it involves interactions with a live Forgejo server.
    - I ran...
      - [x] `make pr-go` before pushing
    
    ### Documentation
    
    - [ ] I created a pull request [to the documentation](https://codeberg.org/forgejo/docs) to explain to Forgejo users how to use this change.
    - [x] I did not document these changes and I do not expect someone else to do it.
    
    ### Release notes
    
    - [x] This change will be noticed by a Forgejo user or admin (feature, bug fix, performance, etc.). I suggest to include a release note for this change.
    - [ ] This change is not visible to a Forgejo user or admin (refactor, dependency upgrade, etc.). I think there is no need to add a release note for this change.
    
    Reviewed-on: https://codeberg.org/forgejo/forgejo/pulls/12107
    Reviewed-by: Andreas Ahlenstorf <aahlenst@noreply.codeberg.org>
    Co-authored-by: Mathieu Fenniak <mathieu@fenniak.net>
    Co-committed-by: Mathieu Fenniak <mathieu@fenniak.net>

--- routers/web/repo/pull.go
+++ routers/web/repo/pull.go
@@ -1032,7 +1032,11 @@ func viewPullFiles(ctx *context.Context, specifiedStartCommit, specifiedEndCommi
 		}
 
 		endCommitID = commitID
-		startCommitID = prInfo.MergeBase
+		if prevCommit != nil {
+			startCommitID = prevCommit.ID.String()
+		} else {
+			startCommitID = prInfo.MergeBase
+		}
 		ctx.Data["IsShowingAllCommits"] = false
 	} else if willShowSpecifiedCommitRange {
 		if len(specifiedEndCommit) > 0 {
