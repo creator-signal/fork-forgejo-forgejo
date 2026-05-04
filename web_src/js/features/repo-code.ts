3f10e1c4de18d4645b486fb9d48569b229687394
Andres Rodriguez <andresmichelrodriguez@gmail.com>
2026-05-03 17:38:02 -0700

    feat: add shortcuts to repo page

--- web_src/js/features/repo-code.ts
+++ web_src/js/features/repo-code.ts
@@ -125,6 +125,53 @@ function showLineButton() {
   });
 }
 
+function keyboardSelector(up: boolean) {
+  const subpath = window.location.pathname.split('/')[3];
+  let query = '';
+  switch (subpath) {
+    case 'issues':
+    case 'pulls':
+      query = '#issue-list';
+      break;
+    case 'projects':
+      query = '.milestone-list';
+      break;
+    case 'src':
+      query = '#repo-files-table tbody';
+      break;
+    case 'wiki':
+      query = '.wiki-pages-list tbody';
+      break;
+    default:
+      if (window.location.pathname.split('/').length === 3) {
+        query = '#repo-files-table tbody';
+      } else return;
+  }
+  const rows = Array.from(document.querySelector(query)?.children ?? []) as HTMLElement[];
+  if (rows.length === 0) return;
+  const cur = rows.findIndex((row) =>
+    row.classList.contains('keyboard-selected'),
+  );
+  if (cur === -1) {
+    rows[0].classList.add('keyboard-selected');
+    rows[0].scrollIntoView({block: 'nearest'});
+    return;
+  }
+  const el = rows[cur];
+  if (up) {
+    if (el.previousElementSibling) {
+      el.previousElementSibling.classList.add('keyboard-selected');
+      el.previousElementSibling.scrollIntoView({block: 'nearest'});
+    } else return;
+  } else {
+    if (el.nextElementSibling) {
+      el.nextElementSibling.classList.add('keyboard-selected');
+      el.nextElementSibling.scrollIntoView({block: 'nearest'});
+    } else return;
+  }
+  rows[cur].classList.remove('keyboard-selected');
+}
+
 export function initRepoCodeView() {
   if (document.querySelector('.code-view .lines-num')) {
     document.addEventListener('click', (e) => {
@@ -203,5 +250,55 @@ export function initRepoCodeView() {
     }
 
     await clippie(toAbsoluteUrl(target.getAttribute('data-url')));
+  });
+  document.addEventListener('keydown', (e) => {
+    if (
+      e.altKey ||
+      e.ctrlKey ||
+      e.metaKey ||
+      e.shiftKey ||
+      !(e.target instanceof Element) ||
+      e.target.closest(
+        'input, textarea, [contenteditable], .CodeMirror, .cm-editor',
+      )
+    ) return;
+    switch (e.key) {
+      case 'Enter':
+        if (e.target.tagName !== 'BUTTON') {
+          document
+            .querySelector<HTMLAnchorElement>('.keyboard-selected a')
+            ?.click();
+        }
+        break;
+      case 'a':
+        document.querySelector<HTMLAnchorElement>('#repo-actions-tab')?.click();
+        break;
+      case 'c':
+        document.querySelector<HTMLAnchorElement>('#repo-code-tab')?.click();
+        break;
+      case 'i':
+        document.querySelector<HTMLAnchorElement>('#repo-issues-tab')?.click();
+        break;
+      case 'j':
+        (e.target as HTMLInputElement).blur();
+        keyboardSelector(false);
+        break;
+      case 'k':
+        (e.target as HTMLInputElement).blur();
+        keyboardSelector(true);
+        break;
+      case 'o':
+        document.querySelector<HTMLAnchorElement>('#repo-projects-tab')?.click();
+        break;
+      case 'p':
+        document.querySelector<HTMLAnchorElement>('#repo-pull-requests-tab')?.click();
+        break;
+      case 'r':
+        document.querySelector<HTMLAnchorElement>('#repo-releases-tab')?.click();
+        break;
+      case 'w':
+        document.querySelector<HTMLAnchorElement>('#repo-wiki-tab')?.click();
+        break;
+    }
   });
 }
