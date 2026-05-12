c884369e2359fcb532a8a305adb320fd09674d9c
Gusted <postmaster@gusted.xyz>
2026-05-12 16:25:15 +0200

    chore: remove `ENABLE_AUTO_GIT_WIRE_PROTOCOL` option
    Its sole usage is to set `-c protocol.version=2` on each git command
    execution. The default value is already 2 since at least the minimum
    version of Git that Forgejo requires. When this setting was added, this
    was not the case.
    
    Thus, automatically defaulting to protocol v2 is already the case due to
    git themselves making it the default. And instances that want to use a
    older protocol already have to override the value like:
    
    ```ini
    [git.config]
    protocol.version=1
    ```

--- modules/setting/git.go
+++ modules/setting/git.go
@@ -17,18 +17,17 @@ var Git = struct {
 	HomePath             string
 	DisableDiffHighlight bool
 
-	MaxGitDiffLines           int
-	MaxGitDiffLineCharacters  int
-	MaxGitDiffFiles           int
-	CommitsRangeSize          int // CommitsRangeSize the default commits range size
-	BranchesRangeSize         int // BranchesRangeSize the default branches range size
-	VerbosePush               bool
-	VerbosePushDelay          time.Duration
-	GCArgs                    []string `ini:"GC_ARGS" delim:" "`
-	EnableAutoGitWireProtocol bool
-	PullRequestPushMessage    bool
-	DisablePartialClone       bool
-	Timeout                   struct {
+	MaxGitDiffLines          int
+	MaxGitDiffLineCharacters int
+	MaxGitDiffFiles          int
+	CommitsRangeSize         int // CommitsRangeSize the default commits range size
+	BranchesRangeSize        int // BranchesRangeSize the default branches range size
+	VerbosePush              bool
+	VerbosePushDelay         time.Duration
+	GCArgs                   []string `ini:"GC_ARGS" delim:" "`
+	PullRequestPushMessage   bool
+	DisablePartialClone      bool
+	Timeout                  struct {
 		Default int
 		Migrate int
 		Mirror  int
@@ -38,18 +37,17 @@ var Git = struct {
 		Grep    int
 	} `ini:"git.timeout"`
 }{
-	DisableDiffHighlight:      false,
-	MaxGitDiffLines:           1000,
-	MaxGitDiffLineCharacters:  5000,
-	MaxGitDiffFiles:           100,
-	CommitsRangeSize:          50,
-	BranchesRangeSize:         20,
-	VerbosePush:               true,
-	VerbosePushDelay:          5 * time.Second,
-	GCArgs:                    []string{},
-	EnableAutoGitWireProtocol: true,
-	PullRequestPushMessage:    true,
-	DisablePartialClone:       false,
+	DisableDiffHighlight:     false,
+	MaxGitDiffLines:          1000,
+	MaxGitDiffLineCharacters: 5000,
+	MaxGitDiffFiles:          100,
+	CommitsRangeSize:         50,
+	BranchesRangeSize:        20,
+	VerbosePush:              true,
+	VerbosePushDelay:         5 * time.Second,
+	GCArgs:                   []string{},
+	PullRequestPushMessage:   true,
+	DisablePartialClone:      false,
 	Timeout: struct {
 		Default int
 		Migrate int
