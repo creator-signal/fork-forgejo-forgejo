2c7383ca20d1784df029271ff546c58d19c05dd4
numen <pgodfsab@gmail.com>
2026-05-19 15:59:29 -0400

    fix: wrong assert method on test
    Signed-off-by: numen <pgodfsab@gmail.com>

--- tests/integration/api_org_test.go
+++ tests/integration/api_org_test.go
@@ -49,7 +49,7 @@ func TestAPIOrgCreate(t *testing.T) {
 	assert.Equal(t, org.Website, apiOrg.Website)
 	assert.Equal(t, org.Location, apiOrg.Location)
 	assert.Equal(t, org.Visibility, apiOrg.Visibility)
-	assert.NotNil(t, apiOrg.Created)
+	assert.False(t, apiOrg.Created.IsZero())
 
 	unittest.AssertExistsAndLoadBean(t, &user_model.User{
 		Name:      org.UserName,
