b06d88b55ab340ccf737dc58eac4d3234710ec3c
Gus Libens <goestav@noreply.codeberg.org>
2026-05-21 21:00:41 +0200

    refactor: improve feed channel description

--- routers/web/feed/issue.go
+++ routers/web/feed/issue.go
@@ -28,17 +28,20 @@ func ShowIssueFeed(ctx *context.Context, issue *issues_model.Issue, formatType s
 	}
 
 	titleFormat := "Issue %s/%s#%d: %s"
+	descriptionFormat := "Updates on issue %s/%s#%d by %s"
 
 	if issue.IsPull {
 		titleFormat = "Pull request %s/%s!%d: %s"
+		descriptionFormat = "Updates on pull request %s/%s!%d by %s"
 	}
 
 	title := fmt.Sprintf(titleFormat, issue.Repo.OwnerName, issue.Repo.Name, issue.Index, issue.Title)
 
+
 	feed := &feeds.Feed{
 		Title:       ctx.Locale.TrString("home.feed_of", title),
 		Link:        &feeds.Link{Href: issue.HTMLURL()},
-		Description: issue.Content,
+		Description: fmt.Sprintf(descriptionFormat, issue.Repo.OwnerName, issue.Repo.Name, issue.Index, issue.Poster.DisplayName()),
 		Created:     time.Now(),
 	}
 
