4bf2f137ca4aac9d143c87c0d814c204a71c0b3d
Steven Turturo <steven@turturo.dev>
2026-05-29 01:31:12 -0400

    chore: add some table tests for the IsErr* methods

--- models/error_test.go
+++ models/error_test.go
@@ -5,31 +5,118 @@ import (
 	"testing"
 )
 
-func Test_IsErrRepoFileDoesNotExist(t *testing.T) {
-	t.Run("direct_error_returns_true", func(t *testing.T) {
-		input := ErrRepoFileDoesNotExist{
-			Path: "/some/arbitrary/path",
-			Name: "repo-file-does-exist",
-		}
-
-		out := IsErrRepoFileDoesNotExist(input)
-		if !out {
-			t.Errorf("got %v, wanted true", out)
-		}
-	})
-
-	t.Run("wrapped_error_returns_true", func(t *testing.T) {
-		input := fmt.Errorf(
-			"some wrapping of the base error: %w", 
-			ErrRepoFileDoesNotExist{
-			Path: "/path/for/wrapped/error",
-			Name: "wrapped-error-repo",
+func Test_IsErrMethods(t *testing.T) {
+	tests := []struct {
+		name  string
+		input error
+		want  bool
+		fn    func(err error) bool
+	}{
+		{
+			name:  "IsErrUserOwnRepos_direct_returns_true",
+			input: ErrUserOwnRepos{UID: 1},
+			want:  true,
+			fn:    IsErrUserOwnRepos,
 		},
-		)
+		{
+			name:  "IsErrUserOwnRepos_wrapped_returns_true",
+			input: fmt.Errorf("wrapped_ErrUserOwnRepos: %w", ErrUserOwnRepos{UID: 2}),
+			want:  true,
+			fn:    IsErrUserOwnRepos,
+		},
+		{
+			name:  "IsErrUserHasOrgs_direct_returns_true",
+			input: ErrUserHasOrgs{UID: 1},
+			want:  true,
+			fn:    IsErrUserHasOrgs,
+		},
+		{
+			name:  "IsErruserHasOrgs_wrapped_returns_true",
+			input: fmt.Errorf("wrapped_ErrUserHasOrgs: %w", ErrUserHasOrgs{UID: 2}),
+			want:  true,
+			fn:    IsErrUserHasOrgs,
+		},
+		{
+			name:  "IsErrUserOwnPackages_direct_returns_true",
+			input: ErrUserOwnPackages{UID: 1},
+			want:  true,
+			fn:    IsErrUserOwnPackages,
+		},
+		{
+			name:  "IsErrUserOwnPackages_wrapped_returns_true",
+			input: fmt.Errorf("wrapped_ErrUserOwnPackages: %w", ErrUserOwnPackages{UID: 2}),
+			want:  true,
+			fn:    IsErrUserOwnPackages,
+		},
+		{
+			name:  "IsErrDeleteLastAdminUser_direct_returns_true",
+			input: ErrDeleteLastAdminUser{UID: 1},
+			want:  true,
+			fn:    IsErrDeleteLastAdminUser,
+		},
+		{
+			name:  "IsErrUserOwnPackages_wrapped_returns_true",
+			input: fmt.Errorf("wrapped_ErrDeleteLastAdminUser: %w", ErrDeleteLastAdminUser{UID: 2}),
+			want:  true,
+			fn:    IsErrDeleteLastAdminUser,
+		},
 
-		out := IsErrRepoFileDoesNotExist(input)
-		if !out {
-			t.Errorf("got %v, wanted true", out)
-		}
-	})
+		{
+			name:  "IsErrNoPendingTransfer_direct_returns_true",
+			input: ErrNoPendingRepoTransfer{RepoID: 1},
+			want:  true,
+			fn:    IsErrNoPendingTransfer,
+		},
+		{
+			name:  "IsErrNoPendingTransfer_wrapped_returns_true",
+			input: fmt.Errorf("wrapped_ErrNoPendingRepoTransfer: %w", ErrNoPendingRepoTransfer{RepoID: 2}),
+			want:  true,
+			fn:    IsErrNoPendingTransfer,
+		},
+		{
+			name:  "IsErrRepoTransferInProgress_direct_returns_true",
+			input: ErrRepoTransferInProgress{Uname: "direct", Name: "other_name"},
+			want:  true,
+			fn:    IsErrRepoTransferInProgress,
+		},
+		{
+			name:  "IsErrRepoTransferInProgress_wrapped_returns_true",
+			input: fmt.Errorf("wrapped_ErrRepoTransferInProgress: %w", ErrRepoTransferInProgress{Uname: "wrapped", Name: "other_name"}),
+			want:  true,
+			fn:    IsErrRepoTransferInProgress,
+		},
+		{
+			name:  "IsErrInvalidCloneAddr_direct_returns_true",
+			input: &ErrInvalidCloneAddr{},
+			want:  true,
+			fn:    IsErrInvalidCloneAddr,
+		},
+		{
+			name:  "IsErrInvalidCloneAddr_wrapped_returns_true",
+			input: fmt.Errorf("wrapped_ErrInvalidCloneAddr: %w", &ErrInvalidCloneAddr{}),
+			want:  true,
+			fn:    IsErrInvalidCloneAddr,
+		},
+		{
+			name:  "IsErrInvalidTagName_direct_returns_true",
+			input: ErrInvalidTagName{TagName: "v0.0.0"},
+			want:  true,
+			fn:    IsErrInvalidTagName,
+		},
+		{
+			name:  "IsErrInvalidTagName_wrapped_returns_true",
+			input: fmt.Errorf("wrapped_ErrInvalidTagName: %w", ErrInvalidTagName{TagName: "v0.0.0-wrapped"}),
+			want:  true,
+			fn:    IsErrInvalidTagName,
+		},
+	}
+
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			got := tc.fn(tc.input)
+			if got != tc.want {
+				t.Errorf("wanted %v, got %v", tc.want, got)
+			}
+		})
+	}
 }
