0ce544bc40c7f7731cdc76b3c26a96cea58fc0f1
Gusted <postmaster@gusted.xyz>
2026-06-02 14:39:03 +0200

    Fix lint

--- services/pull/comment.go
+++ services/pull/comment.go
@@ -5,6 +5,7 @@ package pull
 
 import (
 	"context"
+	"slices"
 
 	issues_model "forgejo.org/models/issues"
 	repo_model "forgejo.org/models/repo"
@@ -53,8 +54,8 @@ func getCommitIDsFromRepo(ctx context.Context, repo *repo_model.Repository, oldC
 	}
 
 	commitIDs = make([]string, 0, len(commits))
-	for i := len(commits) - 1; i >= 0; i-- {
-		commitIDs = append(commitIDs, commits[i].ID.String())
+	for _, v := range slices.Backward(commits) {
+		commitIDs = append(commitIDs, v.ID.String())
 	}
 
 	return commitIDs, isForcePush, err
