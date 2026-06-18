e0d97934dce5b02922cec674836b771e524c04ba
Gus Libens <goestav@noreply.codeberg.org>
2026-06-18 19:36:13 +0200

    refactor: provide `FeedURL` context for pull request "Commits" and "Files changed" tabs

--- routers/web/repo/pull.go
+++ routers/web/repo/pull.go
@@ -358,6 +358,15 @@ func getPullInfo(ctx *context.Context) (issue *issues_model.Issue, ok bool) {
 	ctx.Data["Title"] = fmt.Sprintf("#%d - %s", issue.Index, emoji.ReplaceAliases(issue.Title))
 	ctx.Data["Issue"] = issue
 
+	if setting.Other.EnableFeed {
+		ctx.Data["EnableFeed"] = true
+		if issue.IsPull {
+			ctx.Data["FeedURL"] = fmt.Sprintf("%s/pulls/%d", ctx.Repo.RepoLink, issue.Index)
+		} else {
+			ctx.Data["FeedURL"] = fmt.Sprintf("%s/issues/%d", ctx.Repo.RepoLink, issue.Index)
+		}
+	}
+
 	if !issue.IsPull {
 		ctx.Redirect(issue.Link())
 		return nil, false
