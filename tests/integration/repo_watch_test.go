29aa5117abad25bc737710799ca1edd537ea806a
christopher-besch <mail@chris-besch.com>
2026-07-04 14:52:06 +0200

    fix: source = true for TestRepoWatch

--- tests/integration/repo_watch_test.go
+++ tests/integration/repo_watch_test.go
@@ -21,6 +21,6 @@ func TestRepoWatch(t *testing.T) {
 		session := loginUser(t, "user2")
 		unittest.AssertNotExistsBean(t, &repo_model.Watch{UserID: 2, RepoID: 3})
 		testEditFile(t, session, "org3", "repo3", "master", "README.md", "Hello, World (Edited for watch)\n")
-		assert.Equal(t, 1, unittest.GetCount(t, &repo_model.Watch{UserID: 2, RepoID: 3}, "source = false"))
+		assert.Equal(t, 1, unittest.GetCount(t, &repo_model.Watch{UserID: 2, RepoID: 3}, "source = true"))
 	})
 }
