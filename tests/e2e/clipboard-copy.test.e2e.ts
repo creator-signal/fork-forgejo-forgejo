07264e68f0b296505cc4a015d42bf073de2db9d8
0ko <0ko@noreply.codeberg.org>
2026-07-11 14:59:22 +0500

    fix(e2e): adapt tests, verify js auto-close

--- tests/e2e/clipboard-copy.test.e2e.ts
+++ tests/e2e/clipboard-copy.test.e2e.ts
@@ -31,41 +31,65 @@ test('copy issue content to clipboard', async ({page}) => {
   const response = await page.goto('/user2/repo1/issues/1');
   expect(response?.status()).toBe(200);
 
-  await page.click('#issue-1 .comment-container .context-menu');
-  await page.locator('#issue-1 .comment-container .menu').getByText('Copy Markdown').click();
+  const areaOfInterest = await page.locator('#issue-1 .comment-container details.dropdown');
 
+  // Open dropdown
+  await areaOfInterest.locator('summary').click();
+  await expect(areaOfInterest).toHaveAttribute('open');
+
+  // Request copy and check if it succeeded
+  await areaOfInterest.locator('.content ul li button').getByText('Copy Markdown').click();
   await expect(async () => {
     const clipboardText = await page.evaluate(() => navigator.clipboard.readText());
     expect(clipboardText).toBe('content for the first issue');
   }).toPass();
+
+  // Dropdown should have been closed
+  await expect(areaOfInterest).not.toHaveAttribute('open');
 });
 
 test('copy comment content copies the original markdown', async ({page}) => {
   const response = await page.goto('/user2/repo1/issues/1');
   expect(response?.status()).toBe(200);
 
-  await page.click('#issuecomment-1001 .comment-container .context-menu');
-  await page.locator('#issuecomment-1001 .comment-container .menu').getByText('Copy Markdown').click();
+  const areaOfInterest = await page.locator('#issuecomment-1001 .comment-container details.dropdown');
 
+  // Open dropdown
+  await areaOfInterest.locator('summary').click();
+  await expect(areaOfInterest).toHaveAttribute('open');
+
+  // Request copy and check if it succeeded
+  await areaOfInterest.locator('.content ul li button').getByText('Copy Markdown').click();
   await expect(async () => {
     const clipboardText = await page.evaluate(() => navigator.clipboard.readText());
     expect(clipboardText).toContain('## Lorem Ipsum');
     expect(clipboardText).toContain('**I am not appealed**');
     expect(clipboardText).toContain('`feature`');
   }).toPass();
+
+  // Dropdown should have been closed
+  await expect(areaOfInterest).not.toHaveAttribute('open');
 });
 
 test('copy pull request content to clipboard', async ({page}) => {
   const response = await page.goto('/user2/repo1/pulls/5');
   expect(response?.status()).toBe(200);
 
-  await page.click('#issue-11 .comment-container .context-menu');
-  await page.locator('#issue-11 .comment-container .menu').getByText('Copy Markdown').click();
+  const areaOfInterest = await page.locator('#issue-11 .comment-container details.dropdown');
 
+  // Open dropdown
+  await areaOfInterest.locator('summary').click();
+  await expect(areaOfInterest).toHaveAttribute('open');
+
+  // Request copy and check if it succeeded
+  await areaOfInterest.locator('.content ul li button').getByText('Copy Markdown').click();
   await expect(async () => {
     const clipboardText = await page.evaluate(() => navigator.clipboard.readText());
     expect(clipboardText).toBe('content for the a pull request');
   }).toPass();
+
+  // Dropdown should have been closed
+  await expect(areaOfInterest).not.toHaveAttribute('open');
 });
 
 test('copy diff file path to clipboard', async ({page}) => {
