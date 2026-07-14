af637018dd8e2aed787c41b22a620fa6370f0636
hest <henrik.starefors@gmail.com>
2026-07-14 20:06:12 +0200

    update the test cases for GC

--- services/repository/gc_test.go
+++ services/repository/gc_test.go
@@ -6,11 +6,13 @@ package repository
 import (
 	"bytes"
 	"testing"
+	"time"
 
 	"forgejo.org/models/db"
 	git_model "forgejo.org/models/git"
 	repo_model "forgejo.org/models/repo"
 	"forgejo.org/models/unittest"
+	"forgejo.org/modules/git"
 	"forgejo.org/modules/lfs"
 	"forgejo.org/modules/setting"
 	"forgejo.org/modules/storage"
@@ -20,19 +22,66 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestRunManualGCForRepo(t *testing.T) {
+func TestRunManualGCForRepo_RemovesOrphanedCommit(t *testing.T) {
 	unittest.PrepareTestEnv(t)
 	defer test.MockVariableValue(&setting.LFS.StartServer, true)()
 	require.NoError(t, storage.Init())
+	defer test.MockVariableValue(&setting.Git.GCArgs, []string{"--prune=now"})()
+
+	author := &git.Signature{Name: "Test", Email: "test@test.com", When: time.Now()}
 
 	repo, err := repo_model.GetRepositoryByOwnerAndName(db.DefaultContext, "user2", "lfs")
 	require.NoError(t, err)
 
-	require.NoError(t, RunManualGCForRepo(t.Context(), repo))
-
-	repoAfter, err := repo_model.GetRepositoryByID(db.DefaultContext, repo.ID)
+	gitRepo, err := git.OpenRepository(t.Context(), repo.RepoPath())
 	require.NoError(t, err)
-	assert.GreaterOrEqual(t, repoAfter.GitSize, int64(0))
+	defer gitRepo.Close()
+
+	headCommit, err := gitRepo.GetBranchCommit("master")
+	require.NoError(t, err)
+
+	firstID, err := gitRepo.CommitTree(author, author, &headCommit.Tree, git.CommitTreeOpts{
+		Parents:   []string{headCommit.ID.String()},
+		Message:   "initial commit",
+		NoGPGSign: true,
+	})
+	require.NoError(t, err)
+	require.NoError(t, gitRepo.SetReference("refs/heads/master", firstID.String()))
+
+	// run GC make sure gitSize is > 0
+	// to verify the initial commit is present
+	require.NoError(t, RunManualGCForRepo(t.Context(), repo))
+	repoAfterFirst, err := repo_model.GetRepositoryByID(db.DefaultContext, repo.ID)
+	require.NoError(t, err)
+	assert.Positive(t, repoAfterFirst.GitSize)
+
+	// Amend commit, overwriting initial commit reference
+	amendedID, err := gitRepo.CommitTree(author, author, &headCommit.Tree, git.CommitTreeOpts{
+		Parents:   []string{headCommit.ID.String()},
+		Message:   "amended commit",
+		NoGPGSign: true,
+	})
+	require.NoError(t, err)
+	require.NoError(t, gitRepo.SetReference("refs/heads/master", amendedID.String()))
+
+	// Both commits exist before GC cleans up
+	assert.True(t, gitRepo.IsObjectExist(firstID.String()))
+	assert.True(t, gitRepo.IsObjectExist(amendedID.String()))
+
+	// Run GC again, now the first commit should be pruned
+	require.NoError(t, RunManualGCForRepo(t.Context(), repo))
+	repoAfterSecond, err := repo_model.GetRepositoryByID(db.DefaultContext, repo.ID)
+	require.NoError(t, err)
+	assert.Less(t, repoAfterSecond.GitSize, repoAfterFirst.GitSize)
+
+	// Open a fresh repo handle to verify first commit is gone
+	// the first git repo hanlde is stale and will not display changes
+	gitRepo2, err := git.OpenRepository(t.Context(), repo.RepoPath())
+	require.NoError(t, err)
+	defer gitRepo2.Close()
+
+	assert.False(t, gitRepo2.IsObjectExist(firstID.String()))
+	assert.True(t, gitRepo2.IsObjectExist(amendedID.String()))
 }
 
 func TestRunManualGCForRepo_RemovesOrphanedLFSObjects(t *testing.T) {
