8c8067e80948aa6a5667102499abd60d1791faf4
Nils Goroll <nils.goroll@uplex.de>
2026-07-16 20:12:04 +0200

    fix: Initialize oauth2 only if enabled (#13483)
    Fixes #13478
    
    Reviewed-on: https://codeberg.org/forgejo/forgejo/pulls/13483
    Reviewed-by: Andreas Ahlenstorf <aahlenst@noreply.codeberg.org>
    Reviewed-by: Gusted <gusted@noreply.codeberg.org>
    (cherry picked from commit f538db4755477e28500520f1842ddf6e4bf2f3ed)

--- services/auth/source/oauth2/init.go
+++ services/auth/source/oauth2/init.go
@@ -34,6 +34,10 @@ var DefaultSigningKey jwtx.SigningKey
 
 // Init initializes the oauth source
 func Init(ctx context.Context) error {
+	if !setting.OAuth2.Enabled {
+		return nil
+	}
+
 	var err error
 	DefaultSigningKey, err = jwtx.InitSigningKey(&setting.OAuth2.KeyCfg.Signing)
 	if err != nil {
