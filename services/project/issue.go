8f74d75ad418ac38df11a3c28038b7ceb1c985e5
erik <erik.seiert@meissa-gmbh.de>
2026-07-28 16:05:53 +0200

    Add service functions
    Co-authored-by: hwipl <hwipl@noreply.codeberg.org>

---
+++ services/project/issue.go
@@ -0,0 +1,117 @@
+// Copyright 2026 The Forgejo Authors. All rights reserved.
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+package project
+
+import (
+	"context"
+	"errors"
+	"fmt"
+
+	"forgejo.org/models/db"
+	issues_model "forgejo.org/models/issues"
+	project_model "forgejo.org/models/project"
+	user_model "forgejo.org/models/user"
+	project_types "forgejo.org/modules/structs"
+	validation "forgejo.org/modules/validation"
+)
+
+func ValidIssueID(ctx context.Context, ownerID int64, issues issues_model.IssueList) error {
+	if _, err := issues.LoadRepositories(ctx); err != nil {
+		return err
+	}
+	for _, issue := range issues { // TODO Is this a case we actually need to check?
+		if issue.Repo.OwnerID != ownerID {
+			return errors.New("Some issue's ownerID is not equal to project's ownerID")
+		}
+	}
+	return nil
+}
+
+// ListProjectIssues Get list of ProjectIssues of project
+func ListProjectIssuesByColumn(ctx context.Context, columnID int64, listOptions db.ListOptions) ([]*project_model.ProjectIssue, int64, error) {
+	col, err := project_model.GetColumn(ctx, columnID)
+	if err != nil {
+		return nil, 0, err
+	}
+	issues, total, err := col.GetIssues(ctx, listOptions)
+	if err != nil {
+		return nil, 0, err
+	}
+	return issues, total, nil
+}
+
+// getProjectIssueByID Get a single ProjectIssue of a project
+func getProjectIssueByID(ctx context.Context, issueID int64) (*project_model.ProjectIssue, error) {
+	issue, err := project_model.GetProjectIssue(ctx, issueID)
+	if err != nil {
+		return nil, err
+	}
+	return issue, nil
+}
+
+func GetValidProjectIssueByID(ctx context.Context, projectID, columnID, issueID int64) (*project_model.ProjectIssue, error) {
+	if issueID == int64(0) {
+		return nil, validation.ErrNotValid{
+			Message: "IssueID must not be empty",
+		}
+	}
+	i, err := getProjectIssueByID(ctx, issueID)
+	if err != nil {
+		return nil, err
+	}
+	if i.ProjectID != projectID || i.ProjectColumnID != columnID {
+		return nil, validation.ErrNotValid{
+			Message: fmt.Sprintf("Issue with ID %v did not belong to Project with ID %v or Column with ID %v",
+				issueID, projectID, columnID),
+		}
+	}
+	return i, nil
+}
+
+// ListProjectIssues Get list of ProjectIssues of project
+func ListProjectIssues(ctx context.Context, projectID int64, listOptions db.ListOptions) ([]*project_model.ProjectIssue, int64, error) {
+	issues, total, err := project_model.GetProjectIssues(ctx, projectID, listOptions)
+	if err != nil {
+		return nil, 0, err
+	}
+	return issues, total, nil
+}
+
+// CreateIssueInProject Create a ProjectIssue in a Project, adds to DefaultColumn if columnID is 0
+func CreateIssueInProject(ctx context.Context, issue *issues_model.Issue, doer *user_model.User, projectID, colID int64) (*project_model.ProjectIssue, error) {
+	projIssue := &project_model.ProjectIssue{
+		ProjectID:       projectID,
+		ProjectColumnID: colID,
+	}
+	// CreateProjectIssue checks if the colID is 0 and then assigns to defaultCol
+	if err := issues_model.CreateProjectIssue(ctx, issue, doer, projIssue); err != nil {
+		return nil, err
+	}
+	return projIssue, nil
+}
+
+// GetIssues Get an issue list by ID and check for completeness
+func GetIssues(ctx context.Context, issueIDs []int64) (issues_model.IssueList, bool, error) {
+	issues, err := issues_model.GetIssuesByIDs(ctx, issueIDs, true)
+	if err != nil {
+		return nil, false, fmt.Errorf("ListMissingIssues %v", err.Error())
+	}
+	complete := len(issues) == len(issueIDs)
+	return issues, complete, nil
+}
+
+// MoveIssuesOnProjectColumn Moving Issues between columns or sort within column
+func MoveIssuesOnProjectColumn(ctx context.Context, column *project_model.Column, projectIssues *project_types.MovedIssuesOption) error {
+	sortedIssueIDs := projectIssues.GetSortingsMap()
+	err := project_model.MoveIssuesOnProjectColumn(ctx, column, sortedIssueIDs)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+// RemoveIssueFromProject Delete a ProjectIssue from a Project
+func RemoveIssueFromProject(ctx context.Context, issue *issues_model.Issue, doer *user_model.User, columnID int64) error {
+	return issues_model.IssueAssignOrRemoveProject(ctx, issue, doer, 0, columnID)
+}
---
+++ services/project/project.go
@@ -0,0 +1,205 @@
+// Copyright 2026 The Forgejo Authors. All rights reserved.
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+package project
+
+import (
+	"context"
+
+	"forgejo.org/models/db"
+	project_model "forgejo.org/models/project"
+	repo_model "forgejo.org/models/repo"
+	user_model "forgejo.org/models/user"
+	"forgejo.org/modules/optional"
+	project_module "forgejo.org/modules/project"
+	project_structs "forgejo.org/modules/structs"
+	validation "forgejo.org/modules/validation"
+)
+
+func getBasicSearchOpts(isShowClosed bool, sortType, keyword string, projectType project_module.ProjectAPIType, pageOpts ...int) *project_model.SearchOptions {
+	opts := &project_model.SearchOptions{
+		IsClosed: optional.Some(isShowClosed),
+		Type:     projectType.Convert(),
+	}
+	opts.OrderBy = project_model.GetSearchOrderBySortType(sortType)
+	if keyword != "" {
+		opts.Title = keyword
+	}
+	if len(pageOpts) > 1 {
+		opts.ListOptions = db.ListOptions{
+			Page:     pageOpts[0],
+			PageSize: pageOpts[1],
+		}
+	}
+	return opts
+}
+
+func GetAPIProjectType(isOrg, isRepo bool) project_module.ProjectAPIType {
+	var t project_module.ProjectAPIType
+	if isOrg {
+		t = project_module.ProjectAPITypeOrganization
+	} else if isRepo {
+		t = project_module.ProjectAPITypeRepository
+	} else {
+		t = project_module.ProjectAPITypeIndividual
+	}
+	return t
+}
+
+// Returns a valid project or fails with validation error if invalid
+func NewProject(
+	form *project_structs.CreateProjectOptions,
+	owner *user_model.User,
+	repo *repo_model.Repository,
+	projectType project_module.ProjectAPIType,
+) (*project_model.Project, error) {
+	var err error
+
+	projectTemplateType := project_module.ProjectTemplateType(form.TemplateType)
+	valid, err := validation.IsValid(projectTemplateType)
+	if !valid {
+		return nil, err
+	}
+
+	projectCardType := project_module.ProjectCardType(form.CardType)
+	valid, err = validation.IsValid(projectCardType)
+	if !valid {
+		return nil, err
+	}
+
+	valid, err = validation.IsValid(projectType)
+	if !valid {
+		return nil, err
+	}
+
+	res := &project_model.Project{
+		Title:        form.Title,
+		Description:  form.Description,
+		Owner:        owner,
+		OwnerID:      owner.ID,
+		TemplateType: projectTemplateType.Convert(),
+		CardType:     projectCardType.Convert(),
+		Type:         projectType.Convert(),
+	}
+
+	errNotValid := validation.ErrNotValid{}
+	switch projectType {
+	case project_module.ProjectAPITypeIndividual:
+		if owner.IsOrganization() {
+			errNotValid.Message = "Type was TypeIndividual, but owner was org"
+		} else if repo != nil {
+			errNotValid.Message = "Type was TypeIndividual, repo was given"
+		}
+	case project_module.ProjectAPITypeOrganization:
+		if owner.IsIndividual() {
+			errNotValid.Message = "Type was TypeOrganization, but owner was individual"
+		} else if repo != nil {
+			errNotValid.Message = "Type was TypeOrganization, repo was given"
+		}
+	case project_module.ProjectAPITypeRepository:
+		if repo != nil {
+			res.Repo = repo
+			res.RepoID = repo.ID
+		} else {
+			errNotValid.Message = "Repo type given, but repo struct was empty"
+		}
+	}
+
+	if errNotValid.Message != "" {
+		return nil, errNotValid
+	}
+
+	return res, nil
+}
+
+// GetSearchOpts returns search options for user, org or repo depending on the projectType
+func GetSearchOpts(id int64, isShowClosed bool, sortType, keyword string, projectType project_module.ProjectAPIType, pageOpts ...int) *project_model.SearchOptions {
+	opts := getBasicSearchOpts(isShowClosed, sortType, keyword, projectType, pageOpts...)
+	if projectType == project_module.ProjectAPITypeRepository {
+		opts.RepoID = id
+	} else {
+		opts.OwnerID = id
+	}
+	return opts
+}
+
+// GetProjectByID gets a specific project.
+func GetProjectByID(ctx context.Context, projectID int64) (*project_model.Project, error) {
+	project, err := project_model.GetProjectByID(ctx, projectID)
+	if err != nil {
+		return nil, err
+	}
+	return project, nil
+}
+
+func ListProjectsByOptions(ctx context.Context, opts *project_model.SearchOptions) ([]*project_model.Project, error) {
+	projects, err := db.Find[project_model.Project](ctx, opts)
+	return projects, err
+}
+
+func CountProjectsByOptions(ctx context.Context, opts *project_model.SearchOptions) (int64, error) {
+	count, err := db.Count[project_model.Project](ctx, opts)
+	return count, err
+}
+
+// Write
+// CreateProject Expects a valid project and creates it in DB
+func CreateProject(ctx context.Context, project *project_model.Project) error {
+	err := project_model.CreateProject(ctx, project)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+// UpdateProject Update Project in DB
+func UpdateProject(ctx context.Context, project *project_model.Project, updated *project_structs.CreateProjectOptions) error {
+	if updated.Title != "" {
+		project.Title = updated.Title
+	}
+
+	if updated.Description != "" {
+		project.Description = updated.Description
+	}
+
+	if updated.CardType != "" {
+		projectCardType := project_module.ProjectCardType(updated.CardType)
+		valid, err := validation.IsValid(projectCardType)
+		if !valid {
+			return err
+		}
+		project.CardType = projectCardType.Convert()
+	}
+
+	if err := project_model.UpdateProject(ctx, project); err != nil {
+		return err
+	}
+
+	if updated.Status != "" {
+		projectStatus := project_module.ProjectStatus(updated.Status)
+		valid, err := validation.IsValid(projectStatus)
+		if !valid {
+			return err
+		}
+		if project.IsClosed != projectStatus.IsClosed() {
+			if err := ChangeProjectStatus(ctx, project, projectStatus.IsClosed()); err != nil {
+				return err
+			}
+		}
+	}
+	return nil
+}
+
+// DeleteProjectByID Delete Project from DB
+func DeleteProjectByID(ctx context.Context, projectID int64) error {
+	err := project_model.DeleteProjectByID(ctx, projectID)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+// ChangeProjectStatus Change status to closed or open
+func ChangeProjectStatus(ctx context.Context, project *project_model.Project, close bool) error {
+	return project_model.ChangeProjectStatus(ctx, project, close)
+}
