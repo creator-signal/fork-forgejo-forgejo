dafd040fac48cdf1bee12637bf2286b2530a3097
Mathieu Fenniak <mathieu@fenniak.net>
2026-08-04 02:32:33 +0200

    fix: error when replying to comments on removed lines-of-code in review (#13718)
    When a pull request review comment is placed on a `previous` line -- a line that was removed in the PR, or a line on the left-hand-side of the diff -- it is not possible to reply to the comment.  A 500 error will occur when posting the comment, and the server will log an error similar to this:
    
    ```
    testlogger.go:411: 2026/07/31 14:06:54 .../repo/pull_review.go:135:CreateCodeComment() [E] CreateCodeComment: LineBlame[refs/pull/1/head, /tmp/TestPullRequestCommentPlacement1823572717/001/user2/repo-testpullrequestcommentplacement_reply_to_review_on_removed_change-f58f427a.git, file1.md, -50]: exit status 128 - fatal: bad revision ''
                 - fatal: bad revision ''
    ```
    
    The cause is that when creating a comment, Forgejo is attempting to fill out a variety of fields such as the commit SHA and patch for the comment, even in a reply.  Replies to positive line numbers (`proposed` lines) check for existing comments that are being replied to, and copy those fields from the original comment.  Replies to negative line numbers (`previous` lines) did not hit this code path.
    
    The fix is to run the search for the comment being replied to in both cases.
    
    (cherry picked from commit db760a61d52ac179f8f308f089b4741c072f17ce)
    
    Reviewed-on: https://codeberg.org/forgejo/forgejo/pulls/13718
    Reviewed-by: Andreas Ahlenstorf <aahlenst@noreply.codeberg.org>

--- services/pull/review.go
+++ services/pull/review.go
@@ -199,35 +199,38 @@ func CreateCodeCommentKnownReviewID(ctx context.Context, doer *user_model.User, 
 
 	invalidated := false
 	head := pr.GetGitRefName()
-	if line > 0 {
-		if reviewID != 0 {
-			first, err := issues_model.FindComments(ctx, &issues_model.FindCommentsOptions{
-				ReviewID: reviewID,
-				Line:     line,
-				TreePath: treePath,
-				Type:     issues_model.CommentTypeCode,
-				ListOptions: db.ListOptions{
-					PageSize: 1,
-					Page:     1,
-				},
-			})
-			if err == nil && len(first) > 0 {
-				commitID = first[0].CommitSHA
-				invalidated = first[0].Invalidated
-				patch = first[0].Patch
-			} else if err != nil && !issues_model.IsErrCommentNotExist(err) {
-				return nil, fmt.Errorf("Find first comment for %d line %d path %s. Error: %w", reviewID, line, treePath, err)
-			} else {
-				review, err := issues_model.GetReviewByID(ctx, reviewID)
-				if err == nil && len(review.CommitID) > 0 {
-					head = review.CommitID
-				} else if err != nil && !issues_model.IsErrReviewNotExist(err) {
-					return nil, fmt.Errorf("GetReviewByID %d. Error: %w", reviewID, err)
-				}
+	if reviewID != 0 {
+		// If the comment is a reply to an existing comment, populate properties based upon the comment we're replying
+		// to (commit, patch, etc.) rather than recalculating those unnecessarily.  The UI also doesn't provide enough
+		// context (eg. before_commit_id, after_commit_id) to calculate this.
+		first, err := issues_model.FindComments(ctx, &issues_model.FindCommentsOptions{
+			ReviewID: reviewID,
+			Line:     line,
+			TreePath: treePath,
+			Type:     issues_model.CommentTypeCode,
+			ListOptions: db.ListOptions{
+				PageSize: 1,
+				Page:     1,
+			},
+		})
+		if err == nil && len(first) > 0 {
+			commitID = first[0].CommitSHA
+			invalidated = first[0].Invalidated
+			patch = first[0].Patch
+		} else if err != nil && !issues_model.IsErrCommentNotExist(err) {
+			return nil, fmt.Errorf("Find first comment for %d line %d path %s. Error: %w", reviewID, line, treePath, err)
+		} else {
+			review, err := issues_model.GetReviewByID(ctx, reviewID)
+			if err == nil && len(review.CommitID) > 0 {
+				head = review.CommitID
+			} else if err != nil && !issues_model.IsErrReviewNotExist(err) {
+				return nil, fmt.Errorf("GetReviewByID %d. Error: %w", reviewID, err)
 			}
 		}
+	}
 
-		if len(commitID) == 0 {
+	if len(commitID) == 0 {
+		if line > 0 {
 			// FIXME validate treePath
 			// Get latest commit referencing the commented line
 			// No need for get commit for base branch changes
@@ -239,33 +242,33 @@ func CreateCodeCommentKnownReviewID(ctx context.Context, doer *user_model.User, 
 				return nil, fmt.Errorf("LineBlame[%s, %s, %s, %d]: %w", pr.GetGitRefName(), gitRepo.Path, treePath, line, err)
 			}
 		} else {
-			blamedCommitID = commitID
+			// Commenting on a line that was removed. In this case, what we want to track in the comment is which line of
+			// code was this, in the last commit that the line of code actually existed in. We'll use a reverse git blame to
+			// identify this, from the PR base -> commit being viewed.
+			blame, err := gitRepo.ReverseLineBlame(beforeCommitID, treePath, uint64(-1*line), afterCommitID)
+			if err != nil {
+				return nil, fmt.Errorf("ReverseLineBlame[%s, %s, %d, %s]: %w", beforeCommitID, treePath, -1*line, afterCommitID, err)
+			} else if blame.CommitID == afterCommitID {
+				// Although this is a comment on the "previous" side of the diff, the reverse blame indicates that the line
+				// of code still exists in the commit being viewed (eg. it was a comment on a white line in the left-side of
+				// the diff, not a red removed line). In order to record the right information for where to place this
+				// commit, we'll convert this into a right-hand comment -- using the present line number that the reverse
+				// blame gave us:
+				commit, lineres, err := gitRepo.LineBlame(afterCommitID, treePath, blame.LineNumber)
+				if err == nil {
+					blamedCommitID = commit.ID.String()
+					blamedLine = int64(lineres)
+				} else if !errors.Is(err, git.ErrBlameFileDoesNotExist) && !errors.Is(err, git.ErrBlameFileNotEnoughLines) {
+					return nil, fmt.Errorf("LineBlame[%s, %s, %s, %d]: %w", pr.GetGitRefName(), gitRepo.Path, treePath, line, err)
+				}
+			} else {
+				blamedCommitID = blame.CommitID
+				// retain negative line numbering to identify we're commenting on the "previous" side of the diff
+				blamedLine = -1 * int64(blame.LineNumber)
+			}
 		}
 	} else {
-		// Commenting on a line that was removed. In this case, what we want to track in the comment is which line of
-		// code was this, in the last commit that the line of code actually existed in. We'll use a reverse git blame to
-		// identify this, from the PR base -> commit being viewed.
-		blame, err := gitRepo.ReverseLineBlame(beforeCommitID, treePath, uint64(-1*line), afterCommitID)
-		if err != nil {
-			return nil, fmt.Errorf("ReverseLineBlame[%s, %s, %d, %s]: %w", beforeCommitID, treePath, -1*line, afterCommitID, err)
-		} else if blame.CommitID == afterCommitID {
-			// Although this is a comment on the "previous" side of the diff, the reverse blame indicates that the line
-			// of code still exists in the commit being viewed (eg. it was a comment on a white line in the left-side of
-			// the diff, not a red removed line). In order to record the right information for where to place this
-			// commit, we'll convert this into a right-hand comment -- using the present line number that the reverse
-			// blame gave us:
-			commit, lineres, err := gitRepo.LineBlame(afterCommitID, treePath, blame.LineNumber)
-			if err == nil {
-				blamedCommitID = commit.ID.String()
-				blamedLine = int64(lineres)
-			} else if !errors.Is(err, git.ErrBlameFileDoesNotExist) && !errors.Is(err, git.ErrBlameFileNotEnoughLines) {
-				return nil, fmt.Errorf("LineBlame[%s, %s, %s, %d]: %w", pr.GetGitRefName(), gitRepo.Path, treePath, line, err)
-			}
-		} else {
-			blamedCommitID = blame.CommitID
-			// retain negative line numbering to identify we're commenting on the "previous" side of the diff
-			blamedLine = -1 * int64(blame.LineNumber)
-		}
+		blamedCommitID = commitID
 	}
 
 	// Only fetch diff if comment is review comment
